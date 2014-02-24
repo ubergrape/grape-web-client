06658e12f952bf89d356ae07eb87160096068cac
Arpad Borsos <arpad.borsos@googlemail.com>
2014-02-24 12:40:45 +0100

    move code coverage to istanbul

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 /* vim: set shiftwidth=2 tabstop=2 noexpandtab textwidth=80 wrap : */
 "use strict";
 
-module.exports = process.env.CG_COV
-	? require('./lib-cov')
-	: require('./lib');
+module.exports = require('./lib');
