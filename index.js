67537f2206f7a884653c4e8865fc0d06f8ef53a7
Arpad Borsos <arpad.borsos@googlemail.com>
2014-03-04 12:48:21 +0100

    #133: add ability to run tests with coverage clientside and with phantomjs

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 /* vim: set shiftwidth=2 tabstop=2 noexpandtab textwidth=80 wrap : */
 "use strict";
 
-module.exports = require('./lib');
+var lib = require('./lib');
+exports.App = lib.App;
+exports.models = lib.models;
+
+try {
+
+// configure locales and template locals
+var template = require('template');
+template.root = '/cg/templates';
+template.locals.strftime = require('strftime');
+var _ = require('t');
+['de', 'en'].forEach(function (lang) {
+	_.merge(lang, require('./locale/' + lang));
+});
+template.locals._ = _;
+
+exports.UI = require('./browser');
+
+} catch (e) {} // FIXME: remove this wrapper when the tests are done purely clientside
