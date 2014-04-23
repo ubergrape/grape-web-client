d875d9c55cdd70a692b1ff68013f71b8eadbc3df
Felix Häusler <fh@newsgrape.com>
2014-04-23 12:48:03 +0200

    Sidebar Design - Avatar abstrahation

--- browser/elements/itemlist.js
+++ browser/elements/itemlist.js
@@ -34,6 +34,7 @@ ItemList.prototype.bind = function ItemList_bind() {
 	var self = this;
 	this.events = events(this.el, {
 		selectitem: function (ev) {
+			ev.preventDefault();
 			var itemEl = closest(ev.target, '.item', true);
 			var item = self.itemIds[itemEl.getAttribute('data-id')];
 			self.emit('selectitem', item);
