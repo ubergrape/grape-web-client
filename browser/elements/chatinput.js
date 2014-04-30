2c1ba4bce81b93f2b810c4a96c85238e56e958d6
Stefan Kröner <sk-github@kanen.at>
2014-04-30 15:45:04 +0200

    resizable textarea #252

--- browser/elements/chatinput.js
+++ browser/elements/chatinput.js
@@ -3,6 +3,7 @@
 
 var Emitter = require('emitter');
 var inputarea = require('inputarea');
+var resizable = require('resizable-textarea');
 var debounce = require('debounce');
 var textcomplete = require('textcomplete');
 var qs = require('query');
@@ -65,6 +66,12 @@ ChatInput.prototype.bind = function ChatInput_bind() {
 		}*/);
 		//self.attachments = [];
 	});
+
+	// make the textarea auto resize
+	var resize = debounce(function() {
+		resizable(self.textarea, {min: 31, max: 76}) }, delay);
+	resize();
+
 	// emit typing (start and stop) events
 	var delay = ChatInput.DELAY;
 	var stopped = false;
