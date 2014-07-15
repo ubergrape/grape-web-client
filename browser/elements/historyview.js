e329ac2db8f2bea042be8930633102545d9df29a
Mohamed Bahgat Elrakaiby <mbe@ubergrape.com>
2014-07-15 19:50:46 +0200

    fixing scrolling after uploading images and files #583

--- browser/elements/historyview.js
+++ browser/elements/historyview.js
@@ -114,7 +114,23 @@ HistoryView.prototype.redraw = function HistoryView_redraw() {
 	// otherwise, scroll to the last element in the room
   	if(this.room.history.length && this.scrollMode === 'automatic'){
   		if(this.lastScrolledMessage != this.room.history[this.room.history.length - 1]){
-  			this.scrollTo(this.history.el.lastChild);
+  			//if there is the length of the attachments in the message is not 0,
+  			// i.e. it's a file, do another check
+  			if(this.room.history[this.room.history.length - 1].attachments.length){
+  				// if the attachment has a property of thumbnail_height, then it is an image.
+  				// scroll the histroy with an amount of the image height
+  				if(this.room.history[this.room.history.length - 1].attachments[0].thumbnail_height){
+  					this.scrollWindow.scrollTop += this.room.history[this.room.history.length - 1].attachments[0].thumbnail_height;
+  				}
+  				// else, then it is a normal file, just scroll the history with an amount of 50
+  				else{
+  					this.scrollWindow.scrollTop += 80
+  				}
+  			}
+  			// else it is  a normal text message, thenm do the normal scroll
+  			else{
+  				this.scrollTo(this.history.el.lastChild);
+  			}
 		}
   	}
 	// update the read messages. Do this before we redraw, so the new message
@@ -142,11 +158,25 @@ HistoryView.prototype.redraw = function HistoryView_redraw() {
 	    // if the last message was already scrolled(no new messages),
   		// don't scroll as the user is navigating to a specific place
   		// otherwise, scroll to the last element in the room
-		if (focus.state === 'focus' && this.room.history.length) {
-			if(this.lastScrolledMessage != this.room.history[this.room.history.length - 1]){
-				this.scrollTo(history.lastChild);
-				this.lastScrolledMessage = 	this.room.history[this.room.history.length - 1]
-			}
+		if(this.lastScrolledMessage != this.room.history[this.room.history.length - 1]){
+  			//if there is the length of the attachments in the message is not 0,
+  			// i.e. it's a file, do another check
+  			if(this.room.history[this.room.history.length - 1].attachments.length){
+  				// if the attachment has a property of thumbnail_height, then it is an image.
+  				// scroll the histroy with an amount of the image height
+  				if(this.room.history[this.room.history.length - 1].attachments[0].thumbnail_height){
+  					this.scrollWindow.scrollTop += this.room.history[this.room.history.length - 1].attachments[0].thumbnail_height;
+  				}
+  				// else, then it is a normal file, just scroll the history with an amount of 50
+  				else{
+  					this.scrollWindow.scrollTop += 80
+  				}
+  			}
+  			// else it is  a normal text message, thenm do the normal scroll
+  			else{
+  				this.scrollTo(this.history.el.lastChild);
+  			}
+  			this.lastScrolledMessage = this.room.history[this.room.history.length - 1]
 		} else { 
 
 			/* FIXME: since grouping was introduced, this does not work as intended
