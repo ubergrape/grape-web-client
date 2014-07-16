e4a237b721763f7750bc8fa6b4e9af1e2d81c9c8
Tobias Seiler <ts@ubergrape.com>
2014-07-16 13:28:46 +0200

    unified FF and Webkit styles of autocompleted icons, #793

--- browser/renderautocomplete.js
+++ browser/renderautocomplete.js
@@ -15,7 +15,7 @@ function renderAutocompleteItem(obj, asButton) {
 			return '<button class="ac" contenteditable="false" tabindex="-1" data-object="' + obj.id + '" data-url="' + obj.url+ '"><span class="entry-type-icon type-' + obj.service + obj.type +'">&nbsp;</span>' + obj.insert + '</button>';
 		} else {
 			// Firefox, IE
-			return '<input type="button" class="ac service-' + obj.service + ' type-' + obj.service + obj.type +'" tabindex="-1" data-object="' + obj.id + '" data-url="' + obj.url+ '" value="' + obj.insert + '"/>';
+			return '<a class="ac" contenteditable="false" tabindex="-1" data-object="' + obj.id + '" data-url="' + obj.url+ '"><span class="entry-type-icon type-' + obj.service + obj.type +'">&nbsp;</span>' + obj.insert + '</a>';
 		}
 	} else {
 		var target = ' target="_blank"';
