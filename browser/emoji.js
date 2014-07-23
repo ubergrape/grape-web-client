34cf7f3ffdc900caf116619d366780a8e59ea24e
Stefan Kröner <sk-github@kanen.at>
2014-07-23 16:14:33 +0200

    use image emojis (firefox/osx) until mozilla fixes this #839

--- browser/emoji.js
+++ browser/emoji.js
@@ -7,4 +7,10 @@ jsemoji.img_path = staticurl('emoji/');
 jsemoji.sheet_path = staticurl('app/cg/images/emoji_sheet_32_optimized.png');
 jsemoji.use_sheet = true;
 
+// https://github.com/ubergrape/chatgrape/issues/839
+// https://bugzilla.mozilla.org/show_bug.cgi?id=923007
+if (navigator.userAgent.contains('Firefox') && navigator.platform === "MacIntel") {
+	jsemoji.allow_native = false;
+}
+
 module.exports = jsemoji;
