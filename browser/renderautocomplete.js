557cea55647d39f3afa14e612c2db6e3e8e582ae
Felix Häusler <fh@newsgrape.com>
2014-07-23 15:57:27 +0200

    closes #834

--- browser/renderautocomplete.js
+++ browser/renderautocomplete.js
@@ -15,7 +15,7 @@ function renderAutocompleteItem(obj, asButton) {
 			return '<button class="ac ' + obj.service + '" contenteditable="false" tabindex="-1" data-object="' + obj.id + '" data-url="' + obj.url+ '"><span class="entry-type-icon type-' + obj.service + obj.type +'">&nbsp;</span>' + obj.insert + '</button>';
 		} else {
 			// Firefox, IE
-			return '<a class="ac" contenteditable="false" tabindex="-1" data-object="' + obj.id + '" data-url="' + obj.url+ '"><span class="entry-type-icon type-' + obj.service + obj.type +'">&nbsp;</span>' + obj.insert + '</a>';
+			return '<a class="ac ' + obj.service + '" contenteditable="false" tabindex="-1" data-object="' + obj.id + '" data-url="' + obj.url+ '"><span class="entry-type-icon type-' + obj.service + obj.type +'">&nbsp;</span>' + obj.insert + '</a>';
 		}
 	} else {
 		var target = ' target="_blank"';
