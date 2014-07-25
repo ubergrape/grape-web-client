f8f362e764351a47ca0ba88757624e70930b4c00
Felix Häusler <fh@newsgrape.com>
2014-07-25 10:01:36 +0200

    Reverted 6dc35d8a1bd8e87ffc87d65061c7f01d0810daaa as things weren't deletable in FF anymore

--- browser/renderautocomplete.js
+++ browser/renderautocomplete.js
@@ -15,7 +15,8 @@ function renderAutocompleteItem(obj, asButton) {
 			return '<button class="ac ' + obj.service + '" contenteditable="false" tabindex="-1" data-object="' + obj.id + '" data-url="' + obj.url+ '"><span class="entry-type-icon type-' + obj.service + obj.type +'">&nbsp;</span>' + obj.insert + '</button>';
 		} else {
 			// Firefox, IE
-			return '<a class="ac ' + obj.service + '" contenteditable="false" tabindex="-1" data-object="' + obj.id + '" data-url="' + obj.url+ '"><span class="entry-type-icon type-' + obj.service + obj.type +'">&nbsp;</span>' + obj.insert + '</a>';
+			return '<input type="button" class="ac service-' + obj.service + ' type-' + obj.service + obj.type +'" tabindex="-1" data-object="' + obj.id + '" data-url="' + obj.url+ '" value="' + obj.insert + '"/>';
+
 		}
 	} else {
 		var target = ' target="_blank"';
