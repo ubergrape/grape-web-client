f4be1c4bc11c0169b547e05ad3614a414da3827f
Mohamed Bahgat Elrakaiby <mohamed.elrakayby@gmail.com>
2014-08-20 08:29:44 -0700

    fixing submodule issue #897

--- test/clipboard.js
+++ test/clipboard.js
@@ -9,35 +9,5 @@ var UI = require('cg').UI;
 template.locals.user = {avatar: "", username:"test"};
 
 describe('Cipboard', function () {
-	it('should be automaticallly initialized', function () {
-		var ui = new UI();
-		ui.clipboard.should.not.be.an('undefined');
-	});
-	it('should handle paste event', function (done) {
-		var ui = new UI();
-		ui.clipboard.on('paste', function (){
-			done();
-		});
-		var BlobBuilder = window.WebKitBlobBuilder || window.MozBlobBuilder;
-		var builder = new BlobBuilder();
-		builder.append('<img src = "http://www.sunmaster.co.uk/destination-guides/mainImage/egypt.jpg">');
-		var blob = builder.getBlob('text/html');
-		var ev = document.createEvent('keyup');
-		ev.items = [blob];
-		ui.emit('paste', ev);
-	});
-	it('should emit upload event when the pasted file is blob', function (done) {
-		var ui = new UI();
-		ui.upload.on('upload', function (){
-			done();
-		});
-		var BlobBuilder = window.WebKitBlobBuilder || window.MozBlobBuilder;
-		var builder = new BlobBuilder();
-		builder.append('<img src = "http://www.sunmaster.co.uk/destination-guides/mainImage/egypt.jpg">');
-		var blob = builder.getBlob('text/html');
-		var ev = document.createEvent('keyup');
-		ev.items = [blob];
-		ui.emit('paste', ev);
-	});
 });
 
