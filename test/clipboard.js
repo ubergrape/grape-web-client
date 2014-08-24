4bded04f5b4dd55f38d4694e59dc8ec3d7769f76
Mohamed Bahgat Elrakaiby <mohamed.elrakayby@gmail.com>
2014-08-24 11:19:22 -0700

    adding clipboard tests #897

--- test/clipboard.js
+++ test/clipboard.js
@@ -9,27 +9,27 @@ var UI = require('cg').UI;
 template.locals.user = {avatar: "", username:"test"};
 
 describe('Cipboard', function () {
-	/*it('should be automaticallly initialized', function (done) {
+	it('should be automaticallly initialized', function (done) {
 		var ui = new UI();
 		ui.clipboard.should.not.be.an('undefined');
 		done();
 	});
 	it('should handle paste event', function (done) {
 		var ui = new UI();
-		ui.clipboard.on('paste', function (){
+		ui.clipboard.on('paste', function (e){
 			done();
 		});
-		var BlobBuilder = window.WebKitBlobBuilder || window.MozBlobBuilder;
-		var builder = new BlobBuilder();
-		builder.append('<img src = "http://www.sunmaster.co.uk/destination-guides/mainImage/egypt.jpg">');
-		var blob = builder.getBlob('text/html');
+		//var BlobBuilder = window.WebKitBlobBuilder || window.MozBlobBuilder;
+		//var builder = new BlobBuilder();
+		//builder.append('<img src = "http://www.sunmaster.co.uk/destination-guides/mainImage/egypt.jpg">');
+		//var blob = builder.getBlob('text/html');
 		var ev = document.createEvent('CustomEvent');
 		ev.initCustomEvent('paste');
 		//ev.keyCode = 13;
-		ev.items = [blob];
+		ev.items = [];
 		window.dispatchEvent(ev);
 	});
-	it('should emit upload event when the pasted file is blob', function (done) {
+	/*it('should emit upload event when the pasted file is blob', function (done) {
 		var ui = new UI();
 		ui.upload.on('upload', function (){
 			done();
