cd47fe64f9d5678a393fcc5f1deb3075305432b8
Stefan Kröner <sk-github@kanen.at>
2014-12-16 18:08:50 +0100

    refactor dialogs

--- browser/elements/dialogs/deleteroom.js
+++ browser/elements/dialogs/deleteroom.js
@@ -1,33 +1,25 @@
 /* vim: set shiftwidth=2 tabstop=2 noexpandtab textwidth=80 wrap : */
 "use strict";
 
-var Emitter = require('emitter');
+var Dialog = require('./dialog');
 var events = require('events');
-var dialog = require('dialog');
 var qs = require('query');
-var v = require('virtualdom');
-var template = require('template');
 var _ = require('t');
 
 
 module.exports = DeleteRoomDialog;
 
 function DeleteRoomDialog(context) {
-	Emitter.call(this);
-	this.context = context || {};
-	var html = v.toDOM(template('dialogs/deleteroom.jade', this.context));
-	this.dialog = dialog(html);
-	this.el = this.dialog.el;
+	this.template_path = 'dialogs/deleteroom.jade';
+	Dialog.call(this, context);
 	this.button = qs('.delete', this.el);
 	this.form = qs('.delete-room-form', this.el);
 	if (this.form) {
 		this.passwordInput = this.form.password;
 	}
-	this.bind();
-	return this;
 }
 
-DeleteRoomDialog.prototype = Object.create(Emitter.prototype);
+DeleteRoomDialog.prototype = Object.create(Dialog.prototype);
 
 DeleteRoomDialog.prototype.bind = function DeleteRoomDialog_bind() {
 	this.events = events(this.el, this);
@@ -35,24 +27,6 @@ DeleteRoomDialog.prototype.bind = function DeleteRoomDialog_bind() {
 	this.events.obj.resetvalidity = this.resetvalidity.bind(this);
 	this.events.bind('submit .delete-room-form', 'submit');
 	this.events.bind('input .password', 'resetvalidity');
-};
-
-// proxy for dialog
-DeleteRoomDialog.prototype.closable = function DeleteRoomDialog_closable() {
-	this.dialog.closable();
-	return this;
-};
-
-// proxy for dialog
-DeleteRoomDialog.prototype.show = function DeleteRoomDialog_show() {
-	this.dialog.show();
-	return this;
-};
-
-// proxy for dialog
-DeleteRoomDialog.prototype.overlay = function DeleteRoomDialog_overlay() {
-	this.dialog.overlay();
-	return this;
 };
 
 DeleteRoomDialog.prototype.submit = function DeleteRoomDialog_submit(ev) {
--- browser/elements/dialogs/markdowntips.js
+++ browser/elements/dialogs/markdowntips.js
@@ -1,45 +1,13 @@
 /* vim: set shiftwidth=2 tabstop=2 noexpandtab textwidth=80 wrap : */
 "use strict";
 
-var Emitter = require('emitter');
-var events = require('events');
-var dialog = require('dialog');
-var v = require('virtualdom');
-var template = require('template');
-
+var Dialog = require('./dialog');
 
 module.exports = MarkdownTipsDialog;
 
 function MarkdownTipsDialog() {
-	Emitter.call(this);
-	var html = v.toDOM(template('dialogs/markdowntips.jade'));
-	this.dialog = dialog(html);
-	this.el = this.dialog.el;
-	this.bind();
-	return this;
+	this.template_path = 'dialogs/markdowntips.jade';
+	Dialog.call(this);
 }
 
-MarkdownTipsDialog.prototype = Object.create(Emitter.prototype);
-
-MarkdownTipsDialog.prototype.bind = function MarkdownTipsDialog_bind() {
-	this.events = events(this.el, this);
-};
-
-// proxy for dialog
-MarkdownTipsDialog.prototype.closable = function MarkdownTipsDialog_closable() {
-	this.dialog.closable();
-	return this;
-};
-
-// proxy for dialog
-MarkdownTipsDialog.prototype.show = function MarkdownTipsDialog_show() {
-	this.dialog.show();
-	return this;
-};
-
-// proxy for dialog
-MarkdownTipsDialog.prototype.overlay = function MarkdownTipsDialog_overlay() {
-	this.dialog.overlay();
-	return this;
-};
-
+MarkdownTipsDialog.prototype = Object.create(Dialog.prototype);
