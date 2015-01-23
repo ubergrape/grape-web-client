4b0519e1aa5d62acd39ea8c0ff995f462567883a
Stefan Kröner <sk-github@kanen.at>
2015-01-23 14:04:43 +0100

    fix intercom button #1390

--- browser/elements/popovers/user.js
+++ browser/elements/popovers/user.js
@@ -5,6 +5,7 @@ var template = require('template');
 var render = require('../../rendervdom');
 var Popover = require('./popover');
 var classes = require('classes');
+var query = require('query');
 
 module.exports = UserPopover;
 
@@ -27,3 +28,16 @@ UserPopover.prototype.redraw = function UserPopover_redraw() {
 	this.classes.add('top');
 	render(this.content, template('popovers/user.jade'));
 };
+
+UserPopover.prototype.show = function RoomPopover_show() {
+	Popover.prototype.show.apply(this, arguments);
+
+	// intercom button
+	// first, fix app_id, based on settings
+	var intercom_button = query('a' + window.intercomSettings.widget.activator, this.content.el);
+	intercom_button.href = 'mailto:' + window.intercomSettings.app_id + '@incoming.intercom.io';
+	// now rebind
+	// alternative: bind call window.Intercom.public_api.show() in a click handler
+	window.Intercom('reattach_activator');
+};
+
