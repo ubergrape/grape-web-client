6db3ec1a462f579ed5eb64f6ac51a0c093ce2d0e
Oleg Slobodskoi <oleg008@gmail.com>
2015-02-06 15:33:20 +0100

    ported fonts 1:1 from chatgrape
    not sure about the values, they use sub pixel sizes which will be an
    issue later

---
+++ fonts.js
@@ -0,0 +1,26 @@
+'use strict'
+
+var base = 13
+
+export default {
+  small: {
+    fontSize: base * .75,
+    lineHeight: base * .875
+  },
+  big: {
+    fontSize: base * 1.25,
+    lineHeight: base * 1.5
+  },
+  bigger: {
+    fontSize: base * 1.6,
+    lineHeight: base * 1.8
+  },
+  biggest: {
+    fontSize: base * 2,
+    lineHeight: base * 2.2
+  },
+  normal: {
+    fontSize: base,
+    lineHeight: base * 1.5
+  }
+}
