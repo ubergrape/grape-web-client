e7108011bc6f84fda00c4995b1e099b760e541b1
Oleg Slobodskoi <oleg008@gmail.com>
2015-02-06 15:49:31 +0100

    use helper fun, always add px

--- fonts.js
+++ fonts.js
@@ -2,25 +2,17 @@
 
 var base = 13
 
-export default {
-  small: {
-    fontSize: base * .75,
-    lineHeight: base * .875
-  },
-  big: {
-    fontSize: base * 1.25,
-    lineHeight: base * 1.5
-  },
-  bigger: {
-    fontSize: base * 1.6,
-    lineHeight: base * 1.8
-  },
-  biggest: {
-    fontSize: base * 2,
-    lineHeight: base * 2.2
-  },
-  normal: {
-    fontSize: base,
-    lineHeight: base * 1.5
+function calc(fontSize, lineHeight) {
+  return {
+    fontSize: base * fontSize + 'px',
+    lineHeight: base * lineHeight + 'px'
   }
+}
+
+export default {
+  small: calc(.75, .875),
+  big: calc(1.25, 1.5),
+  bigger: calc(1.6, 1.8),
+  biggest: calc(2, 2.2),
+  normal: calc(1, 1.5)
 }
