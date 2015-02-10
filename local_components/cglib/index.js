59dd99d9e1340ccce6a73b49316cad9d38fc2707
Stefan Kröner <sk-github@kanen.at>
2015-02-10 17:55:29 +0100

    Revert "#1546 avoid reconnecting while reconnecting, fix ws double references"
    This reverts commit 016431a0cf65648ad3f7ad7b4280bfdc0d5126cb.

--- local_components/cglib/index.js
+++ local_components/cglib/index.js
@@ -36,9 +36,8 @@ function App() {
 	// "reconnected" should reflect if this is the first initial
 	// connection or there have been reconnect attempts
 	this.reconnected = false;
-	this.reconnecting = false;
 	this.connected = false;
-	this.connecting = false;
+	this.reconnecting = false;
 }
 
 App.prototype = Object.create(Emitter.prototype);
@@ -95,12 +94,12 @@ App.prototype.heartbeat = function App_heartbeat(app) {
 App.prototype.onDisconnect = function App_ondisconnect() {
 	if (this.connected) {
 		this.stopHeartbeat();
-		if (this._ws) {
+		if (this.ws) {
 			this.unbind();
-			this._ws.close(3001);
+			this.ws.close(3001);
 		}
 		this.connected = false;
-		this.emit('disconnected', this._ws);
+		this.emit('disconnected', this.ws);
 	}
 	this.reconnect();
 };
@@ -108,10 +107,8 @@ App.prototype.onDisconnect = function App_ondisconnect() {
  * Initializes the connection and gets all the user profile and organization
  * details and joins the first one.
  */
-App.prototype.connect = function App_connect(ws) {
-	if (this.connecting)
-		return;
-	this.connecting = true;
+App.prototype.connect = function App_connect(ws, callback) {
+	callback = callback || function() {};
 	var self = this;
 	this._ws = ws;
 	if (typeof ws === 'string')
@@ -133,10 +130,10 @@ App.prototype.connect = function App_connect(ws) {
 			self.emit('change settings', self.settings);
 			self.emit('change organizations', self.organizations);
 			self.connected = true;
-			self.connecting = false;
-			self.emit('connected', self.reconnected, self._ws);
+			self.emit('connected', self.reconnected, self.ws);
 			self.startHeartbeat();
 			console.log(self.reconnected ? 'Reconnected!' : 'Connected!');
+			callback();
 		});
 	});
 	ws.on('close', function(e) {
@@ -147,8 +144,9 @@ App.prototype.connect = function App_connect(ws) {
 		console.log("Websocket Error, disconnecting!", arguments);
 		self.onDisconnect();
 	});
+	this.ws = ws;
 	// this is really ugly but needed for the tests to run
-	if (typeof this._ws._openForTest !== "undefined") this._ws._openForTest();
+	if (typeof this.ws._openForTest !== "undefined") this.ws._openForTest();
 };
 
 App.prototype.reconnect = function App_reconnect(ws) {
@@ -157,8 +155,8 @@ App.prototype.reconnect = function App_reconnect(ws) {
 	this.reconnecting = true;
 	var self = this;
 	var timeout = Math.floor((Math.random() * 5000) + 1);
-	if (this._ws)
-		delete this._ws;
+	if (this.ws)
+		delete this.ws;
 	console.log("Attempting reconnect in ms:", timeout);
 	setTimeout(function() {
 		try {
@@ -329,7 +327,7 @@ App.prototype.bindEvents = function App_bindEvents() {
 };
 
 App.prototype.unbind = function App__unbind() {
-	if (this._ws) this._ws.off();
+	if (this.ws) this.ws.off();
 	// our wamp implementation has no off() right now
 	// so we do some hacking
 	if (this.wamp) this.wamp._listeners = {};
