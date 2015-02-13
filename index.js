73f7d7139f2dcdee3723cb64d45c93292559b25e
Oleg Slobodskoi <oleg008@gmail.com>
2015-02-13 14:46:52 +0100

    fix container height

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ import Smartcomplete from './src/core/Smartcomplete'
 
 // Register reactive element.
 if (document.registerReact) {
-    document.registerReact('smart-complete', Smartcomplete);
+    document.registerReact('smart-complete', Smartcomplete)
 }
--- src/core/smartcompleteStyle.js
+++ src/core/smartcompleteStyle.js
@@ -7,6 +7,7 @@ export default {
   container: {
     display: 'flex',
     flexDirection: 'column',
+    height: '100%',
     background: colors.white,
     border: '1px solid ' + colors.gainsboroLight,
     boxShadow: '0px 3px 4px 0 ' + Color(colors.grapeTypo).alpha(.5).hslString()
