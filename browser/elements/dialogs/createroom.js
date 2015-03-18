166cb8e89cf94c1d428fe7b93916b93d835ddb54
fey <federica.recanatini@gmail.com>
2015-03-18 15:01:36 +0100

    focus on input as dialog opens #1565

--- browser/elements/dialogs/createroom.js
+++ browser/elements/dialogs/createroom.js
@@ -8,28 +8,33 @@ function CreateRoomDialog() {
 	this.template_path = 'dialogs/createroom.jade';
 	Dialog.call(this);
 	this.form = qs('form.create-room-form', this.el);
+	this.newRoomName = this.form['newroom-name'];
 }
 
 CreateRoomDialog.prototype = Object.create(Dialog.prototype);
 
 CreateRoomDialog.prototype.bind = function CreateRoomDialog_bind() {
 	this.events = events(this.el, this);
-	
 	this.events.obj.close = this.close;
 	this.events.obj.createroom = function(e) {
 		e.preventDefault();
-		var isPublic = qs('input:checked', this.form).value == "false" ? false : true;
-		var room = {
-			'name': this.form['newroom-name'].value.trim(),
-			'is_public': isPublic
-		};
+		var	isPublic = qs('input:checked', this.form).value == "false" ? false : true,
+			room = {
+				'name': this.newRoomName.value.trim(),
+				'is_public': isPublic
+			};
 		this.emit('createroom', room);
 	}
-
 	this.events.bind('click input[type="reset"]', 'close');
 	this.events.bind('submit form.create-room-form', 'createroom');
 }
 
 CreateRoomDialog.prototype.close = function CreateRoomDialog_close() {
 	this.dialog.hide();
+}
+
+CreateRoomDialog.prototype.show = function CreateRoomDialog_show() {
+	this.dialog.show();
+	this.newRoomName.focus();
+	return this;
 }
