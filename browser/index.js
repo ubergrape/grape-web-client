84b6bf13f9cf42e380aadf110025d58048a8bd03
fey <federica.recanatini@gmail.com>
2015-03-19 13:35:36 +0100

    Revert "standard proxied events naming"
    This reverts commit a10c2e06f571afc6bc2dbd87d4ad37ea7f602612.

--- browser/index.js
+++ browser/index.js
@@ -326,10 +326,10 @@ UI.prototype.bind = function UI_bind() {
 	broker(this, 'toggleinvite', this.membersMenu, 'toggle');
 
 	// navigation
-	broker(this, 'deleteduser', this.navigation, 'deleteUser');
+	broker(this, 'deletedUser', this.navigation, 'deleteUser');
 	broker(this, 'newmessage', this.navigation, 'newMessage');
-	broker(this, 'new org member', this.navigation, 'newOrgMember');
-	broker(this, 'roomdeleted', this.navigation, 'deleteRoom');
+	broker(this, 'newOrgMember', this.navigation, 'newOrgMember');
+	broker(this, 'roomDeleted', this.navigation, 'deleteRoom');
 
 	this.room = null;
 
