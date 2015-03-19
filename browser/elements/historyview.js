6846d01b2afebb46b39de59dbc3a2ee45afc1511
fey <federica.recanatini@gmail.com>
2015-03-19 12:00:34 +0100

    removed unused code for fixing scroll #1682

--- browser/elements/historyview.js
+++ browser/elements/historyview.js
@@ -191,14 +191,10 @@ HistoryView.prototype._bindScroll = function HistoryView__bindScroll() {
 		self.emit('hasread', self.room, line);
 		self.redraw();
 	}, 1500);
-	/*var reset = debounce(function () {
-		self.scrollMode = 'automatic';
-	}, 60 * 1000);*/
 	focus.on('focus', updateRead);
 	this.scrollWindow.addEventListener('scroll', updateRead);
 	this.scrollWindow.addEventListener('scroll', function () {
 		self.scrollMode = 'manual';
-		//reset();
 	});
 };
 
