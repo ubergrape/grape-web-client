31df494f3ffed5f2c0737d449213898a627ff15e
fey <federica.recanatini@gmail.com>
2015-03-20 12:25:01 +0100

    roomcreation popover bindings setup + focus on show #1566

--- browser/elements/popovers/roomcreation.js
+++ browser/elements/popovers/roomcreation.js
@@ -2,6 +2,7 @@ var Popover = require('./popover');
 var template = require('template');
 var render = require('../../rendervdom');
 var classes = require('classes');
+var qs = require('query');
 
 module.exports = RoomCreationPopover;
 
@@ -17,10 +18,18 @@ RoomCreationPopover.prototype.init = function RoomCreationPopover_init() {
 	this.redraw();
 	this.content.classes = classes(this.content.el);
 	this.el.appendChild(this.content.el);
+	this.form = qs('form.create-room-form', this.el);
+}
+
+RoomCreationPopover.prototype.bind = function RoomCreationPopover_bind() {
+	Popover.prototype.bind.call(this);	
+	
+	this.on('show', function() {
+		this.form['newroom-name'].focus();
+	});
 }
 
 RoomCreationPopover.prototype.redraw = function RoomCreationPopover_redraw() {
 	this.classes.add('right');	
 	render(this.content, template('popovers/roomcreation.jade'));
-	this.el.focus();
 }
