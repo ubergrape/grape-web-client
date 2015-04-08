8b4a3690d94a40fe1c9a321e6c421de2291ef122
fey <federica.recanatini@gmail.com>
2015-04-08 15:28:52 +0200

    activity grouping added to groupHistory function #1302

--- browser/elements/historyview.js
+++ browser/elements/historyview.js
@@ -102,10 +102,26 @@ function groupHistory(history) {
 	var groups = [];
 	var last;
 	var group;
+	var counter = 1;
+
 	for (var i = 0; i < history.length; i++) {
 		var line = history[i];
-		if (!last || last.author !== line.author || last.time.getTime() + TIME_THRESHOLD < line.time.getTime())
+		var author = line.author;
+		var isTimeSpanShort = last && last.time.getTime() + TIME_THRESHOLD > line.time.getTime();
+		var isService = author.type == "service";
+		var canCollapse = isTimeSpanShort && last.author.id == author.id && ((isService && line.title == last.title) || !isService);
+
+		if (canCollapse) {
+			if (isService) {
+				group.pop();
+				counter++;
+				line.times = counter.toString(); // convert to string cause jade gets crazy with numbers		
+			}
+		} else {
 			groups.push(group = []);
+			counter = 1;		
+		}
+
 		group.push(last = line);
 	}
 	return groups;
@@ -120,10 +136,11 @@ HistoryView.prototype.redraw = function HistoryView_redraw() {
 		(this.scrollMode === 'automatic' && focus.state === 'focus')))
 		this.emit('hasread', this.room, this.room.history[this.room.history.length - 1]);
 
+	var groupedHistory = groupHistory(this.room.history);
+
 	render(this.history, template('chathistory.jade', {
 		room: this.room,
-		history: this.room.history,
-		groupHistory: groupHistory
+		history: groupedHistory
 	}));
 
 	if (this.lastwindow.lastmsg !== this.room.history[0]) {
