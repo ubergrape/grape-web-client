b7ac2a3dbabd8bfc30321a30d1e2729b2883fe25
fey <federica.recanatini@gmail.com>
2015-04-18 08:49:39 +0200

    no history redraw on user change - better if commented out

--- browser/index.js
+++ browser/index.js
@@ -346,6 +346,7 @@ UI.prototype.setUser = function UI_setUser(user) {
 		template.locals.user = user;
 		this.grapeInput.redraw();
 	}
+	//this.historyView.redraw();
 };
 
 UI.prototype.setSettings = function UI_setSettings(settings) {
