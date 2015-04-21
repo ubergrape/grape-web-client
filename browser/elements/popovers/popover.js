5cd9997d5b7627cd51a7a6b59c8657a222ef9438
Felix Häusler <fh@ubergrape.com>
2015-04-21 10:09:38 +0200

    new popover animation - and for JSS i fiddled sth in

--- browser/elements/popovers/popover.js
+++ browser/elements/popovers/popover.js
@@ -57,11 +57,13 @@ Popover.prototype.show = function Popover_show(trigger) {
 	document.body.appendChild(this.el);
 	this.hidden = false;
 	this.emit('show');
+	/*
 	if (this.classes.has('left')) {
 		animate(this.el, 'fade-left-in');  
 	} else { // TOP
 		animate(this.el, 'fade-down-in');  
 	}
+	*/
 };
 
 Popover.prototype.hide = function Popover_hide() {
