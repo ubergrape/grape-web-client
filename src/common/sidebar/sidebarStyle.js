a92752309c0c994040a3c3fe59a992084f6473f9
Gadzhi Kharkharov <kkga@me.com>
2015-05-05 17:21:09 +0300

    remove sidebar inset shadow

--- src/common/sidebar/sidebarStyle.js
+++ src/common/sidebar/sidebarStyle.js
@@ -5,9 +5,6 @@ export default {
   sidebar: {
     extend: fonts.normal,
     height: '100%',
-    boxShadow: '-1px 0 0 rgba(0,0,0,0.10)',
-    color: colors.grapeTypo
-    boxShadow: 'inset 0px 0px 3px 3px rgba(0,0,0,0.10)',
     color: colors.grapeTypo,
     borderLeft: '3px solid ' + colors.grapeLight
   }
