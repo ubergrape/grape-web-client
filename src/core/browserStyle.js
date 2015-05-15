c77c30461e3480230d1d4e3bc93279e027a27cea
Gadzhi Kharkharov <kkga@me.com>
2015-05-15 20:38:46 +0300

    remove browser container border and add radius

--- src/core/browserStyle.js
+++ src/core/browserStyle.js
@@ -6,7 +6,8 @@ export default {
     flexDirection: 'column',
     height: '100%',
     background: colors.white,
-    border: '1px solid ' + colors.gainsboroLight,
+    borderRadius: '3px',
+    overflow: 'hidden',
     boxShadow: '0 5px 11px 0 rgba(0, 0, 0, 0.18), 0 4px 15px 0 rgba(0, 0, 0, 0.15)',
     transform: 'scaleX(1) translate3d(0, 0, 0)',
     animation: 'hifromthebottom 0.2s cubic-bezier(0.25, 0.46, 0.45, 0.94)'
