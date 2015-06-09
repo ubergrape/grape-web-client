91e8627c530c2ef716ea05e44ef293273636a0f6
Gadzhi Kharkharov <kkga@me.com>
2015-06-09 13:56:51 +0300

    kill appearing animation

--- src/search/style.js
+++ src/search/style.js
@@ -8,8 +8,6 @@ export default {
     background: colors.white,
     borderRadius: '3px',
     overflow: 'hidden',
-    boxShadow: '0 5px 11px 0 rgba(0, 0, 0, 0.18), 0 4px 15px 0 rgba(0, 0, 0, 0.15)',
-    transform: 'scaleX(1) translate3d(0, 0, 0)',
-    animation: 'hifromthebottom 0.2s cubic-bezier(0.25, 0.46, 0.45, 0.94)'
+    boxShadow: '0 5px 11px 0 rgba(0, 0, 0, 0.18), 0 4px 15px 0 rgba(0, 0, 0, 0.15)'
   }
 }
