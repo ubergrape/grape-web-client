f7457b113dfd82d8af99c4e6fd3747804be9b039
fey <federica.recanatini@gmail.com>
2015-06-18 12:55:41 +0200

    better phrasing in room deletion input #2134

--- browser/elements/dialogs/deleteroom.js
+++ browser/elements/dialogs/deleteroom.js
@@ -33,7 +33,7 @@ DeleteRoomDialog.prototype.submit = function DeleteRoomDialog_submit(ev) {
 
 	var roomName = self.roomNameInput.value.trim();
 	if (!roomName) {
-		self.roomNameInput.setCustomValidity(_("Please enter the name of the room you want to delete"));
+		self.roomNameInput.setCustomValidity(_("Please type in the name of the room to confirm."));
 		self.button.click();
 		return;
 	}
