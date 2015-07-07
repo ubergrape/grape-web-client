88a108261fa0fe52bf1a840d29768b292cae7b38
Oleg Slobodskoi <oleg008@gmail.com>
2015-07-07 14:45:53 +0200

    a bit better spinner
    ubergrape/chatgrape#2374

--- src/components/browser/style.js
+++ src/components/browser/style.js
@@ -5,7 +5,7 @@ export default {
   browser: {
     display: 'flex',
     flexDirection: 'column',
-    background: colors.silverDark,
+    background: colors.white,
     borderRadius: sizes.borderRadius.small,
     overflow: 'hidden',
     boxShadow: '0 5px 11px 0 rgba(0, 0, 0, 0.18), 0 4px 15px 0 rgba(0, 0, 0, 0.15)'
