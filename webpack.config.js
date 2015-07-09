f8cc764a155b21ec909ae52eb37fa7a437e8735d
fey <federica.recanatini@gmail.com>
2015-07-09 18:15:39 +0200

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -8,19 +8,19 @@ module.exports = {
 	},
 	resolve: {
 		alias: {
-		    "classes": "component-classes",
-		    "clipboard": "component-clipboard",
-		    "closest": "component-closest",
-		    "dialog": "dialog-component",
-		    "emitter": "component-emitter",
-		    "file": "component-file",
-		    "notification": "component-notification",
-		    "page": "visionmedia-page.js",
-		    "progress": "progress-component",
-		    "query": "component-query",
-		    "raf": "component-raf",
-		    "resizable": "jh3y-resizable",
-		    "upload": "component-upload"	
+			"classes": "component-classes",
+			"clipboard": "component-clipboard",
+			"closest": "component-closest",
+			"dialog": "dialog-component",
+			"emitter": "component-emitter",
+			"file": "component-file",
+			"notification": "component-notification",
+			"page": "visionmedia-page.js",
+			"progress": "progress-component",
+			"query": "component-query",
+			"raf": "component-raf",
+			"resizable": "jh3y-resizable",
+			"upload": "component-upload"	
 		},
 		modulesDirectories: [
 			"lib",
