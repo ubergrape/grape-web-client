d9bbc3e80451ed10cf686b96bb92937efe37da4d
fey <federica.recanatini@gmail.com>
2015-07-09 18:12:22 +0200

    missing some components, created aliases

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,27 @@ module.exports = {
 		path: path.resolve(__dirname, 'loip'),
 		filename: 'loip.js',
 	},
+	resolve: {
+		alias: {
+		    "classes": "component-classes",
+		    "clipboard": "component-clipboard",
+		    "closest": "component-closest",
+		    "dialog": "dialog-component",
+		    "emitter": "component-emitter",
+		    "file": "component-file",
+		    "notification": "component-notification",
+		    "page": "visionmedia-page.js",
+		    "progress": "progress-component",
+		    "query": "component-query",
+		    "raf": "component-raf",
+		    "resizable": "jh3y-resizable",
+		    "upload": "component-upload"	
+		},
+		modulesDirectories: [
+			"lib",
+			"local_components",
+			"node_modules"
+		],
+		subDirectories: true
+	}
 };
