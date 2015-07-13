3c790fcf09ea9ae605a8ade90d192693178ef39c
Oleg Slobodskoi <oleg008@gmail.com>
2015-07-13 20:08:10 +0200

    register in index, export correctly
    ubergrape/chatgrape#2406

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import 'reactive-elements'
-import Browser from './src'
-export default Browser
+import * as browsers from './src'
+export default browsers
+
+// Register reactive element.
+if (document.registerReact) {
+  document.registerReact('grape-search-browser', browsers.SearchBrowser)
+  document.registerReact('grape-emoji-browser', browsers.EmojiBrowser)
+}
--- src/index.js
+++ src/index.js
@@ -4,9 +4,3 @@ export {SearchBrowser as SearchBrowser}
 
 import EmojiBrowser from './components/browser-emoji/Browser'
 export {EmojiBrowser as EmojiBrowser}
-
-// Register reactive element.
-if (document.registerReact) {
-  document.registerReact('grape-search-browser', SearchBrowser)
-  document.registerReact('grape-emoji-browser', EmojiBrowser)
-}
