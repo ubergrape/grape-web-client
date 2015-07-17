5b7f49ebdde305be64e52c42853e4493d31a03f7
fey <federica.recanatini@gmail.com>
2015-07-17 14:59:47 +0200

    changed paths, loaded images and fonts, extracted css

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* vim: set shiftwidth=2 tabstop=2 noexpandtab textwidth=80 wrap : */
 "use strict";
-// var style = require('./stylus/app.styl');
+var style = require('./stylus/app.styl');
 //var chatheader = require('!./jade-virtualdom-loader!./templates/chatheader.jade')
 exports.App = require('cglib');
 exports.UI = require('./browser');
--- webpack.config.js
+++ webpack.config.js
@@ -20,11 +20,23 @@ module.exports = {
 			{
 				test: /\.json$/,
 				loader: 'json-loader'
+			},
+			{
+				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: "url-loader?limit=10000&minetype=application/font-woff"
+			},
+			{
+				test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: "file-loader"
+			},
+			{
+				test: /\.(png|jpg|gif)$/,
+				loader: 'url-loader?limit=8192'
 			}
 		]
 	},
 	plugins: [
-		new extractTextPlugin('./loip/styles.css')
+		new extractTextPlugin('app.css')
 	],
 	resolve: {
 		alias: {
