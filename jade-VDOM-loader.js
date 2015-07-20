8457b58f58c3e564aa385eb18e6fa0748575da2d
fey <federica.recanatini@gmail.com>
2015-07-20 15:10:56 +0200

    loader sketch

--- jade-VDOM-loader.js
+++ jade-VDOM-loader.js
@@ -1,6 +1,10 @@
-//var jadeV = require('require-cwd')('jade-virtualdom');
-//var loaderUtils = require('loader-utils');
+var jadeV = require('require-cwd')('jade-virtualdom');
+var loaderUtils = require('loader-utils');
+var root = 'templates/';
 
 module.exports = function(source) {
-	return source;
+	var filePath = loaderUtils.getRemainingRequest(this).replace(/^!/, "");
+	var splitPath = filePath.split(root);
+	var fileName = splitPath[splitPath.length - 1];
+	return jadeV(source, {filename: filePath}).toString();
 }
