ecc6714d734db1295fd7e847ee88250589cea33d
fey <federica.recanatini@gmail.com>
2015-07-20 11:23:00 +0200

    list of templates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* vim: set shiftwidth=2 tabstop=2 noexpandtab textwidth=80 wrap : */
 "use strict";
-var style = require('./stylus/app.styl');
-//var chatheader = require('!./jade-virtualdom-loader!./templates/chatheader.jade')
+require('./stylus/app.styl');
+require('./templates')
 exports.App = require('cglib');
 exports.UI = require('./browser');
 exports.broker = require('./lib/broker');
--- jade-virtualdom-loader.js
+++ jade-virtualdom-loader.js
@@ -4,8 +4,5 @@ var loaderUtils = require('loader-utils');
 module.exports = function(source) {
 	this.cacheable && this.cacheable();
 	var filepath = loaderUtils.getRemainingRequest(this).replace(/^!/, "");
-//	var transform = "require.modules['bubi'] = function(exports, module) {";
-//	var res = "require.modules['bubi'] = function(exports, module) { module.exports = " + jadeV(source).toString() + " };";
-//	return "module.exports= " + jadeV(source, {filename: filepath}).toString();
-	return "require.modules['bubi'] = " + jadeV(source, {filename: filepath}).toString();
+	return source;
 }
