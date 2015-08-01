5a2c5cd12b10fd4b0a44a75c6e383940ff65bed5
fey <federica.recanatini@gmail.com>
2015-08-01 23:41:36 +0200

    router cleanup

--- local_modules/router/index.js
+++ local_modules/router/index.js
@@ -3,8 +3,10 @@ var page = require('page');
 module.exports = Router;
 
 function Router(ui) {	
-	var	baseURL	= '/chat',
-		cUser	= ui.user;
+	var	baseURL	= '/chat';
+	var	cUser = ui.user;
+	var navRoomList = ui.navigation.roomList.items;
+	var navPMList = ui.navigation.pmList.items;
 	page.stop();
 	page.base(baseURL);
 	page('/', pickChannel);
@@ -17,70 +19,70 @@ function Router(ui) {
 
 	function pickChannel () {
 		var redirectRoom = false;
-		ui.navigation.roomList.items.every(function(room) {
+		navRoomList.every(function(room) {
 			if (room.joined) {
 				redirectRoom = room;
 				return false;
 			}
 			return true;
 		});
-		var redirectSlug = redirectRoom ? redirectRoom.slug : '@' + ui.navigation.pmList.items[0].username.toLowerCase();		
+		var redirectSlug = redirectRoom ? redirectRoom.slug : '@' + navPMList[0].username.toLowerCase();		
 		page.replace(baseURL + '/' + redirectSlug);
 	}
 
 	function goToPM (cxt) {
-		var	username	= cxt.params.pm,
-			chosenPM	= selectPM(username),
-			message		= cxt.params.message ? cxt.params.message : null;
-		if (chosenPM) {
-			if (chosenPM.pm) return ui.emit('selectchannel', chosenPM.pm, message);
-			ui.emit('openpm', chosenPM, function() {
-				ui.emit('selectchannel', chosenPM.pm, message);
+		var	username = cxt.params.pm;
+		var user = findPM(username);
+		var message = cxt.params.message ? cxt.params.message : null;
+		if (user) {
+			if (user.pm) return ui.emit('selectchannel', user.pm, message);
+			ui.emit('openpm', user, function() {
+				ui.emit('selectchannel', user.pm, message);
 			});
 		} else {
 			notFound();
 		}
 	}
 
-	function selectPM (username) {
-		var chosenPM = false;
+	function findPM (username) {
+		var selectedUser = false;
 		ui.org.users.every(function(user) {
 			if(user.username.toLowerCase() == username
 				&& cUser.username != username) {
-				chosenPM = user;
+				selectedUser = user;
 				return false;
 			} else {
 				return true;
 			}
 		});
-		return chosenPM;
+		return selectedUser;
 	}
 
 	function goToRoom (cxt) {
-		var	slug		= cxt.params.room,
-			chosenRoom	= selectRoom(slug),
-			message		= cxt.params.message ? cxt.params.message : null;
-		if (chosenRoom) {
-			if (chosenRoom.joined) return ui.emit('selectchannel', chosenRoom, message);
-			ui.emit('joinroom', chosenRoom, function() {
-				ui.emit('selectchannel', chosenRoom, message);
+		var	slug = cxt.params.room;
+		var room = findRoom(slug);
+		var message = cxt.params.message ? cxt.params.message : null;
+		if (room) {
+			if (room.joined) return ui.emit('selectchannel', room, message);
+			ui.emit('joinroom', room, function() {
+				ui.emit('selectchannel', room, message);
 			});
 		} else {
 			notFound();
 		}
 	}
 
-	function selectRoom (slug) {
-		var chosenRoom = false;
+	function findRoom (slug) {
+		var selectedRoom = false;
 		ui.org.rooms.every(function(room) {
 			if(room.slug == slug) {
-				chosenRoom = room;
+				selectedRoom = room;
 				return false;
 			} else {
 				return true;
 			}
 		});
-		return chosenRoom;
+		return selectedRoom;
 	}
 
 	function notFound() {
