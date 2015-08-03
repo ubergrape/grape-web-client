10db945b025e4dc90866b481d27c51cf5b549fbc
Stefan Kröner <sk7@users.noreply.github.com>
2015-08-03 17:52:15 +0200

    toggle body class #2508

--- src/ui/elements/dialogs/preferences.js
+++ src/ui/elements/dialogs/preferences.js
@@ -5,6 +5,7 @@ var Dialog = require('./dialog');
 
 var events = require('events');
 var qs = require('query');
+var classes = require('classes');
 var _ = require('t');
 
 module.exports = PreferencesDialog;
@@ -20,8 +21,21 @@ PreferencesDialog.prototype.bind = function PreferencesDialog_bind() {
 	this.events = events(this.el, this);
 
 	this.events.bind('click input.compactmode', 'toggleCompactMode');
-};
-PreferencesDialog.prototype.toggleCompactMode = function PreferencesDialog_toggleCompactMode(ev) {
-	console.log("COMPACT MODE TOGGLED");
+	this.events.bind('click input.darkmode', 'toggleDarkMode');
 };
 
+PreferencesDialog.prototype.toggleCompactMode = function PreferencesDialog_toggleCompactMode(ev) {
+	toggleBodyClass(ev.target.checked, "compact");
+};
+
+PreferencesDialog.prototype.toggleDarkMode = function PreferencesDialog_toggleDarkMode(ev) {
+	toggleBodyClass(ev.target.checked, "dark");
+};
+
+var toggleBodyClass = function toggleBodyClass(condition, classname) {
+	if(condition) {
+		classes(qs('body')).add(classname);
+	} else {
+		classes(qs('body')).remove(classname);
+	}
+};
