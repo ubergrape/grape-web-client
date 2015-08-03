edfd6a6c0b7d86b5f6f0f709fad770011c20dad3
fey <federica.recanatini@gmail.com>
2015-08-03 22:07:47 +0200

    private room in to join list again #2511

--- src/ui/elements/popovers/roommanager.js
+++ src/ui/elements/popovers/roommanager.js
@@ -129,10 +129,6 @@ RoomManagerPopover.prototype.onJoinedChannel = function RoomManagerPopover_onJoi
 }
 
 RoomManagerPopover.prototype.onLeftChannel = function RoomManagerPopover_onLeftChannel (room) {
-	if (!room.is_public) {
-		var roomIndex = this.itemList.items.indexOf(room);
-		this.itemList.items.splice(roomIndex, 1);
-	}	
 	this.itemList.redraw();
 }
 
