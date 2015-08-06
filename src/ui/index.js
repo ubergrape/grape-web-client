9639c3fdc933b16d257f4928f7be0c908195ef35
fey <federica.recanatini@gmail.com>
2015-08-06 19:20:28 +0200

    more cleanup

--- src/ui/index.js
+++ src/ui/index.js
@@ -57,7 +57,6 @@ exports.ItemList = require('./elements/itemlist');
 var Navigation = exports.Navigation = require('./elements/navigation');
 var RoomManagerPopover = exports.RoomManagerPopover = require('./elements/popovers/roommanager');
 var UserPopover = exports.UserPopover = require('./elements/popovers/user');
-var PMManagerPopover = exports.PMPopover = require('./elements/popovers/pmmanager');
 var OrganizationPopover = exports.OrganizationPopover = require('./elements/popovers/organization');
 var ChatHeader = exports.ChatHeader = require('./elements/chatheader');
 var RightSidebar = exports.RightSidebar = require('./elements/rightsidebar');
@@ -107,7 +106,6 @@ UI.prototype.init = function UI_init() {
 
 	// initialize the popovers
 	this.roomManager = new RoomManagerPopover();
-	this.PMManager = new PMManagerPopover();
 	this.userMenu = new UserPopover();
 	this.organizationMenu = new OrganizationPopover();
 	this.searchView = new SearchView();
