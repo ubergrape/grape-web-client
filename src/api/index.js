1d3e069f439d46e1d17abf9b205290ec8ed60d4b
fey <federica.recanatini@gmail.com>
2015-08-12 00:18:18 +0200

    Revert "deployment triggering commit"
    This reverts commit bc3f6efa65a380afbfe774dd1b03936200a7e35f.

--- src/api/index.js
+++ src/api/index.js
@@ -550,6 +550,8 @@ API.prototype.getRoomIcons = function API_getRoomIcons(org, callback) {
 
 	self.wamp.call(PREFIX + 'organizations/list_icons', org.id, function (err, res) {
 		if (err) return self.emit('error', err);
+
+		console.log("List Room icons", "Result: " + res);
 	})
 }
 
