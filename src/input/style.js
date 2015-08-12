7f692cfcd71cee7dd7d6b3ef6a928450083aa1a4
orhnsnmz <orhnsnmz@gmail.com>
2015-08-12 10:27:47 +0200

    Fixed lint problems

--- src/input/style.js
+++ src/input/style.js
@@ -6,8 +6,8 @@ export default {
     ...fonts.big,
     width: '100%',
     color: colors.grapeTypo,
-    borderRadius: "6px 6px 0px 0px",
-    border: "solid 1px #E5E5E5",
+    borderRadius: '6px 6px 0px 0px',
+    border: 'solid 1px #E5E5E5',
     padding: 15,
     outline: 'none'
   }
