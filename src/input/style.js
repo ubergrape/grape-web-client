14e88b72390aca334382248c32878047cbe43b50
orhnsnmz <orhnsnmz@gmail.com>
2015-08-12 10:18:26 +0200

    Small but fine input field facelift

--- src/input/style.js
+++ src/input/style.js
@@ -1,10 +1,14 @@
 import fonts from 'grape-theme/dist/fonts'
+import colors from 'grape-theme/dist/base-colors'
 
 export default {
   input: {
     ...fonts.big,
     width: '100%',
-    padding: 5,
+    color: colors.grapeTypo,
+    borderRadius: "6px 6px 0px 0px",
+    border: "solid 1px #E5E5E5",
+    padding: 15,
     outline: 'none'
   }
 }
