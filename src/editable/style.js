1ef67df15ef432559b9c81d05488caaa4f6b8aea
Oleg Slobodskoi <oleg008@gmail.com>
2015-08-19 11:14:55 +0200

    fixing editable styling
    ubergrape/chatgrape#2473

--- src/editable/style.js
+++ src/editable/style.js
@@ -1,9 +1,10 @@
 import colors from 'grape-theme/dist/base-colors'
-import inputStyle from '../input/style'
 
 export default {
   editable: {
-    ...inputStyle.input,
+    display: 'inline-block',
+    width: '100%',
+    height: '100%',
     lineHeight: 1.5,
     padding: 8,
     outline: 'none',
