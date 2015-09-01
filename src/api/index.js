d4384fe85c103052bd5ef2f80b0d3b687b7b6020
fey <federica.recanatini@gmail.com>
2015-09-01 13:19:22 +0200

    syncing read messages

--- src/api/index.js
+++ src/api/index.js
@@ -336,7 +336,10 @@ API.prototype.bindEvents = function API_bindEvents() {
 		if (!line) return; // ignore read notifications for messages we don’t have
 		var room = line.channel;
 		// ignore this for the current user, we track somewhere else
-		if (user === self.user) return self.emit('channelRead');
+		if (user === self.user) {
+			room.unread = 0;
+			return self.emit('channelRead');
+		}
 		var last = room._readingStatus[data.user];
 		// remove the user from the last lines readers
 		if (last) {
