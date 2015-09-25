33d64e976b376017c309bde6e4b0b4cb1538ca22
Oleg Slobodskoi <oleg008@gmail.com>
2015-09-25 19:58:29 +0200

    move example to js file

---
+++ examples/grape-input/example.js
@@ -0,0 +1,109 @@
+var searchResults = window.searchData.results
+
+var input = document.querySelectorAll('grape-input')[0]
+
+function setProps(newProps) {
+  var props = getDefaultProps()
+  for (var key in newProps) props[key] = newProps[key]
+  input.props = props
+}
+
+function getDefaultProps() {
+  return {
+    customEmojis: {test: '../images/avatar.gif'},
+    placeholder: 'Some placeholder',
+    images: {
+      emojiSheet: '../../node_modules/js-emoji/images/sheet_32.png',
+      traubyReading: '../images/trauby-reading.png',
+      traubyJuggling: '../images/trauby-juggling.png',
+      noDetail: '../images/no-detail.png',
+      orgLogo: '../images/avatar.gif',
+      spinner: '../images/loading.gif'
+    }
+  }
+}
+
+function init() {
+  setProps({focused: true})
+
+  input.addEventListener('grapeComplete', function (e) {
+    console.log('complete', e.detail)
+
+    if (e.detail.trigger == '#') {
+        var data = window.searchData
+        data.results = searchResults.filter(match)
+        setProps({
+          browser: 'search',
+          data: data
+        })
+    }
+    else if (e.detail.trigger == '@') {
+      var data = window.userData
+      if (e.detail.search) data = data.filter(match)
+      setProps({
+        data: data,
+        browser: 'user'
+      })
+    }
+    else if (e.detail.trigger == ':') {
+      setProps({browser: 'emoji'})
+    }
+
+    function match(result) {
+      return result.name.toLowerCase().indexOf(e.detail.search) >= 0
+    }
+  })
+  input.addEventListener('grapeSubmit', function (e) {
+    console.log('submit', e.detail)
+    input.setTextContent('')
+  })
+
+  input.addEventListener('grapeEditPrevious', function (e) {
+    console.log('edit previous')
+  })
+
+  input.addEventListener('grapeChange', function (e) {
+    console.log(input.getTextContent())
+  })
+
+  input.addEventListener('grapeFocus', function (e) {
+    console.log('focus')
+  })
+
+  input.addEventListener('grapeBlur', function (e) {
+    console.log('blur')
+  })
+
+  input.addEventListener('grapeAbort', function (e) {
+    console.log('abort')
+  })
+
+  input.addEventListener('grapeInsertItem', function (e) {
+    console.log('grapeInsertItem', e.detail)
+  })
+
+  input.addEventListener('grapeAddIntegration', function (e) {
+    console.log('add integration')
+  })
+
+  input.addEventListener('grapeSelectFilter', function (e) {
+    console.log('select filter', e.detail)
+    setProps({
+      browser: 'search',
+      data: window.searchData
+    })
+  })
+
+}
+
+function showSearchDelayed(delay) {
+  setProps({browser: 'search', isLoading: true})
+  setTimeout(function() {
+    setProps({
+      browser: 'search',
+      data: window.searchData
+    })
+  }, delay)
+}
+
+document.addEventListener('DOMContentLoaded', init)
