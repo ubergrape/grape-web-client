b55c04e4616fbf9401de9c4bc9c854a4be30f645
fey <federica.recanatini@gmail.com>
2015-10-07 09:01:23 +0200

    linter on dialog tests

--- src/ui/elements/dialogs/tests/index.js
+++ src/ui/elements/dialogs/tests/index.js
@@ -1,56 +1,54 @@
-var expect = require('expect.js');
-var Dialog = require('../dialog');
-var qs = require('query');
-var MDTip = require('../markdowntips');
-var RoomDelete = require('../deleteroom');
-var PMManager = require('../pmmanager');
-var RoomManager = require('../roommanager');
-var fixtures = require('../../../../../fixtures');
-var room = fixtures.room;
-var user = fixtures.user;
-var template = require('template');
-template.locals.user = user;
+let expect = require('expect.js')
+let qs = require('query')
+let MDTip = require('../markdowntips')
+let RoomDelete = require('../deleteroom')
+let RoomManager = require('../roommanager')
+let fixtures = require('../../../../../fixtures')
+let room = fixtures.room
+let user = fixtures.user
+let template = require('template')
+template.locals.user = user
 
-var mdTip = new MDTip();
-var roomDelete = new RoomDelete({
+let mdTip = new MDTip()
+let roomDelete = new RoomDelete({
     room: room
-});
-var roomManager = new RoomManager({
+})
+let roomManager = new RoomManager({
     rooms: [room]
-});
+})
 
-describe('Dialog', function (){
-    describe('Markdown Dialog', function () {
-        it('should have a DOM element', function () {
-            expect(mdTip).to.have.property('el');
-            expect(mdTip.el).to.be.an(Element);
-        });
-        it('should open', function () {
-            mdTip.show();
-            var el = qs('.markdown-tips', mdTip.el);
-            expect(el).not.to.be(null);
-        });
-    });
-    describe('Room Delete Dialog', function () {
-        it('should have a DOM element', function () {
-            expect(roomDelete).to.have.property('el');
-            expect(roomDelete.el).to.be.an(Element);
-        });
-        it('should open', function () {
-            roomDelete.show();
-            var el = qs('.delete-room', roomDelete.el);
-            expect(el).not.to.be(null);
-        });
-    });
-    describe('Room Manager', function () {
-        it('should have a DOM element', function () {
-            expect(roomManager).to.have.property('el');
-            expect(roomManager.el).to.be.an(Element);
-        });
-        it('should open', function () {
-            roomManager.show();
-            var el = qs('.room-list-container', roomManager.el);
-            expect(el).not.to.be(null);
-        });
-    });
-});
+describe('Dialog', function() {
+    describe('Markdown Dialog', function() {
+        it('should have a DOM element', function() {
+            expect(mdTip).to.have.property('el')
+            expect(mdTip.el).to.be.an(Element)
+        })
+        it('should open', function() {
+            mdTip.show()
+            let el = qs('.markdown-tips', mdTip.el)
+            expect(el).not.to.be(null)
+        })
+    })
+    describe('Room Delete Dialog', function() {
+        it('should have a DOM element', function() {
+            expect(roomDelete).to.have.property('el')
+            expect(roomDelete.el).to.be.an(Element)
+        })
+        it('should open', function() {
+            roomDelete.show()
+            let el = qs('.delete-room', roomDelete.el)
+            expect(el).not.to.be(null)
+        })
+    })
+    describe('Room Manager', function() {
+        it('should have a DOM element', function() {
+            expect(roomManager).to.have.property('el')
+            expect(roomManager.el).to.be.an(Element)
+        })
+        it('should open', function() {
+            roomManager.show()
+            let el = qs('.room-list-container', roomManager.el)
+            expect(el).not.to.be(null)
+        })
+    })
+})
