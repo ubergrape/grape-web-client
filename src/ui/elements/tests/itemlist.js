bbfc98b9399496848f8bfe271490fb4674cb5477
fey <federica.recanatini@gmail.com>
2015-10-07 08:58:27 +0200

    linter on tests for itemlist

--- src/ui/elements/tests/itemlist.js
+++ src/ui/elements/tests/itemlist.js
@@ -1,42 +1,43 @@
-var Emitter = require('emitter');
-var ItemList = require('../itemlist');
-var fixtures = require('../../../../fixtures');
-var room = fixtures.room;
-var anotherRoom = fixtures.anotherRoom;
+let expect = require('expect.js')
+let Emitter = require('emitter')
+let ItemList = require('../itemlist')
+let fixtures = require('../../../../fixtures')
+let room = fixtures.room
+let anotherRoom = fixtures.anotherRoom
 
-var roomList = new ItemList({
+let roomList = new ItemList({
     'template': 'roomlist.jade'
-});
+})
 
-describe('ItemList', function () {
-    it('should be able to emit events', function () {
-        expect(roomList).to.be.a(Emitter);
-    });
-    it('should have an empty item list by default', function () {
-        expect(roomList.items).to.be.empty();
-    });
-    it('should have no selected item by default', function () {
-        expect(roomList.selected).to.be(null);
-    });
-    it('should be possible to redraw', function () {
-        expect(roomList.redraw).to.be.a('function');
-    });
-    it('should accept items', function () {
-        var rooms = [room, anotherRoom];
-        roomList.setItems(rooms);
-        expect(roomList.items).to.eql(rooms);
-    });
-    it('should offer a way to select items', function () {
-        roomList.selectItem(room);
-        expect(roomList.selected).to.eql(room);
-    });
-    it('should contain no more than one selected item at once', function () {
-        roomList.selectItem(anotherRoom);
-        expect(roomList.selected).to.be.an('object');
-        expect(roomList.selected).to.be.eql(anotherRoom);
-    });
-    it('should be able to order its items', function () {
-        roomList.order('slug');
-        expect(roomList.items[0]).to.be.eql(anotherRoom);
-    });
-});
+describe('ItemList', function() {
+    it('should be able to emit events', function() {
+        expect(roomList).to.be.a(Emitter)
+    })
+    it('should have an empty item list by default', function() {
+        expect(roomList.items).to.be.empty()
+    })
+    it('should have no selected item by default', function() {
+        expect(roomList.selected).to.be(null)
+    })
+    it('should be possible to redraw', function() {
+        expect(roomList.redraw).to.be.a('function')
+    })
+    it('should accept items', function() {
+        let rooms = [room, anotherRoom]
+        roomList.setItems(rooms)
+        expect(roomList.items).to.eql(rooms)
+    })
+    it('should offer a way to select items', function() {
+        roomList.selectItem(room)
+        expect(roomList.selected).to.eql(room)
+    })
+    it('should contain no more than one selected item at once', function() {
+        roomList.selectItem(anotherRoom)
+        expect(roomList.selected).to.be.an('object')
+        expect(roomList.selected).to.be.eql(anotherRoom)
+    })
+    it('should be able to order its items', function() {
+        roomList.order('slug')
+        expect(roomList.items[0]).to.be.eql(anotherRoom)
+    })
+})
