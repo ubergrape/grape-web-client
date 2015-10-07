c5ce84cdc838213dd76b7ac43d9d67d8835574ee
fey <federica.recanatini@gmail.com>
2015-10-07 16:08:21 +0200

    renamed and linted new org invite file

--- src/ui/elements/dialogs/OrgInvite.js
+++ src/ui/elements/dialogs/OrgInvite.js
@@ -1,50 +1,50 @@
-var Dialog = require('./dialog');
-var events = require('events');
-var qs = require('query');
-var _ = require('t');
+import Dialog from './dialog'
+import events from 'events'
+import qs from 'query'
+import _ from 't'
 
-module.exports = OrgInvite;
+export default OrgInvite
 
 function OrgInvite(context) {
-    this.template_path = 'dialogs/orginvite.jade';
-    Dialog.call(this, context);
-};
+    this.template_path = 'dialogs/orginvite.jade'
+    Dialog.call(this, context)
+}
 
-OrgInvite.prototype = Object.create(Dialog.prototype);
+OrgInvite.prototype = Object.create(Dialog.prototype)
 
-OrgInvite.prototype.bind = function () {
-    this.events = events(this.el, this);
-    this.events.bind('input .input-invite', 'resetValidity');
-    this.events.bind('submit .invite-to-org', 'inviteToOrg');
-};
+OrgInvite.prototype.bind = function() {
+    this.events = events(this.el, this)
+    this.events.bind('input .input-invite', 'resetValidity')
+    this.events.bind('submit .invite-to-org', 'inviteToOrg')
+}
 
-OrgInvite.prototype.inviteToOrg = function (e) {
-    e.preventDefault();
-    var inviteInput = qs('.input-invite', this.el);
-    var inviteButton = qs('.btn-invite', this.el);
-    this.resetValidity();
+OrgInvite.prototype.inviteToOrg = function(e) {
+    e.preventDefault()
+    let inviteInput = qs('.input-invite', this.el)
+    let inviteButton = qs('.btn-invite', this.el)
+    this.resetValidity()
     if (inviteInput.value === '') {
-        inviteInput.setCustomValidity(_('Please enter at least one user to invite'));
-        setTimeout(function () { inviteButton.click() });
-        return;
+        inviteInput.setCustomValidity(_('Please enter at least one user to invite'))
+        setTimeout(function() { inviteButton.click() })
+        return
     }
-    inviteButton.disabled = true;
-    this.emit('inviteToOrg', inviteInput.value);
-};
+    inviteButton.disabled = true
+    this.emit('inviteToOrg', inviteInput.value)
+}
 
-OrgInvite.prototype.onInviteError = function () {
-    var inviteInput = qs('.input-invite', this.el);
-    var inviteButton = qs('.btn-invite', this.el);
-    inviteInput.setCustomValidity(_('Enter valid email addresses separated by a space.'));
-    inviteButton.disabled = false;
-    setTimeout(function () { inviteButton.click() });
-};
+OrgInvite.prototype.onInviteError = function() {
+    let inviteInput = qs('.input-invite', this.el)
+    let inviteButton = qs('.btn-invite', this.el)
+    inviteInput.setCustomValidity(_('Enter valid email addresses separated by a space.'))
+    inviteButton.disabled = false
+    setTimeout(function() { inviteButton.click() })
+}
 
-OrgInvite.prototype.onInviteSuccess = function () {
+OrgInvite.prototype.onInviteSuccess = function() {
     this.dialog.hide()
-};
+}
 
-OrgInvite.prototype.resetValidity = function () {
-    var inviteInput = qs('.input-invite', this.el);
-    inviteInput.setCustomValidity('');
-};
+OrgInvite.prototype.resetValidity = function() {
+    let inviteInput = qs('.input-invite', this.el)
+    inviteInput.setCustomValidity('')
+}
