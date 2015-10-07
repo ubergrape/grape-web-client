25b1c0bd36779d0d2d4025a1846d24de98677a7c
fey <federica.recanatini@gmail.com>
2015-10-07 09:05:37 +0200

    linter on init-broker

--- src/init-broker/tests/index.js
+++ src/init-broker/tests/index.js
@@ -1,60 +1,61 @@
-var Emitter = require('emitter');
-var doBroker = require('../');
-var fixtures = require('../../../fixtures');
-var room = fixtures.room;
-var user = fixtures.user;
-var org = fixtures.org;
-var chatLine = fixtures.chatLine;
+let expect = require('expect.js')
+let Emitter = require('emitter')
+let doBroker = require('../')
+let fixtures = require('../../../fixtures')
+let room = fixtures.room
+let user = fixtures.user
+let org = fixtures.org
+let chatLine = fixtures.chatLine
 
-describe('Events Router', function () {
-    it('should route user change events', function () {
-        var ui = Emitter({
-            setUser: function (_user) {
-                expect(_user).to.eql(user);
+describe('Events Router', function() {
+    it('should route user change events', function() {
+        let ui = new Emitter({
+            setUser: function(_user) {
+                expect(_user).to.eql(user)
             }
-        });
-        var app = Emitter({});
-        doBroker(ui, app);
-        app.emit('changeUser', user);
-    });
-    it('should route organisation/s change events', function () {
-        var ui = Emitter({
-            setOrganizations: function (_orgs) {
-                expect(_orgs).to.be.eql(org);
+        })
+        let app = new Emitter({})
+        doBroker(ui, app)
+        app.emit('changeUser', user)
+    })
+    it('should route organisation/s change events', function() {
+        let ui = new Emitter({
+            setOrganizations: function(_orgs) {
+                expect(_orgs).to.be.eql(org)
             },
-            setOrganization: function (_org) {
+            setOrganization: function(_org) {
                 expect(_org).to.be.eql(org)
             }
-        });
-        var app = Emitter({});
-        doBroker(ui, app);
-        app.emit('change organizations', org);
-        app.emit('change organization', org);
-    });
-    it('should route room join events', function () {
-        var app = Emitter({
-            joinRoom: function (_room) {
-                expect(_room).to.eql(room);
-            }
-        });
-        var ui = Emitter({});
-        doBroker(ui, app);
-        ui.emit('joinRoom', room);
-    });
-    it('should route history requests', function () {
-        var ui = Emitter({
-            gotHistory: function (_room, lines) {
-                expect(_room).to.eql(room);
-                expect(lines[0]).to.eql(chatLine);
-            }
-        });
-        var app = Emitter({
-            getHistory: function (_room) {
-                expect(_room).to.eql(room);
-                this.emit('gotHistory', room, [chatLine]);
-            }
-        });
-        doBroker(ui, app);
-        ui.emit('needhistory', room);
+        })
+        let app = new Emitter({})
+        doBroker(ui, app)
+        app.emit('change organizations', org)
+        app.emit('change organization', org)
     })
-});
+    it('should route room join events', function() {
+        let app = new Emitter({
+            joinRoom: function(_room) {
+                expect(_room).to.eql(room)
+            }
+        })
+        let ui = new Emitter({})
+        doBroker(ui, app)
+        ui.emit('joinRoom', room)
+    })
+    it('should route history requests', function() {
+        let ui = new Emitter({
+            gotHistory: function(_room, lines) {
+                expect(_room).to.eql(room)
+                expect(lines[0]).to.eql(chatLine)
+            }
+        })
+        let app = new Emitter({
+            getHistory: function(_room) {
+                expect(_room).to.eql(room)
+                this.emit('gotHistory', room, [chatLine])
+            }
+        })
+        doBroker(ui, app)
+        ui.emit('needhistory', room)
+    })
+})
