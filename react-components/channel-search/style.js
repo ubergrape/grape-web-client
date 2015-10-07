9266efd3db63d4cc2abbaa2f15d7534210b6c048
Oleg Slobodskoi <oleg008@gmail.com>
2015-10-07 13:47:02 +0200

    first working version #2780

--- react-components/channel-search/style.js
+++ react-components/channel-search/style.js
@@ -1,2 +1,36 @@
+import button from '../button/style'
+import fonts from 'grape-theme/dist/fonts'
+
 export default {
+  content: {
+    padding: 15,
+    height: 300,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  input: {
+    width: '100%'
+  },
+  datalist: {
+    overflow: 'auto'
+  },
+  item: {
+    cursor: 'pointer',
+    '&:hover': {
+      background: 'blue'
+     }
+  },
+  fallback: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%'
+  },
+  fallbackHeadline: {
+    ...fonts.normal,
+    marginBottom: 20
+  },
+  button: button
 }
+
--- src/ui/elements/ChannelSearch.js
+++ src/ui/elements/ChannelSearch.js
@@ -1,4 +1,5 @@
 import Emitter from 'emitter'
+import page from 'page'
 
 import channelSearch from '../../../react-components/channel-search'
 
@@ -6,13 +7,60 @@ export default class ChannelSearch extends Emitter {
   constructor() {
     super()
     this.el = document.createElement('grape-channel-search')
+    this.state = {
+      show: false,
+      items: []
+    }
+    this.defaultProps = {
+      onSelect: ::this.onSelect,
+      onCreate: ::this.onCreate,
+      onShow: ::this.onShow,
+      onHide: ::this.onHide
+    }
   }
 
   redraw() {
+    this.el.props = {
+      ...this.defaultProps,
+      ...this.state
+    }
   }
 
   onOrgReady(org) {
-    this.org = org
+    this.state.items = getItems(org)
     this.redraw()
   }
+
+  onSelect(item) {
+    page('/chat/' + item.slug)
+    this.onHide()
+  }
+
+  onCreate() {
+    this.state.show = false
+    this.redraw()
+    this.emit('triggerRoomManager')
+  }
+
+  onShow() {
+    this.state.show = true
+    this.redraw()
+  }
+
+  onHide() {
+    this.state.show = false
+    this.redraw()
+  }
+}
+
+function getItems(org) {
+  let rooms = org.rooms.map(({id, name, slug}) => ({id, name, slug}))
+  let users = org.users.map(({id, displayName, username}) => {
+    return {
+      id,
+      name: displayName,
+      slug: `@${username}`
+    }
+  })
+  return [...rooms, ...users]
 }
