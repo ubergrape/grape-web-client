ba8ad4e9932b84fb72d8e7da5e4cd84fc84411f5
Oleg Slobodskoi <oleg008@gmail.com>
2015-10-27 15:56:04 +0100

    4 spaces -> 2 spaces

--- src/ui/elements/rightsidebar.js
+++ src/ui/elements/rightsidebar.js
@@ -2,87 +2,87 @@ import Emitter from 'emitter'
 import '../../../react-components/right-sidebar'
 
 export default class RightSidebar extends Emitter {
-    constructor() {
-        super()
-        this.el = document.createElement('grape-right-sidebar')
-        this.el.props = {
-            onToggle: ::this.onToggle,
-            hide: ::this.hide,
-            show: ::this.show,
-            toggleRoomInvite: ::this.toggleRoomInvite,
-            onShow: ::this.onShow,
-            kickMember: ::this.kickMember,
-            mode: null,
-            channel: null,
-            cUser: null,
-            searchItems: []
-        }
+  constructor() {
+    super()
+    this.el = document.createElement('grape-right-sidebar')
+    this.el.props = {
+      onToggle: ::this.onToggle,
+      hide: ::this.hide,
+      show: ::this.show,
+      toggleRoomInvite: ::this.toggleRoomInvite,
+      onShow: ::this.onShow,
+      kickMember: ::this.kickMember,
+      mode: null,
+      channel: null,
+      cUser: null,
+      searchItems: []
     }
+  }
 
-    setProps(props) {
-        this.el.props = {
-            ...this.el.props,
-            ...props
-        }
+  setProps(props) {
+    this.el.props = {
+      ...this.el.props,
+      ...props
     }
+  }
 
-    hide() {
-        this.setProps({
-            mode: null
-        })
-        this.emit('hide')
-    }
+  hide() {
+    this.setProps({
+      mode: null
+    })
+    this.emit('hide')
+  }
 
-    show(mode) {
-        this.setProps({
-            mode: mode,
-            channel: this.el.props.channel
-        })
-        this.emit('show')
-    }
+  show(mode) {
+    this.setProps({
+      mode: mode,
+      channel: this.el.props.channel
+    })
+    this.emit('show')
+  }
 
-    toggleRoomInvite() {
-        this.emit('toggleRoomInvite', this.el.props.channel)
-    }
+  toggleRoomInvite() {
+    this.emit('toggleRoomInvite', this.el.props.channel)
+  }
 
-    kickMember(ev) {
-        let channelID = this.el.props.channel.id
-        let userID = ev.target.getAttribute('data-id')
-        this.emit('kickMember', channelID, userID)
-    }
+  kickMember(ev) {
+    let channelID = this.el.props.channel.id
+    let userID = ev.target.getAttribute('data-id')
+    this.emit('kickMember', channelID, userID)
+  }
 
-    onToggle(mode) {
-        if (mode === this.el.props.mode) this.hide()
-        else this.show(mode); 
-    }
+  onToggle(mode) {
+    if (mode === this.el.props.mode) this.hide()
+    else this.show(mode);
+  }
 
-    onShow(mode) {
-        this.show(mode)
-    }
+  onShow(mode) {
+    this.show(mode)
+  }
 
-    onSelectChannel(channel) {
-        let mode = this.el.props.mode
-        if (mode === 'members' && channel.type === 'pm') {
-            mode = 'profile'
-        }
-        else if (mode === 'profile' && channel.type === 'room') {
-            mode = 'members'
-        }
-        this.setProps({
-            mode: mode,
-            channel: channel
-        })
+  onSelectChannel(channel) {
+    let mode = this.el.props.mode
+    if (mode === 'members' && channel.type === 'pm') {
+      mode = 'profile'
     }
+    else if (mode === 'profile' && channel.type === 'room') {
+      mode = 'members'
+    }
+    this.setProps({
+      mode: mode,
+      channel: channel
+    })
+  }
 
-    onGotSearchPayload(payload) {
-        this.setProps({
-            searchItems: payload.results
-        })
-    }
+  onGotSearchPayload(payload) {
+    this.setProps({
+      searchItems: payload.results
+    })
+  }
 
-    onSetUser(user) {
-        this.setProps({
-            cUser: user
-        })
-    }
+  onSetUser(user) {
+    this.setProps({
+      cUser: user
+    })
+  }
 }
