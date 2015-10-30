b1c87391a90b49a370b3e844e7f3ca2dbbb1c40c
Stefan Kröner <sk7@users.noreply.github.com>
2015-10-30 15:12:41 +0100

    remove semicolons

--- src/ui/elements/notifications.js
+++ src/ui/elements/notifications.js
@@ -36,72 +36,72 @@ Notifications.prototype.onNewInviteNotification = function Notification_onNewInv
   this.dispatch(title, content, icon, room)
 }
 Notifications.prototype.onNewMsgNotification = function Notifications_onNewMsgNotification (notif) {
-  let i, opts, title, content_dom, imgs, img, replacement, filename;
-  let content = notif.content;
-  let attachments = notif.attachments;
-  let hasExpired  = (new Date() - notif.time)/1000 > 60;
+  let i, opts, title, content_dom, imgs, img, replacement, filename
+  let content = notif.content
+  let attachments = notif.attachments
+  let hasExpired  = (new Date() - notif.time)/1000 > 60
 
   // don't notify if:
   // - message is too old - to prevent old msgs avalanche when server reloads or device resumes from standby
   // - chat is focused on the room the notification comes from
-  if ((notif.channel.id === this.room.id && document.hasFocus()) || hasExpired) return; 
-  
+  if ((notif.channel.id === this.room.id && document.hasFocus()) || hasExpired) return
+
   if (notif.channel.type === 'room') {
-    title = notif.author.name + ' (' + notif.channel.name + ')';
+    title = notif.author.name + ' (' + notif.channel.name + ')'
   } else {
-    title = notif.author.name + ' (' + _('Private message') + ')';
+    title = notif.author.name + ' (' + _('Private message') + ')'
   }
 
   // parse markdown
   if (content) {
     opts = {
       emoji: function (emo) {
-        emoji.init_colons();
+        emoji.init_colons()
         // we can't display custom emojis here because we only have them as images, they will automatically be displayed as :xyz:
-        let val = emoji.map.colons[emo];
-        return val ? emoji.data[val][0][0] : ':' + emo + ':';
+        let val = emoji.map.colons[emo]
+        return val ? emoji.data[val][0][0] : ':' + emo + ':'
       }
-    };
+    }
     // build dom from parsed markdown message
-    content_dom = domify(markdown(content, opts));
+    content_dom = domify(markdown(content, opts))
 
     // replace images
-    imgs = content_dom.getElementsByTagName('img');
-    replacement = document.createElement("p");
-    replacement.innerHTML = _('[Image]');
+    imgs = content_dom.getElementsByTagName('img')
+    replacement = document.createElement("p")
+    replacement.innerHTML = _('[Image]')
     for (i=0; i<imgs.length; i++) {
-      img = imgs[i];
-      img.parentElement.replaceChild(replacement, img);
+      img = imgs[i]
+      img.parentElement.replaceChild(replacement, img)
     }
 
     // strip html
-    content = content_dom.textContent || content_dom.innerText || "";
+    content = content_dom.textContent || content_dom.innerText || ""
   }
 
   // remove "[Image]" for service connections
-  if (notif.author.type === "service") content.replace("[Image]", "");
+  if (notif.author.type === "service") content.replace("[Image]", "")
 
   // attach files
   if (typeof attachments !== "undefined" && attachments.length > 0) {
     // currently the client doesn't supprt text content AND attachment
     // but the API supports it
-    if (typeof content !== "undefined" && content !== "") content += "\n\n";
+    if (typeof content !== "undefined" && content !== "") content += "\n\n"
     // add the filenames to the notification
     // currently the client only allows to add one attachment
     // but the API supports multiple
     for(i=0; i<attachments.length; i++) {
-      filename = attachments[i].name;
+      filename = attachments[i].name
       if (typeof filename !== "undefined" && filename !== "") {
-        content += filename;
+        content += filename
         if (i<attachments.length-1) {
-          content + "\n";
+          content + "\n"
         }
       }
     }
   }
 
-  this.dispatch(title, content, notif.author.icon_url, notif.channel);
-};
+  this.dispatch(title, content, notif.author.icon_url, notif.channel)
+}
 
 Notifications.prototype.dispatch = function Notifications_dispatch (title, content, icon, channel) {
   let self = this
