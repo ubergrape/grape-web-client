b8ae6dd151c1de1741b2f481f34feef5d8681f69
Oleg Slobodskoi <oleg008@gmail.com>
2015-11-10 12:51:49 +0100

    more file icons #2742

--- react-components/shared-files-file/icons/index.js
+++ react-components/shared-files-file/icons/index.js
@@ -1,2 +1,6 @@
+import document from './document.svg'
+export {document as document}
+export pdf from './pdf.svg'
+export html from './html.svg'
 export presentation from './presentation.svg'
-export default from './presentation.svg'
+export default document
