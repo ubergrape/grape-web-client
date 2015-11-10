a3b9835a1330df1160db8bab6b7089922e107913
Oleg Slobodskoi <oleg008@gmail.com>
2015-11-10 17:48:11 +0100

    add svg-icons component

---
+++ src/svg-icons/index.js
@@ -0,0 +1,6 @@
+import document from './document.svg'
+export {document as document}
+export pdf from './pdf.svg'
+export html from './html.svg'
+export presentation from './presentation.svg'
+export default document
