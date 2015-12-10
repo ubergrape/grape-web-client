424371f122538920a6683e8c7d891a66e3a5626b
Yuri Tkachenko <yuri@ya.ru>
2015-12-10 18:54:14 +0200

    Icon for private rooms for browser #2477

--- src/mentions/mentions.js
+++ src/mentions/mentions.js
@@ -4,7 +4,7 @@ import {jss} from 'grape-web/lib/jss'
 import Icon from '../icon/Icon'
 import style from './style'
 
-let sheet = jss.createStyleSheet(style).attach()
+const sheet = jss.createStyleSheet(style).attach()
 
 /**
  * Change data for representation.
@@ -12,15 +12,15 @@ let sheet = jss.createStyleSheet(style).attach()
 export function map(data) {
   data.forEach(item => {
     if (item.type === 'user') {
-      let iconStyle = {
+      const iconStyle = {
         backgroundImage: `url(${item.iconURL})`,
         borderRadius: '100%'
       }
       item.icon = <Icon name={item.name} style={iconStyle} />
       return
     }
-
-    item.icon = <Icon name={item.name} className={'fa fa-comments ' + sheet.classes.icon}/>
+    const icon = item.isPrivate ? 'lock' : 'comments'
+    item.icon = <Icon name={item.name} className={`fa fa-${icon} ${sheet.classes.icon}`}/>
   })
 
   return data
