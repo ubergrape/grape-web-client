adab5d81405674e2e0ebe8a336bb0c66e37ec1eb
Oleg Slobodskoi <oleg008@gmail.com>
2015-12-16 23:48:24 +0100

    use close icon #2975

--- react-components/sidebar-panel/style.js
+++ react-components/sidebar-panel/style.js
@@ -2,6 +2,8 @@ import colors from 'grape-theme/dist/base-colors'
 import fonts from 'grape-theme/dist/fonts'
 import utils from 'grape-jss-utils'
 
+import buttonIcon from '../button/icon'
+
 const padding = 20
 
 export default {
@@ -25,21 +27,16 @@ export default {
     alignSelf: 'center',
     // XXX
     color: '#666',
-    padding: '14px 20px'
+    padding: `14px ${padding}px`
   },
   body: {
-    padding,
+    padding: 20,
     background: colors.grayBlueLight,
     overflow: 'auto'
   },
   close: {
-    ...fonts.biggest,
-    padding: '0 20px',
-    border: 'none',
     // XXX
-    color: '#ABB0B9',
-    '&:before': {
-      content: '"×"'
-    }
+    ...buttonIcon('close', {color: '#ABB0B9', hoverColor: '#4098f2', iconOnly: true}),
+    padding: `0 ${padding}px`
   }
 }
