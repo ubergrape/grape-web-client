7ecdae0c818740973da91e27040aa323853b2cd6
Yuri Tkachenko <yuri@ya.ru>
2015-12-21 11:29:54 +0200

    ubergrape/chatgrape#3106: CR fix

--- webpack.examples.config.js
+++ webpack.examples.config.js
@@ -4,11 +4,10 @@ webpackConfig.entry.emoji = ['./src/emoji/index.js']
 webpackConfig.output.filename = '[name].js'
 webpackConfig.output.library = '[name]'
 webpackConfig.output.libraryTarget = 'var'
-webpackConfig.output.libraryTarget = 'var'
 webpackConfig.externals = {
-  'emoji': './src/emoji',
-  'React': 'react',
-  'ReactDOM': 'react-dom'
+  emoji: './src/emoji',
+  React: 'react',
+  ReactDOM: 'react-dom'
 }
 
 module.exports = exports = webpackConfig
