faade1bc191b912c92461ff0f0ad4932b5338244
Valentin Scholz <valentin.scholz@socialisten.at>
2016-01-12 12:45:45 +0100

    Introduces new theme colors

--- src/web-colors.js
+++ src/web-colors.js
@@ -2,23 +2,20 @@ import color from 'color'
 import colors from './base-colors'
 
 export default {
-  link: colors.aquaLight,
-  button: colors.aquaLight,
-  buttonBgDefault: '#503c50',
+  link: colors.blue,
+  button: colors.grayBlue,
+  buttonBgDefault: colors.blue,
   buttonColorDefault: colors.white,
-  buttonBgPrimary: colors.grassLight,
-  buttonColorPrimary: color(colors.grassDark).darken(0.5).rgbaString(),
+  buttonBgPrimary: colors.green,
+  buttonColorPrimary: colors.white,
   alertInfo: colors.aquaDark,
   alertSuccess: colors.grassDark,
   alertWarning: colors.gold,
   alertDanger: colors.bittersweetDark,
-  roomHeaderBackground: colors.grapeDark,
+  roomHeaderBackground: colors.blue,
   chatBackground: colors.white,
   chatContent: colors.silverLight,
   navigationBackground: colors.silverLight,
   organisationBackground: colors.grapeDark,
-  searchHighlightColor: '#ffeead',
-  sidebarBackground: '#503c50',
-  sidebarButtonBackground: '#6e4b6e',
-  sidebarGroupTitle: '#aa96aa'
+  searchHighlightColor: colors.yellow,
 }
