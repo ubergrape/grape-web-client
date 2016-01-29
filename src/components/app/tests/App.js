1bbe68a8a12e78ff22b1e466ba7d3b0748659b3a
Oleg Slobodskoi <oleg008@gmail.com>
2016-01-29 13:22:57 +0100

    rename some folders and components
    grape-input -> app
    highlighted-textarea -> grape-input
    
    ubergrape/chatgrape#3183

---
+++ src/components/app/tests/App.js
@@ -0,0 +1,108 @@
+import {$, render} from '../../test'
+import expect from 'expect.js'
+import React from 'react'
+import times from 'lodash/utility/times'
+import {Simulate} from 'react-addons-test-utils'
+import App from '../App'
+import data0 from './mocks/data0.json'
+
+describe('app:', () => {
+  describe('App()', () => {
+    it('should render without props', () => {
+      render(<App />)
+      expect($('grape-browser')).to.be.an(Element)
+    })
+  })
+
+  describe('App() with search', () => {
+    it('should open search browser', () => {
+      const input = <App browser="search" data={data0} focused setTrigger />
+      render(input)
+      expect($('search-browser', document.body)).to.be.an(Element)
+    })
+  })
+
+  describe('App() auto close', () => {
+    function create(onDidMount, onRender) {
+        // Results removed.
+      const data = {...data0, results: []}
+      const input = (
+        <App
+          browser="search"
+          data={data}
+          onDidMount={onDidMount}
+          focused/>
+      )
+      render(input, onRender)
+    }
+
+    it('shound render "nothing found"', done => {
+      create(null, () => {
+        expect($('search-browser empty', document.body)).to.be.an(Element)
+        done()
+      })
+    })
+
+    it('should close browser if there is space at the end and no results', (done) => {
+      create(component => {
+        component.query.set('search', 'something ', {silent: true})
+        create(null, () => {
+          const browser = $('search-browser', document.body)
+          expect(browser).to.be(null)
+          done()
+        })
+      })
+    })
+
+    it('should stay opened when space is not at the end', (done) => {
+      create(component => {
+        component.query.set('search', 'something else', {silent: true})
+        create(null, () => {
+          const browser = $('search-browser', document.body)
+          expect(browser).to.be.an(Element)
+          done()
+        })
+      })
+    })
+  })
+
+  describe('App() insert object', () => {
+    function insert(onInsertItem, onDidMount) {
+      const data = {...data0}
+      data.search.queries = []
+      const input = (
+        <App
+          browser="search"
+          data={data}
+          onInsertItem={onInsertItem}
+          onDidMount={onDidMount}
+          focused/>
+      )
+      render(input)
+
+      times(2, () => {
+        Simulate.keyDown($('search-browser input', document.body), {keyCode: 13})
+      })
+    }
+
+    it('should call onInsertItem with correct argument', (done) => {
+      insert(param => {
+        expect(param.type).to.be('file')
+        expect(param.service).to.be('googledrive')
+        expect(param.rank).to.be(1)
+        done()
+      })
+    })
+
+    it('should call replaceQuery with correct replacement', (done) => {
+      insert(null, input => {
+        input.replaceQuery = replacement => {
+          // Verify there are no missing params in objects.
+          expect(replacement.name).to.be('"Plans/Discussions"')
+          expect(replacement.content).to.be('#"Plans/Discussions"')
+          done()
+        }
+      })
+    })
+  })
+})
---
+++ src/components/app/utils.js
@@ -0,0 +1,44 @@
+import isEmpty from 'lodash/lang/isEmpty'
+import get from 'lodash/object/get'
+import {SEARCH_TRIGGER} from '../query/constants'
+
+/**
+ * Returns true if search is external.
+ */
+export function isExternalSearch(data) {
+  return get(data, 'search.type') === 'external'
+}
+
+/**
+ * Returns true if browser can be shown.
+ */
+export function canShowBrowser(prevState = {}, nextState) {
+  const {query, data, browser} = nextState
+
+  if (!nextState.browser) return false
+
+  if (nextState.isLoading) return true
+
+  const isClosed = !prevState.browser
+  const isSearch = browser === 'search'
+  const noResults = !data || isEmpty(data.results)
+  const hasSearch = query && query.search.length > 0
+  const closedBySpace = hasSearch && query.search[query.search.length - 1] === ' '
+
+  const isDataList = browser === 'emojiSuggest' || browser === 'user'
+  if (isDataList && isEmpty(data)) return false
+
+  if (isClosed && noResults && hasSearch) return false
+
+  if (isSearch && noResults && closedBySpace) return false
+
+  return true
+}
+
+/**
+ * Returns true if type will be rendered using grape-browser.
+ */
+export function isBrowserType(typeOrTrigger) {
+  return typeOrTrigger === SEARCH_TRIGGER ||
+    typeOrTrigger === 'search'
+}
