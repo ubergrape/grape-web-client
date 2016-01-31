e23c4ed5f25a61e97b14c393b239229df74a75e5
Oleg Slobodskoi <oleg008@gmail.com>
2016-01-31 03:16:54 +0100

    update to the latest jss version

--- src/jss/index.js
+++ src/jss/index.js
@@ -3,7 +3,7 @@ import {create} from 'jss'
 import reactJss from 'react-jss'
 import nested from 'jss-nested'
 import camelCase from 'jss-camel-case'
-import px from 'jss-px'
+import defaultUnit from 'jss-default-unit'
 import vendorPrefixer from 'jss-vendor-prefixer'
 
 export const jss = create()
@@ -11,9 +11,5 @@ export const useSheet = reactJss(jss)
 
 jss.use(nested())
 jss.use(camelCase())
-jss.use(px())
+jss.use(defaultUnit())
 jss.use(vendorPrefixer())
-
-if (__DEV__ || __TEST__) {
-  jss.use(require('jss-debug')())
-}
