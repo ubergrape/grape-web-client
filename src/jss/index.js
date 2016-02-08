454de184f2cd220d61bdf01356b8b01a74136d63
Oleg Slobodskoi <oleg008@gmail.com>
2016-02-08 13:55:39 +0100

    add extend plugin

--- src/jss/index.js
+++ src/jss/index.js
@@ -1,6 +1,7 @@
 // Setup jss plugins.
 import {create} from 'jss'
 import reactJss from 'react-jss'
+import extend from 'jss-extend'
 import nested from 'jss-nested'
 import camelCase from 'jss-camel-case'
 import defaultUnit from 'jss-default-unit'
@@ -9,6 +10,7 @@ import vendorPrefixer from 'jss-vendor-prefixer'
 export const jss = create()
 export const useSheet = reactJss(jss)
 
+jss.use(extend())
 jss.use(nested())
 jss.use(camelCase())
 jss.use(defaultUnit())
