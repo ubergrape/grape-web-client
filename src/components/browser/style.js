d824ad38df8ba42cfea95b0b14895ac746c81afd
Oleg Slobodskoi <oleg008@gmail.com>
2016-02-23 15:06:27 +0100

    refactor centering of search
    ubergrape/chatgrape#3286

--- src/components/browser/style.js
+++ src/components/browser/style.js
@@ -12,8 +12,6 @@ export default {
       '0 5px 11px 0 rgba(0, 0, 0, 0.18)',
       '0 4px 15px 0 rgba(0, 0, 0, 0.15)'
     ].join(','),
-    maxWidth: 800,
-    minWidth: 200,
     '& a': {
       color: webColors.link
     }
