ea47cd98f2f78194a141d3d19f0e64da43261c2a
Yuri Tkachenko <yuri@ya.ru>
2016-03-14 15:50:43 +0200

    cross platform notifications support ubergrape/chatgrape#3468

---
+++ src/x-platform/constants.js
@@ -0,0 +1,4 @@
+export const config = {
+  pageVisibility: true,
+  autoClose: 6000
+}
--- src/x-platform/index.js
+++ src/x-platform/index.js
@@ -1,14 +1,65 @@
+import 'html5-desktop-notifications'
+import config from './constants'
+import noop from 'lodash/utility/noop'
+
 let electron
-if (require) electron = require('electron')
+let remote
+if (window.require) {
+  electron = require('electron')
+  remote = require('remote')
+}
+
+const macGap = window.MacGap
+
+export function onElectron(event, callback) {
+  if (electron) {
+    electron.ipcRenderer.on(event, callback)
+    return true
+  }
+  return false
+}
+
+export function notificate({title, content, icon, slug}, callback = noop) {
+  if (macGap) {
+    macGap.notify({
+      title,
+      content,
+      sound: false
+    })
+    return
+  }
+
+  if (electron && !remote.getGlobal('isNotificationSupported')) {
+    electron.ipcRenderer.send(
+      'showNotification',
+      {
+        title,
+        slug,
+        message: content
+      }
+    )
+    return
+  }
+
+  const {notify} = window
+  notify.config(config)
+  const notification = notify.createNotification(title, {
+    body: content,
+    icon,
+    timeout: 6000,
+    onclick: () => {
+      callback()
+      window.focus()
+      notification.close()
+    }
+  })
+}
 
 /**
  * Open a url in browser and MacGap.
  */
 export function openUrl(url) {
-  if (window.MacGap) {
-    return window.MacGap.openURL(url)
-  }
-
+  if (macGap) return macGap.openURL(url)
   window.open(url)
 }
 
@@ -16,8 +67,8 @@ export function openUrl(url) {
  * Renders the doc icon badge.
  */
 export function addBadge(text) {
-  if (window.MacGap) {
-    window.MacGap.Dock.addBadge(text)
+  if (macGap) {
+    macGap.Dock.addBadge(text)
     return
   }
   if (electron) {
@@ -30,10 +81,12 @@ export function addBadge(text) {
  * Removes the doc icon badge.
  */
 export function removeBadge() {
-  if (window.MacGap) {
-    window.MacGap.Dock.removeBadge()
+  if (macGap) {
+    macGap.Dock.removeBadge()
+    return
   }
   if (electron) {
     electron.ipcRenderer.send('removeBadge')
+    return
   }
 }
