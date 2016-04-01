362b9f68f68f3424b5831427b17b85904b927ade
Oleg Slobodskoi <oleg008@gmail.com>
2016-04-01 12:50:35 +0200

    fix indentation #2863

--- karma.conf.js
+++ karma.conf.js
@@ -3,31 +3,31 @@ var webpackConfig = require('./webpack.config');
 var assign = require('lodash/object/assign');
 
 module.exports = function (config) {
-    config.set({
-        browsers: ['Chrome'],
-        singleRun: process.env.NODE_ENV !== 'test',
-        files: [
-            'tests.webpack.js'
-        ],
-        frameworks: ['mocha'],
-        plugins: [
-            'karma-chrome-launcher',
-            'karma-mocha',
-            'karma-sourcemap-loader',
-            'karma-webpack',
-        ],
-        preprocessors: {
-            'tests.webpack.js': ['webpack', 'sourcemap']
-        },
-        reporters: ['dots'],
-        webpack: assign(webpackConfig, {
-            devtool: 'inline-source-map'
-        }),
-        webpackServer: {
-            noInfo: true
-        },
-        webpackMiddleware: {
-            noInfo: true
-        }
-    });
+  config.set({
+    browsers: ['Chrome'],
+    singleRun: process.env.NODE_ENV !== 'test',
+    files: [
+      'tests.webpack.js'
+    ],
+    frameworks: ['mocha'],
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-mocha',
+      'karma-sourcemap-loader',
+      'karma-webpack',
+    ],
+    preprocessors: {
+      'tests.webpack.js': ['webpack', 'sourcemap']
+    },
+    reporters: ['dots'],
+    webpack: assign(webpackConfig, {
+      devtool: 'inline-source-map'
+    }),
+    webpackServer: {
+      noInfo: true
+    },
+    webpackMiddleware: {
+      noInfo: true
+    }
+  });
 };
