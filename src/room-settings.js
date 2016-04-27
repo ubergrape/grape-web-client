6761032b9df6fff3cac8b49c7bbd9bdb459527d1
Yuri Tkachenko <yuri@ya.ru>
2016-04-27 20:51:47 +0300

    CR renamings

--- src/room-settings.js
+++ src/room-settings.js
@@ -1,4 +1,17 @@
-export default [
+export const colors = [
+  '#ED8928',
+  '#0080FF',
+  '#97A6BD',
+  '#6FC936',
+  '#EA4C3A',
+  '#36BDBD',
+  '#6257D2',
+  '#E96038',
+  '#707782',
+  '#A16027'
+]
+
+export const icons = [
   'education',
   'chart',
   'science',
