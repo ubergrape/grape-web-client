2dd756a252294f31bf710be5a8ffbda34cececdf
Yuri Tkachenko <yuri@ya.ru>
2016-04-27 18:04:57 +0300

    moved room settings actions to separate file

--- src/actions/common.js
+++ src/actions/common.js
@@ -1,7 +1,6 @@
 import page from 'page'
 
 import * as types from '../constants/actionTypes'
-import {maxChannelDescriptionLength} from '../constants/app'
 import {
   normalizeChannelData,
   removeBrokenPms
@@ -235,105 +234,6 @@ export function handleConnectionError(err) {
     if (connection === 'lp' && err.status === 401) {
       dispatch(reloadOnAuthError())
     }
-  }
-}
-
-export function renameRoom(id, name) {
-  return dispatch => {
-    return api
-      .renameRoom(id, name)
-      .then(() => {
-        dispatch({
-          type: types.RENAME_ROOM_REQUEST,
-          payload: {
-            id,
-            name
-          }
-        })
-      })
-      .catch(({message}) => dispatch({
-        type: types.RENAME_ROOM_ERROR,
-        payload: message
-      }))
-  }
-}
-
-export function setRoomDescription(id, rawDescription) {
-  return dispatch => {
-    const description = rawDescription.slice(0, maxChannelDescriptionLength - 1)
-    return api
-      .setRoomDescription(id, description)
-      .then(() => {
-        dispatch({
-          type: types.SET_ROOM_DESCRIPTION,
-          payload: {
-            id,
-            description
-          }
-        })
-      })
-      .catch(err => dispatch(error(err)))
-  }
-}
-
-export function setRoomPrivacy(id, isPublic) {
-  return dispatch => {
-    return api
-      .setRoomPrivacy(id, isPublic)
-      .then(() => {
-        dispatch({
-          type: types.SET_ROOM_PRIVACY,
-          payload: {
-            id,
-            isPublic
-          }
-        })
-      })
-      .catch(err => dispatch(error(err)))
-  }
-}
-
-export function setRoomColor(id, color) {
-  return dispatch => {
-    return api
-      .setRoomColor(id, color)
-      .then(() => {
-        dispatch({
-          type: types.SET_ROOM_COLOR,
-          payload: {
-            id,
-            color
-          }
-        })
-      })
-      .catch(err => dispatch(error(err)))
-  }
-}
-
-export function setRoomIcon(id, icon) {
-  return dispatch => {
-    return api
-      .setRoomIcon(id, icon)
-      .then(() => {
-        dispatch({
-          type: types.SET_ROOM_ICON,
-          payload: {
-            id,
-            icon
-          }
-        })
-      })
-      .catch(err => dispatch(error(err)))
-  }
-}
-
-export function showRoomDeteteDialog(id) {
-  reduxEmitter.showRoomDeteteDialog()
-  return dispatch => {
-    dispatch({
-      type: types.SHOW_ROOM_DELETE_DIALOG,
-      payload: id
-    })
   }
 }
 
