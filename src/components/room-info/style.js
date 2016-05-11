cb053771f145cda3f51bb5fa0a6852df7ca2b084
Yuri Tkachenko <yuri@ya.ru>
2016-05-11 15:49:09 +0300

    hover border color for  icons and colors settings ubergrape/chatgrape#3105

--- src/components/room-info/style.js
+++ src/components/room-info/style.js
@@ -50,7 +50,10 @@ const chooserButton = {
   borderRadius: '50%',
   backgroundClip: 'content-box',
   border: '1px solid transparent',
-  boxShadow: `inset 0 0 0 2px ${colors.white}`
+  boxShadow: `inset 0 0 0 2px ${colors.white}`,
+  '&:hover': {
+    borderColor: webColors.link
+  }
 }
 
 export default {
