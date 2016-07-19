678dfd0bbbe5ab11795a95c426542a1baaff34ed
Oleg Slobodskoi <oleg008@gmail.com>
2016-07-19 14:09:45 +0200

    add comment for created pr in markdown-react-js repo
    ubergrape/chatgrape#3661

--- src/components/grapedown/GrapeUser.js
+++ src/components/grapedown/GrapeUser.js
@@ -3,6 +3,10 @@ import React, {PropTypes} from 'react'
 import {userSelector} from '../../selectors'
 import getStore from '../../app/store'
 
+// FIXME waiting for https://github.com/alexkuz/markdown-react-js/pull/10
+// Once it is merged, we can put this component back into GrapeObject and pass user
+// via props.
+
 /**
  * Returns true if passed path to a pm chat leads to logged in user.
  */
