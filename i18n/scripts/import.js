5ba6ca7d369475f59add7a9b88fe365c271a4810
Yuri Tkachenko <yuri@ya.ru>
2016-07-28 17:49:16 +0300

    fixed comment ubergrape/chatgrape#4003

--- i18n/scripts/import.js
+++ i18n/scripts/import.js
@@ -16,7 +16,7 @@ sync(messagesPattern).map((file) => {
   const output =
 `/* eslint-disable */
 /**
- * This is auto-generated file by './i18n/utils/import.js'.
+ * This is auto-generated file by 'i18n/scripts/import.js'.
  * Details at https://github.com/ubergrape/chatgrape/wiki/Web-client-i18n
  * DON'T EDIT!
  */
