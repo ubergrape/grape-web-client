a5f9e141f7212489ea4070f7fb50c03fa9d63d25
Yuri Tkachenko <yuri@ya.ru>
2016-07-29 18:07:06 +0300

    i18n export/import

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -1,9 +1,19 @@
 /* eslint-disable */
 /**
- * This is auto-generated file by './i18n/utils/import.js'.
+ * This is auto-generated file by 'i18n/scripts/import.js'.
  * Details at https://github.com/ubergrape/chatgrape/wiki/Web-client-i18n
  * DON'T EDIT!
  */
 export default {
-  "noDetailForItem": "No Detail Infos for this Item"
+  "NoEmojiFound": "No emoji found.",
+  "willNotifyRoom": "notify everyone in this room",
+  "wountNotifyRoom": "does not notify room members",
+  "notInRoom": "not in room",
+  "noDetailForItem": "No Detail Infos for this Item",
+  "searchInfo": "Search Wikipedia, Youtube, Giphy and {tools} by clicking {plusIcon} or pressing the plus key",
+  "yourBusinessTools": "your business tools",
+  "GrapeSearch": "Grape Search",
+  "NoResults": "No Results.",
+  "amountResults": "{resultsAmount} {resultsAmount, plural, one {Result} other {Results}}",
+  "No services found": "No services found"
 }
