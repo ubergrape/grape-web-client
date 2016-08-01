38a498b65a73ad9cce547540a8e48faa0d396ee6
Yuri Tkachenko <yuri@ya.ru>
2016-08-01 17:36:07 +0300

    use edited value when exporting legacy ubergrape/chatgrape#4003

--- i18n/scripts/legacyExport.js
+++ i18n/scripts/legacyExport.js
@@ -3,13 +3,15 @@ import {extractFromCode} from 'i18n-extract'
 
 const messagesSrc = './dist/app.js'
 const exportDir = './i18n/export/'
+const lastImport = JSON.parse(fs.readFileSync('./i18n/import/legacy.en.json', 'utf8'))
 
 const keys = extractFromCode(fs.readFileSync(messagesSrc, 'utf8'), {
   marker: '_'
 })
 
 const messages = keys.reduce((_messages, message) => {
-  _messages[message] = message
+  // use edited value from last import or create new from source
+  _messages[message] = lastImport[message] || message
   return _messages
 }, {})
 
