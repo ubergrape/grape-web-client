7e2b73109ca4846016a662e28a7a03f123a9499d
Oleg Slobodskoi <oleg008@gmail.com>
2016-08-04 12:07:05 +0200

    move provider to a separate dir

--- src/components/history/actionNames.js
+++
@@ -1,9 +0,0 @@
-export default {
-  loadHistory: 'onLoad',
-  renderOlderHistory: 'onTouchTopEdge',
-  unsetHistoryScrollTo: 'onUserScrollAfterScrollTo',
-  removeMessage: 'onRemove',
-  editMessage: 'onEdit',
-  resendMessage: 'onResend',
-  readMessage: 'onRead'
-}
