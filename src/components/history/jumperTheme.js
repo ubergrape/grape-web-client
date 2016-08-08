d3fc5e096ff4f6af319ef4e098f21bbb5a742b32
Yuri Tkachenko <yuri@ya.ru>
2016-08-08 15:31:58 +0300

    arrowDown design update ubergrape/chatgrape#4087

--- src/components/history/jumperTheme.js
+++ src/components/history/jumperTheme.js
@@ -1,14 +1,28 @@
 import buttonIcon from '../button/icon'
+import {gainsboroDark} from 'grape-theme/dist/base-colors'
 
+export const size = 50
+export const innerSpace = 5
+
+// we need even size so arrow will be centered
+const dimension = size + 1
+const contrast = 'rgba(255,255,255,0.75)'
 export const styles = {
   jumper: {
-    extend: buttonIcon('arrowDown'),
+    extend: buttonIcon('arrowDown', {color: gainsboroDark}),
     position: 'absolute',
-    fontSize: 50,
+    fontSize: size,
     bottom: 20,
     left: '50%',
-    marginLeft: -25,
-    background: 'none',
-    cursor: 'pointer'
+    width: dimension,
+    height: dimension,
+    padding: innerSpace,
+    transform: 'translateY(-50%)',
+    background: contrast,
+    border: `2px solid ${gainsboroDark}`,
+    borderRadius: '50%',
+    boxShadow: `0 0 0 3px ${contrast}`,
+    cursor: 'pointer',
+    boxSizing: 'content-box'
   }
 }
