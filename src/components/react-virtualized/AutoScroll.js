0058e4443a00025481eda56fc553e8960f78a274
Oleg Slobodskoi <oleg008@gmail.com>
2016-08-10 21:40:24 +0200

    refactor AutoScroll
    ubergrape/chatgrape#4099

--- src/components/react-virtualized/AutoScroll.js
+++ src/components/react-virtualized/AutoScroll.js
@@ -1,50 +1,36 @@
 import {Component, PropTypes} from 'react'
 import shallowCompare from 'react-addons-shallow-compare'
 
-// We use this number to ensure scroll position at the bottom without knowing
-// the actuall height of rows initially.
-const veryHeighScrollTop = 1000000000
-
 /**
- * Preserves the scroll position at the bottom when rows got added.
+ * Preserves the scroll position at the end when rows got added.
  */
 export default class AutoScroll extends Component {
   static propTypes = {
     rows: PropTypes.array.isRequired,
-    bottomThreshold: PropTypes.number.isRequired,
+    minEndThreshold: PropTypes.number.isRequired,
     children: PropTypes.func.isRequired,
-    rowHeight: PropTypes.func.isRequired,
     height: PropTypes.number.isRequired,
-    scrollTop: PropTypes.number.isRequired,
     scrollToIndex: PropTypes.number
   }
 
   static defaultProps = {
-    bottomThreshold: 20,
-    rows: [],
-    scrollTop: veryHeighScrollTop
+    minEndThreshold: 20,
+    rows: []
   }
 
   constructor(props) {
     super(props)
     this.direction = 0
-    this.childrenParam = {
-      onScroll: this.onScroll,
-      onRowsRendered: this.onRowsRendered,
-      scrollToIndex: props.scrollToIndex,
-      scrollTop: props.scrollToIndex === undefined ? props.scrollTop : undefined
-    }
   }
 
   componentWillReceiveProps(nextProps) {
-    // Once scrollToIndex is passed, we need to unset scrollTop.
     if (nextProps.scrollToIndex !== undefined) {
-      this.childrenParam.scrollToIndex = nextProps.scrollToIndex
-      this.childrenParam.scrollTop = undefined
+      this.scrollToIndex = nextProps.scrollToIndex
+      this.scrollToAlignment = 'center'
       return
     }
 
-    const {rowHeight, rows, height, bottomThreshold} = this.props
+    const {rows, height, minEndThreshold} = this.props
 
     if (nextProps.rows !== rows) {
       // We assume user scrolled up (reverse order) and we have loaded
@@ -53,18 +39,12 @@ export default class AutoScroll extends Component {
       // position, so we need to calculate the height of those rows and scroll
       // to the old position.
       if (this.direction < 0 && this.scrollTop === 0) {
-        const prevFirstRenderedRow = rows[this.renderedRows.startIndex]
+        const prevFirstRenderedRow = rows[this.startIndex]
         const prevFirstRowIndex = nextProps.rows.indexOf(prevFirstRenderedRow)
-        if (prevFirstRowIndex === -1) return
-
-        let addedHeight = 0
-        for (let index = 0; index < prevFirstRowIndex; index++) {
-          const rHeight = rowHeight({index})
-          if (rHeight) addedHeight += rHeight
+        if (prevFirstRowIndex !== -1) {
+          this.scrollToIndex = prevFirstRowIndex
+          this.scrollToAlignment = 'start'
         }
-
-        this.childrenParam.scrollToIndex = undefined
-        this.childrenParam.scrollTop = addedHeight
         return
       }
     }
@@ -73,10 +53,13 @@ export default class AutoScroll extends Component {
     // - Rows has been changed.
     // - Parent size has changed.
     if (nextProps.rows !== rows || nextProps.height !== height) {
-      // We are at the bottom within a threshold where we need to ensure last
+      const endThreshold = this.scrollHeight - this.scrollTop - this.clientHeight
+
+      // We are at the end within a threshold where we need to ensure last
       // message is always in view.
-      if (this.bottomThreshold < bottomThreshold) {
-        this.childrenParam.scrollToIndex = nextProps.rows.length - 1
+      if (endThreshold < minEndThreshold) {
+        this.scrollToIndex = nextProps.rows.length - 1
+        this.scrollToAlignment = 'end'
       }
     }
   }
@@ -86,20 +69,24 @@ export default class AutoScroll extends Component {
   }
 
   onScroll = ({scrollHeight, clientHeight, scrollTop}) => {
-    if (this.scrollTop !== undefined && this.scrollTop !== veryHeighScrollTop) {
+    if (this.scrollTop !== undefined) {
       this.direction = scrollTop - this.scrollTop
     }
     this.scrollTop = scrollTop
-    this.childrenParam.scrollTop = scrollTop
-    this.childrenParam.scrollToIndex = undefined
-    this.bottomThreshold = scrollHeight - scrollTop - clientHeight
+    this.scrollHeight = scrollHeight
+    this.clientHeight = clientHeight
   }
 
-  onRowsRendered = (params) => {
-    this.renderedRows = params
+  onRowsRendered = ({startIndex}) => {
+    this.startIndex = startIndex
   }
 
   render() {
-    return this.props.children(this.childrenParam)
+    return this.props.children({
+      onScroll: this.onScroll,
+      onRowsRendered: this.onRowsRendered,
+      scrollToIndex: this.scrollToIndex,
+      scrollToAlignment: this.scrollToAlignment
+    })
   }
 }
