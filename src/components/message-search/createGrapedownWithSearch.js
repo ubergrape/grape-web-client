0a0ce4f451842cd20c79a4195ce6f291203a02b4
Oleg Slobodskoi <oleg008@gmail.com>
2016-09-07 22:10:22 +0200

    fix messages search
    ubergrape/chatgrape#4245

--- src/components/message-search/createGrapedownWithSearch.js
+++ src/components/message-search/createGrapedownWithSearch.js
@@ -2,24 +2,52 @@ import React from 'react'
 import omit from 'lodash/object/omit'
 import findMatches from 'grape-web/lib/search/findMatches'
 
+import Highlight from '../highlight/YellowHighlight'
 import render from '../grapedown/render'
 import {renderTag} from '../grapedown/renderers'
 import {nonStandardProps} from '../grapedown/utils'
 
+const omitProps = [...nonStandardProps, 'href']
 
 export default function createGrapedownWithSearch(initialProps) {
-  const {query, renderHighlight, user} = initialProps
+  const {query, user} = initialProps
   const renderTagWithUser = (tag, props, children) => renderTag(tag, {...props, user}, children)
 
   const renderProps = {
     renderTag: (tag, props, children) => {
-      if (typeof children[0] !== 'string') {
+      if (typeof children[0] !== 'string' && children.length === 1) {
         return renderTagWithUser(tag, props, children)
       }
 
-      const matches = findMatches(children[0], query).filter(({found}) => found)
-      if (!matches.length) return renderTagWithUser(tag, props, children)
-      return <span {...omit(props, [...nonStandardProps, 'href'])}>{matches.map(renderHighlight)}</span>
+      const highlightedChildren = children.reduce((parts, child) => {
+        if (typeof child !== 'string') {
+          parts.push(child)
+          return parts
+        }
+        const matches = findMatches(child, query)
+        const foundMatches = matches.filter(({found}) => found)
+
+        if (!foundMatches.length) {
+          parts.push(renderTagWithUser(tag, {...props, key: `a-${parts.length}`}, [child]))
+          return parts
+        }
+
+        matches.forEach((match, key) => {
+          if (match.found) {
+            parts.push(<Highlight key={`b-${key}`}>{match.text}</Highlight>)
+            return
+          }
+          parts.push(match.text)
+        })
+
+        return parts
+      }, [])
+
+      return (
+        <span {...omit(props, omitProps)}>
+          {highlightedChildren}
+        </span>
+      )
     }
   }
 
