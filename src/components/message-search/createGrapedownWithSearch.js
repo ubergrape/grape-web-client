1b0ffc736b7c864150ea8ef84a470a099269c014
Oleg Slobodskoi <oleg008@gmail.com>
2016-09-07 22:21:26 +0200

    fix unique key
    ubergrape/chatgrape#4245

--- src/components/message-search/createGrapedownWithSearch.js
+++ src/components/message-search/createGrapedownWithSearch.js
@@ -10,13 +10,12 @@ import {nonStandardProps} from '../grapedown/utils'
 const omitProps = [...nonStandardProps, 'href']
 
 export default function createGrapedownWithSearch(initialProps) {
-  const {query, user} = initialProps
-  const renderTagWithUser = (tag, props, children) => renderTag(tag, {...props, user}, children)
+  const {query} = initialProps
 
   const renderProps = {
     renderTag: (tag, props, children) => {
       if (typeof children[0] !== 'string' && children.length === 1) {
-        return renderTagWithUser(tag, props, children)
+        return renderTag(tag, props, children)
       }
 
       const highlightedChildren = children.reduce((parts, child) => {
@@ -28,13 +27,13 @@ export default function createGrapedownWithSearch(initialProps) {
         const foundMatches = matches.filter(({found}) => found)
 
         if (!foundMatches.length) {
-          parts.push(renderTagWithUser(tag, {...props, key: `a-${parts.length}`}, [child]))
+          parts.push(renderTag(tag, {...props, key: `a-${parts.length}`}, [child]))
           return parts
         }
 
-        matches.forEach((match, key) => {
+        matches.forEach(match => {
           if (match.found) {
-            parts.push(<Highlight key={`b-${key}`}>{match.text}</Highlight>)
+            parts.push(<Highlight key={`b-${parts.length}`}>{match.text}</Highlight>)
             return
           }
           parts.push(match.text)
