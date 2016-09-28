a44ff3508a9ea0a93208c7c2826d7be0adad6717
Oleg Slobodskoi <oleg008@gmail.com>
2016-09-28 15:25:56 +0200

    use jss-isolate
    ubergrape/chatgrape#4037

--- src/jss/index.js
+++ src/jss/index.js
@@ -1,3 +1,12 @@
-import jss from 'jss'
+import injectSheet, {jss} from 'react-jss'
+import isolate from 'jss-isolate'
+
+jss.use(isolate({
+  reset: {
+    'font-family': '"proxima-nova", "Helvetica Neue", Arial, Helvetica, sans-serif',
+    'box-sizing': 'border-box'
+  }
+}))
+
 export {jss}
-export default from 'react-jss'
+export default injectSheet
