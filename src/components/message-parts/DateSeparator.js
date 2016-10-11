4b21b19f4a357c616816ded23c9d6aed7a85eb3b
Oleg Slobodskoi <oleg008@gmail.com>
2016-10-11 12:57:46 +0200

    localize date for date separator
    ubergrape/chatgrape#4292

--- src/components/message-parts/DateSeparator.js
+++ src/components/message-parts/DateSeparator.js
@@ -1,22 +1,62 @@
 import React, {Component, PropTypes} from 'react'
-import moment from 'moment'
 import injectSheet from 'grape-web/lib/jss'
 import shallowCompare from 'react-addons-shallow-compare'
+import {FormattedDate, FormattedMessage} from 'react-intl'
+import moment from 'moment'
 
 import {styles} from './dateSeparatorTheme'
+
+function DateWithDay({date}) {
+  const dateEl = (
+    <FormattedDate
+      value={date}
+      year="numeric"
+      month="short"
+      day="2-digit" />
+  )
+
+  const today = moment()
+  const isToday = moment(date).isSame(today, 'day')
+
+  if (isToday) {
+    return (
+      <FormattedMessage
+        id="dateSeparatorToday"
+        description="Date separator used in history, search, mentions etc."
+        defaultMessage="Today, {date}"
+        values={{date: dateEl}} />
+    )
+  }
+
+  const isYesterday = moment(date).isSame(today.subtract(1, 'day'), 'day')
+
+  if (isYesterday) {
+    return (
+      <FormattedMessage
+        id="dateSeparatorYesterday"
+        description="Date separator used in history, search, mentions etc."
+        defaultMessage="Yesterday, {date}"
+        values={{date: dateEl}} />
+    )
+  }
+
+  return dateEl
+}
+
+DateWithDay.propTypes = {
+  date: PropTypes.instanceOf(Date)
+}
 
 @injectSheet(styles)
 export default class DateSeparator extends Component {
   static propTypes = {
     sheet: PropTypes.object.isRequired,
     date: PropTypes.instanceOf(Date),
-    format: PropTypes.string,
     theme: PropTypes.object
   }
 
   static defaultProps = {
     date: new Date(),
-    format: 'MMM Do, YYYY',
     theme: {
       date: ''
     }
@@ -27,13 +67,12 @@ export default class DateSeparator extends Component {
   }
 
   render() {
-    const {theme, date, format} = this.props
-    const {classes} = this.props.sheet
+    const {theme, date, sheet: {classes}} = this.props
 
     return (
       <div className={classes.separator}>
-        <span className={`${classes.date} ${theme.date}`} >
-          {moment(date).format(format)}
+        <span className={`${classes.date} ${theme.date}`}>
+          <DateWithDay date={date} />
         </span>
       </div>
     )
