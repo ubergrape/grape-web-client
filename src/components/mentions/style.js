fc80a86d78ef67e285807c237dc6e4715c09dc91
Oleg Slobodskoi <oleg008@gmail.com>
2016-10-18 16:26:41 +0200

    fix room icons in mentions complete
    ubergrape/chatgrape#4530

--- src/components/mentions/style.js
+++ src/components/mentions/style.js
@@ -3,8 +3,9 @@ import colors from 'grape-theme/dist/base-colors'
 export default {
   icon: {
     '&:before': {
-      color: colors.gainsboroDark,
-      fontSize: '1.6em'
+      fontFamily: 'FontAwesome',
+      fontSize: '1.6em',
+      color: colors.gainsboroDark
     }
   }
 }
