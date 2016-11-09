bbacdc4900da74a91de266d35ae0dd0fcbf61bc3
Oleg Slobodskoi <oleg008@gmail.com>
2016-11-09 16:42:10 +0100

    fix margin/padding of grape-input
    ubergrape/chatgrape#4595

--- src/components/footer/theme.js
+++ src/components/footer/theme.js
@@ -3,6 +3,7 @@ import {borderDefault} from 'grape-theme/dist/web-colors'
 export const styles = {
   footer: {
     position: 'relative',
+    margin: '0 20px',
     borderTop: `1px solid ${borderDefault}`
   }
 }
