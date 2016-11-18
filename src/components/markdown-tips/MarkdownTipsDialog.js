fe860d334f6a6c69945febbabf66749a0dbb6e41
giuseppe <giuseppeg@users.noreply.github.com>
2016-11-18 15:37:41 +0100

    Add translations for the Markdown Tips dialog title

--- src/components/markdown-tips/MarkdownTipsDialog.js
+++ src/components/markdown-tips/MarkdownTipsDialog.js
@@ -1,26 +1,45 @@
 import React, {PropTypes, PureComponent} from 'react'
 import injectSheet from 'grape-web/lib/jss'
+import {
+  defineMessages,
+  intlShape,
+  injectIntl
+} from 'react-intl'
 
 import Dialog from '../dialog/Dialog'
 import MarkdownTips from './MarkdownTips'
 import {dialogStyles as styles} from './theme'
 
+const messages = defineMessages({
+  title: {
+    id: 'markdownTipsDialogTitle',
+    defineMessage: 'Markdown tips'
+  }
+})
+
 @injectSheet(styles)
+@injectIntl
 export default class MarkdownTipsDialog extends PureComponent {
   static propTypes = {
+    intl: intlShape.isRequired,
     onHide: PropTypes.func.isRequired,
     sheet: PropTypes.object.isRequired,
     show: PropTypes.bool.isRequired
   }
 
   render() {
-    const {sheet: {classes}, show, onHide} = this.props
+    const {
+      intl: {formatMessage},
+      sheet: {classes},
+      show,
+      onHide
+    } = this.props
 
     return (
       <Dialog
         show={show}
         onHide={onHide}
-        title="Markdown Tips">
+        title={formatMessage(messages.title)}>
         <div className={classes.wrapper}>
           <MarkdownTips />
         </div>
