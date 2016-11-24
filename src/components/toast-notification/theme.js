1879ffc66b88287772970bdf1919ae8f45f919eb
giuseppe <giuseppeg@users.noreply.github.com>
2016-11-24 16:32:12 +0100

    ToastNotification styles tweaks

--- src/components/toast-notification/theme.js
+++ src/components/toast-notification/theme.js
@@ -1,19 +1,23 @@
 import {purple, white} from 'grape-theme/dist/base-colors'
 import {normal} from 'grape-theme/dist/fonts'
 import {borderRadius} from 'grape-theme/dist/sizes'
+import {height} from '../header'
 
 const barStyles = {
-  extend: [normal],
   position: 'fixed',
-  top: '1rem',
+  top: height + 10,
   right: '-100%',
   bottom: 'auto',
   left: 'auto',
   width: 'auto',
-  padding: '1.5rem 1rem',
+  height: 13 * 2 + normal.fontSize,
+  padding: [13, 20],
   margin: 0,
+  boxSizing: 'border-box',
   color: white,
+  fontSize: normal.fontSize,
   fontFamily: 'inherit',
+  lineHeight: normal.fontSize,
   borderRadius: borderRadius.big,
   background: purple,
   boxShadow: {
@@ -24,13 +28,14 @@ const barStyles = {
     color: 'rgba(10, 10, 11, .125)'
   },
   transition: '0.5s cubic-bezier(0.89, 0.01, 0.5, 1.1)',
-  transform: 'translatez(0)'
+  transform: 'translatez(0)',
+
 }
 
 export const styles = {
   bar: barStyles,
   activeBar: {
-    extend: [barStyles],
+    extend: barStyles,
     right: '1rem'
   }
 }
