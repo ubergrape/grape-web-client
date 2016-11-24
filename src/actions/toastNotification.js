1bc1db523c948ab92b0aad9ed28de4dbb10284ab
giuseppe <giuseppeg@users.noreply.github.com>
2016-11-24 16:30:38 +0100

    Cleanup ToastNotification actions

--- src/actions/toastNotification.js
+++ src/actions/toastNotification.js
@@ -2,31 +2,23 @@ import random from 'lodash/number/random'
 
 import * as types from '../constants/actionTypes'
 
-export function hideToastNotification(key) {
-  return {
-    type: types.HIDE_TOAST_NOTIFICATION,
-    payload: {
-      key
-    }
-  }
-}
-
 export function showToastNotification(message, options = {}) {
   const key = random(1000)
-  // react-notification onClick is called with parameter deactivate,
-  // which is a function and can be called to set the notification to inactive.
-  // Used to activate notification exit animation on click.
-  // We also pass the unique notification `key`
-  // in case we need to dispatch a remove action.
-  const {onClick: _onClick} = options
-  const onClick = _onClick ? (deactivate) => _onClick(deactivate, key) : null
   return {
     type: types.SHOW_TOAST_NOTIFICATION,
     payload: {
       message,
       key,
-      ...options,
-      onClick
+      ...options
+    }
+  }
+}
+
+export function hideToastNotification(key) {
+  return {
+    type: types.HIDE_TOAST_NOTIFICATION,
+    payload: {
+      key
     }
   }
 }
