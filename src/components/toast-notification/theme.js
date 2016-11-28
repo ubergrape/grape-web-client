b0f226a96096250a21341fe25261c7b115f05d96
giuseppe <giuseppeg@users.noreply.github.com>
2016-11-28 17:38:37 +0100

    Toast Notification: styles adjustment

--- src/components/toast-notification/theme.js
+++ src/components/toast-notification/theme.js
@@ -1,9 +1,9 @@
-import {purple, white} from 'grape-theme/dist/base-colors'
+import {white} from 'grape-theme/dist/base-colors'
 import {normal} from 'grape-theme/dist/fonts'
 import {borderRadius} from 'grape-theme/dist/sizes'
 import {height} from '../header'
 
-export const verticalSpacing = 13
+export const verticalSpacing = 10
 
 const barStyles = {
   position: 'fixed',
@@ -12,31 +12,32 @@ const barStyles = {
   bottom: 'auto',
   left: 'auto',
   width: 'auto',
-  height: verticalSpacing * 2 + normal.fontSize,
-  padding: [verticalSpacing, 20],
+  minWidth: 250,
+  height: verticalSpacing*4 + normal.fontSize,
+  padding: verticalSpacing*2,
   margin: 0,
   boxSizing: 'border-box',
   color: white,
   fontSize: normal.fontSize,
   fontFamily: 'inherit',
   lineHeight: 1,
+  textAlign: 'center',
   borderRadius: borderRadius.big,
-  background: purple,
-  boxShadow: {
-    x: 0,
-    y: 0,
-    blur: 1,
-    spread: 1,
-    color: 'rgba(10, 10, 11, .125)'
+  border: {
+    width: 1,
+    style: 'solid',
+    color: '#ebeef3'
   },
+  background: '#6257d2',
+  boxShadow: 'none',
   transition: '0.5s cubic-bezier(0.89, 0.01, 0.5, 1.1)',
-  transform: 'translatez(0)'
+  transform: 'translatez(0)',
 }
 
 export const styles = {
   bar: barStyles,
   activeBar: {
     extend: barStyles,
-    right: '1rem'
+    right: verticalSpacing
   }
 }
