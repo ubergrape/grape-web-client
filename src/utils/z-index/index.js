8491298f1cad29ba7eaa23c6ea953bad5bcb3c52
giuseppe <giuseppeg@users.noreply.github.com>
2016-11-28 11:04:35 +0100

    z-index: one function

--- src/utils/z-index/index.js
+++ src/utils/z-index/index.js
@@ -1,20 +1,16 @@
 import map from './map'
 
-const getIndex = (layerName, relative) => {
+export const zIndex = (layerName, relative) => {
   const index = map[layerName]
   if (!index) throw new Error(`z-index: couldn't find "${layerName}"`)
   const increment = typeof relative === 'number' ? relative : 0
   return index + increment
 }
 
-export const zIndex = (layerName) => {
-  return getIndex(layerName)
-}
-
 export const above = (layerName) => {
-  return getIndex(layerName, 1)
+  return zIndex(layerName, 1)
 }
 
 export const below = (layerName) => {
-  return getIndex(layerName, -1)
+  return zIndex(layerName, -1)
 }
