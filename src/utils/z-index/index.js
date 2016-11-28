2c2d527f87cb78d4f06ef3755cfe182420a34c5b
giuseppe <giuseppeg@users.noreply.github.com>
2016-11-28 17:53:01 +0100

    z-index: warn when a layer doesn't exist

--- src/utils/z-index/index.js
+++ src/utils/z-index/index.js
@@ -1,8 +1,12 @@
+import warning from 'warning'
 import map from './map'
 
 export const zIndex = (layerName, relative) => {
   const index = map[layerName]
-  if (!index) throw new Error(`z-index: couldn't find "${layerName}"`)
+  warning(
+    !index,
+    `Could not find z-index layer ${layerName}`
+  )
   const increment = typeof relative === 'number' ? relative : 0
   return index + increment
 }
