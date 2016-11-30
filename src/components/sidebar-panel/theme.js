8a98691582053e81763c5ea3ce087fa5681d38a8
Giuseppe <giuseppeg@users.noreply.github.com>
2016-11-30 10:35:17 +0100

    z-index management util (#177)
    z-indices are stored in a map and can be retrieved with the `zIndex` helper function.
    The helper accepts a second and optional parameter `relative` that can be used to set indices relative to the give one. E.g. zIndex('modal') yields 10, zIndex('modal', 1) yields 11.

--- src/components/sidebar-panel/theme.js
+++ src/components/sidebar-panel/theme.js
@@ -3,6 +3,7 @@ import {ellipsis} from 'grape-web/lib/jss-utils/mixins'
 import {small, big} from 'grape-theme/dist/fonts'
 import colors from 'grape-theme/dist/base-colors'
 import webColors from 'grape-theme/dist/web-colors'
+import {zIndex} from '../../utils/z-index'
 
 import buttonIcon from '../button/icon'
 
@@ -22,7 +23,7 @@ const commonHeader = {
   borderBottom: `1px solid ${webColors.borderDefault}`,
   // Used to overlap absolutely positioned content e.g. loading indicator.
   position: 'relative',
-  zIndex: 1
+  zIndex: zIndex('base')
 }
 
 export const styles = {
---
+++ src/utils/z-index/index.js
@@ -0,0 +1,20 @@
+import warning from 'warning'
+import map from './map'
+
+export const zIndex = (layerName, relative) => {
+  const index = map[layerName]
+  warning(
+    !index,
+    `Could not find z-index layer ${layerName}`
+  )
+  const increment = typeof relative === 'number' ? relative : 0
+  return index + increment
+}
+
+export const above = (layerName) => {
+  return zIndex(layerName, 1)
+}
+
+export const below = (layerName) => {
+  return zIndex(layerName, -1)
+}
