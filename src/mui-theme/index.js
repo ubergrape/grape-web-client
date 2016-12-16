c3cda08a0f6231bfecef9480faba1b444f6bd9e0
Oleg Slobodskoi <oleg008@gmail.com>
2016-12-16 14:53:17 +0100

    progress background with animation

--- src/mui-theme/index.js
+++ src/mui-theme/index.js
@@ -11,6 +11,19 @@ export const LinearProgress = {
   root: {
     height: 9,
     borderRadius: 5
+  },
+  determinateBar1: {
+    backgroundImage: 'gradient(linear, 0 0, 100% 100%, color-stop(.25, rgba(255, 255, 255, .2)), color-stop(.25, rgba(0, 0, 0, 0)), color-stop(.5, rgba(0, 0, 0, 0)), color-stop(.5, rgba(255, 255, 255, .2)), color-stop(.75, rgba(255, 255, 255, .2)), color-stop(.75, rgba(0, 0, 0, 0)), to(rgba(0, 0, 0, 0)) )',
+    backgroundSize: [20, 20],
+    animation: 'determinate-1 2s linear infinite'
+  },
+  '@keyframes determinate-1': {
+    from: {
+      backgroundPosition: [0, 0]
+    },
+    to: {
+      backgroundPosition: ['20px', '20px']
+    }
   }
 }
 
