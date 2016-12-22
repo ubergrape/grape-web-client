352274c237b9796cad502fa7fddde936e3f15e2f
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2016-12-22 12:15:34 +0100

    Fix comment

--- src/components/grapedown/render.js
+++ src/components/grapedown/render.js
@@ -72,7 +72,7 @@ class Renderer {
   render(props) {
     this.props = props
     const text = props.text
-      // workaround to render multiple line breaks
+      // Workaround to render multiple line breaks.
       .split('\n\n')
       .join('\n\n&nbsp;\n\n')
     return this.renderer(text)
