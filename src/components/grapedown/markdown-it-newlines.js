ea2c80a7f34d7cac26cc8fcdf0d4d040027042f2
Chi Vinh Le <vinh@wikiwi.io>
2017-03-10 20:45:09 +0700

    Force new lines in all block elements

--- src/components/grapedown/markdown-it-newlines.js
+++ src/components/grapedown/markdown-it-newlines.js
@@ -1,30 +1,44 @@
 /* eslint-disable */
 
 /**
- * This plugin finds two consecutive paragraphs and adds
+ * This plugin finds two consecutive blocks and adds
  * a custom token `forcebreak` for every new line between
- * the two parapgrahs.
+ * the two blocks.
  */
 
+// Token starts a new block at root level.
+const isRootBlockInit = b => b.level === 0 && b.block && b.nesting >= 0
+
 module.exports = function (md) {
+  // Run plugin after blocks have been tokenized.
   md.core.ruler.after('block', 'forcebreak', plugin)
 
   function plugin(state) {
-    for (let i=0; i < state.tokens.length - 3; i++) {
+    for (let i=0; i < state.tokens.length; i++) {
+      if (!isRootBlockInit(state.tokens[i])) continue
+
+      // Found block init token at root level.
       const current = state.tokens[i]
-      if (current.type !== 'paragraph_open') continue
-      // skip 'inline' and 'paragraph_close'
-      const next = state.tokens[i+3]
-      if (next.type === 'paragraph_open') {
-        const lineEndCurrent = current.map[1]
-        const lineBeginNext = next.map[0]
-        const diff = lineBeginNext - lineEndCurrent
-        const token = new state.Token('forcebreak', 'br', 0);
-        token.attrs = [ [ 'forcebreak', true ] ]
-        state.tokens.splice(i + 3, 0, ...Array(diff).fill(token))
-        // next run i will point to next 'paragraph_open' token.
-        i += 2 + diff
+
+      // Find next block in root.
+      let j, next
+      for (j=i+1; j < state.tokens.length; j++) {
+        if (!isRootBlockInit(state.tokens[j])) continue
+        next = state.tokens[j]
+        break
       }
+      if (!next) break
+
+      // Create `forcebreak` tokens for each line between the two blocks.
+      const lineEndCurrent = current.map[1]
+      const lineBeginNext = next.map[0]
+      const diff = lineBeginNext - lineEndCurrent
+      const token = new state.Token('forcebreak', 'br', 0);
+      token.attrs = [ [ 'forcebreak', true ] ]
+      state.tokens.splice(j, 0, ...Array(diff).fill(token))
+
+      // next run i will point to next block token.
+      i = j + diff - 1
     }
   }
 }
