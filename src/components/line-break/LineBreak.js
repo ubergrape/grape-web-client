ebb99e14c92274737c8ababe1adcbcc7b9052df8
Chi Vinh Le <vinh@wikiwi.io>
2017-03-14 18:59:08 +0700

    Support clipboard across browsers

--- src/components/line-break/LineBreak.js
+++ src/components/line-break/LineBreak.js
@@ -1,15 +1,43 @@
 import React, {PureComponent, PropTypes} from 'react'
 import injectSheet from 'grape-web/lib/jss'
+import {normal} from 'grape-theme/dist/fonts'
+import {prefix} from 'css-vendor'
 
-import {styles} from './theme'
+const styles = {
+  lineBreak: {
+    display: 'block',
+    width: '100%',
+    height: normal.fontSize / 2,
+    cursor: 'text',
+    userSelect: 'none'
+  },
+  // hides text, but visible in clipboard.
+  copyOnly: {
+    display: 'inline-block',
+    verticalAlign: 'baseline',
+    width: '1px',
+    height: 0,
+    fontSize: 0,
+    color: 'transparent',
+    // Firefox needs `user-select: 'all'` otherwise
+    // newlines are not copied to clipboard.
+    userSelect: prefix.js === 'Moz' ? 'all' : 'none'
+  }
+}
 
 @injectSheet(styles)
 export default class LineBreak extends PureComponent {
   static propTypes = {
-    sheet: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired
   }
   render() {
-    const {sheet: {classes}} = this.props
-    return <span className={classes.lineBreak} role="presentation"><br /></span>
+    const {classes} = this.props
+    return (
+      <span className={classes.lineBreak} role="presentation">
+        <span className={classes.copyOnly}>
+          <br />
+        </span>
+      </span>
+    )
   }
 }
--- src/components/line-break/theme.js
+++
@@ -1,9 +0,0 @@
-import {normal} from 'grape-theme/dist/fonts'
-
-export const styles = {
-  lineBreak: {
-    display: 'block',
-    width: '100%',
-    height: normal.fontSize / 2
-  }
-}
