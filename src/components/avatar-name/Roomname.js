f720634d89b2989841756137f1a1b1fe197be1c9
Oleg Slobodskoi <oleg008@gmail.com>
2017-05-19 16:33:04 +0200

    fixes room name shortening within navigation
    ubergrape/grape-web-client#273

--- src/components/avatar-name/Roomname.js
+++ src/components/avatar-name/Roomname.js
@@ -7,22 +7,32 @@ import Icon from '../room-icon/RoomIcon'
 @injectSheet(style)
 export default class Roomname extends PureComponent {
   static propTypes = {
-    sheet: PropTypes.object.isRequired,
-    name: PropTypes.string.isRequired,
+    classes: PropTypes.object.isRequired,
+    name: PropTypes.string,
     icon: PropTypes.string,
-    color: PropTypes.string.isRequired,
-    statusBorderColor: PropTypes.string.isRequired,
-    isPublic: PropTypes.bool.isRequired,
-    showPrivateStatus: PropTypes.bool.isRequired
+    creatorUser: PropTypes.shape({
+      displayName: PropTypes.string.isRequired
+    }),
+    color: PropTypes.string,
+    statusBorderColor: PropTypes.string,
+    isPublic: PropTypes.bool,
+    showPrivateStatus: PropTypes.bool,
+    showRoomInfo: PropTypes.bool,
+    users: PropTypes.array
   }
 
   static defaultProps = {
+    name: '',
+    creatorUser: null,
+    icon: undefined,
     color: '#f00',
     statusBorderColor: '#fff',
     mentions: false,
     unread: 0,
     showPrivateStatus: false,
-    showRoomInfo: false
+    showRoomInfo: false,
+    isPublic: false,
+    users: []
   }
 
   render() {
@@ -30,7 +40,7 @@ export default class Roomname extends PureComponent {
       name, icon, color: backgroundColor,
       creatorUser, users, showRoomInfo,
       statusBorderColor, isPublic, showPrivateStatus,
-      sheet: {classes}
+      classes
     } = this.props
 
     return (
@@ -42,9 +52,10 @@ export default class Roomname extends PureComponent {
             backgroundColor
           }}
           showPrivateStatus={showPrivateStatus}
-          isPrivate={!isPublic} />
+          isPrivate={!isPublic}
+        />
         <div className={classes.name}>
-          <div>{name}</div>
+          {name}
           {showRoomInfo && (
             <ul className={classes.info}>
               <li>
