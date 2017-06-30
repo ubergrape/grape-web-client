60b161422ec05f519d10ab33480b9b2eebf9cbac
Oleg Slobodskoi <oleg008@gmail.com>
2017-06-30 18:41:04 +0200

    optimize react-virtualized imports #498

--- src/components/sidebar/labeled-messages/LabeledMessages.js
+++ src/components/sidebar/labeled-messages/LabeledMessages.js
@@ -6,7 +6,10 @@ import {
   injectIntl
 } from 'react-intl'
 import noop from 'lodash/utility/noop'
-import {List, AutoSizer, CellMeasurer, InfiniteLoader} from 'react-virtualized'
+import List from 'react-virtualized/dist/es/List'
+import AutoSizer from 'react-virtualized/dist/es/AutoSizer'
+import CellMeasurer from 'react-virtualized/dist/es/CellMeasurer'
+import InfiniteLoader from 'react-virtualized/dist/es/InfiniteLoader'
 import injectSheet from 'grape-web/lib/jss'
 
 import SidebarPanel from '../sidebar-panel/SidebarPanel'
