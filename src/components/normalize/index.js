f8f406c7d62c7f0164e19a16e520be07904dfb3e
Oleg Slobodskoi <oleg008@gmail.com>
2017-08-11 00:27:58 +0200

    pass style to normalize div

--- src/components/normalize/index.js
+++ src/components/normalize/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import normalize from 'normalize-jss'
-import reset  from 'reset-jss'
+import reset from 'reset-jss'
 import cn from 'classnames'
 
 import injectSheet from '../../jss'
@@ -11,8 +11,10 @@ const options = {
   increaseSpecificity: false
 }
 
-const Normalize = ({classes, children, className}) => (
-  <div className={cn(classes.normalize, classes.reset, className)}>{children}</div>
+const Normalize = ({classes, children, className, style}) => (
+  <div className={cn(classes.normalize, classes.reset, className)} style={style}>
+    {children}
+  </div>
 )
 
 export default injectSheet({normalize, reset}, options)(Normalize)
