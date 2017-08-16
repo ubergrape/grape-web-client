8d08c09a197ac90974158d82dda82eaf5b929bae
Oleg Slobodskoi <oleg008@gmail.com>
2017-08-16 15:08:10 +0200

    co-locate dialog styles

--- src/components/dialog/Dialog.js
+++ src/components/dialog/Dialog.js
@@ -4,8 +4,12 @@ import Normalize from 'grape-web/lib/components/normalize'
 import Modal from 'react-overlays/lib/Modal'
 import injectSheet from 'grape-web/lib/jss'
 import Button from 'material-ui/Button'
+import {black, white} from 'grape-theme/dist/base-colors'
+import {biggest} from 'grape-theme/dist/fonts'
+import {borderRadius} from 'grape-theme/dist/sizes'
+import {ellipsis} from 'grape-web/lib/jss-utils/mixins'
 
-import {styles} from './theme'
+import {zIndex} from '../../utils/z-index'
 
 /**
  * Dialog has
@@ -13,7 +17,64 @@ import {styles} from './theme'
  * - body
  * - positioned in the middle
  */
-@injectSheet(styles)
+@injectSheet({
+  overlay: {
+    position: 'fixed',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
+  modal: {
+    composes: '$overlay',
+    zIndex: zIndex('dialog'),
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflowY: 'auto'
+  },
+  backdrop: {
+    composes: '$overlay',
+    backgroundColor: black,
+    opacity: 0.3,
+    zIndex: zIndex('below')
+  },
+  content: {
+    width: 525,
+    borderRadius: borderRadius.big,
+    boxShadow: '0px 4px 10px -1px rgba(33,32,34,0.5)',
+    overflow: 'hidden',
+    outline: 0
+  },
+  header: {
+    display: 'flex',
+    background: white,
+    height: 50
+  },
+  close: {
+    font: {
+      size: 26,
+      weight: 'bold'
+    },
+    opacity: 0.5,
+    padding: [0, 20],
+    minWidth: 'auto',
+    '&:hover': {
+      opacity: 1,
+      background: 'none',
+      isolate: false
+    }
+  },
+  title: {
+    extend: [biggest, ellipsis],
+    flex: 2,
+    alignSelf: 'center',
+    paddingLeft: 20
+  },
+  body: {
+    background: white
+  }
+})
 export default class Dialog extends PureComponent {
   static propTypes = {
     onHide: PropTypes.func.isRequired,
