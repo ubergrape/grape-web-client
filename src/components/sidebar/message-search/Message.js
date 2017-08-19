0051ba2400d48f3e9162399f1e92e55201b130a5
Oleg Slobodskoi <oleg008@gmail.com>
2017-08-19 10:48:36 +0200

    fix messages overflow

--- src/components/sidebar/message-search/Message.js
+++ src/components/sidebar/message-search/Message.js
@@ -1,13 +1,65 @@
 import PropTypes from 'prop-types'
 import React, {PureComponent} from 'react'
 import injectSheet from 'grape-web/lib/jss'
+import {gainsboroLight, grayDark} from 'grape-theme/dist/base-colors'
+import omit from 'lodash/object/omit'
+import {icon as iconSize} from 'grape-theme/dist/sizes'
 
 import Avatar from '../../avatar/Avatar'
 import Header from '../../message-parts/Header'
 import Bubble from '../../message-parts/Bubble'
-import styles from './messageStyles'
+import {styles as linkStyles} from '../../message-parts/linkTheme'
 
-@injectSheet(styles)
+const arrowWidth = 7
+const marginRight = 10
+const shadowColor = 'rgba(0,0,0,0.3)'
+const transition = 'box-shadow 150ms ease-out'
+const link = omit(linkStyles.link, '&:hover')
+const linkHover = linkStyles.link['&:hover']
+
+@injectSheet({
+  message: {
+    cursor: 'pointer',
+    margin: [0, 0, 15]
+  },
+  body: {
+    display: 'flex'
+  },
+  header: {
+    paddingLeft: iconSize.l + marginRight + arrowWidth
+  },
+  leftColumn: {
+    flexShrink: 0,
+    marginRight
+  },
+  rightColumn: {
+    flex: 1,
+    width: '100%'
+  },
+  bubble: {
+    '&:hover:before': {
+      transition,
+      boxShadow: `-3px 4px 8px ${shadowColor}`
+    }
+  },
+  content: {
+    transition,
+    '&:hover': {
+      isolate: false,
+      boxShadow: `0px 1px 8px ${shadowColor}`
+    }
+  },
+  innerContent: {
+    overflow: 'hidden',
+    '& a': link,
+    '& a:hover': linkHover,
+    '& pre': {
+      display: 'block',
+      color: grayDark,
+      border: `1px solid ${gainsboroLight}`
+    }
+  }
+})
 export default class Message extends PureComponent {
   static propTypes = {
     classes: PropTypes.object.isRequired,
@@ -40,7 +92,9 @@ export default class Message extends PureComponent {
         <div className={classes.body}>
           <Avatar src={avatar} className={classes.leftColumn} />
           <Bubble className={classes.rightColumn} theme={{classes}}>
-            {children}
+            <div className={classes.innerContent}>
+              {children}
+            </div>
           </Bubble>
         </div>
       </section>
