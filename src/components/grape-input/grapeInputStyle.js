050ffbd0b3ce054fe9d830876545bf62e4492098
Oleg Slobodskoi <oleg008@gmail.com>
2017-09-05 12:04:47 +0200

    fix textarea styling after new isolation

--- src/components/grape-input/grapeInputStyle.js
+++ src/components/grape-input/grapeInputStyle.js
@@ -1,28 +1,25 @@
 import fonts from 'grape-theme/dist/fonts'
 
-const editableAndHighliter = {
-  ...fonts.normal,
-  minHeight: 38,
-  boxSizing: 'border-box',
-  padding: '14px 1px'
-}
-
 export default {
   container: {
     width: '100%',
     height: '100%',
     minHeight: 38
   },
+  highlighter: {
+    extend: fonts.normal,
+    minHeight: 38,
+    boxSizing: 'border-box',
+    padding: [14, 1],
+    overflow: 'hidden',
+    whiteSpace: 'pre-wrap',
+    wordWrap: 'break-word'
+  },
   editable: {
-    extend: editableAndHighliter,
+    composes: '$highlighter',
     outline: 'none',
     border: 'none',
     resize: 'none'
-  },
-  highlighter: {
-    extend: editableAndHighliter,
-    whiteSpace: 'pre-wrap',
-    wordWrap: 'break-word'
   },
   room: {
     background: 'linear-gradient(0deg, #e2c8f0, #e6d0f2)'
