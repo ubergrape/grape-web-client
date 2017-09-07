01fbf1f1384845cf57abf490d19dacb454f8f629
Oleg Slobodskoi <oleg008@gmail.com>
2017-09-07 14:09:47 +0200

    refactor org menu items

---
+++ src/components/org-info/MenuItem.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import injectSheet from 'grape-web/lib/jss'
+import Icon from 'grape-web/lib/svg-icons/Icon'
+import MuiMenuItem from 'material-ui/Menu/MenuItem'
+import {grayDarker} from 'grape-theme/dist/base-colors'
+import {small, normal} from 'grape-theme/dist/fonts'
+import {ellipsis} from 'grape-web/lib/jss-utils/mixins'
+
+const styles = {
+  root: {
+    display: 'block',
+    textDecoration: 'none',
+    '&:hover': {
+      isolate: false,
+      textDecoration: 'none'
+    }
+  },
+  icon: {
+    extend: normal,
+    marginRight: 10,
+    fill: grayDarker,
+    cursor: 'inherit',
+    flex: {
+      grow: 0,
+      basis: '10%'
+    }
+  },
+  text: {
+    extend: [small, ellipsis],
+    width: '100%',
+    flex: 1
+  }
+}
+
+const MenuItem = ({classes, icon, children, onClick, href, target}) => {
+  const item = (
+    <MuiMenuItem onClick={onClick} dense>
+      <Icon name={icon} className={classes.icon} />
+      <span className={classes.text}>{children}</span>
+    </MuiMenuItem>
+  )
+
+  if (href) {
+    return (
+      <a
+        href={href}
+        target={target}
+        className={classes.root}
+      >
+        {item}
+      </a>
+    )
+  }
+
+  return item
+}
+
+export default injectSheet(styles)(MenuItem)
