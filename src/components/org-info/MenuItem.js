f70a185d1bddd32880066c5d82955864e34c880b
Oleg Slobodskoi <oleg008@gmail.com>
2017-09-07 14:18:06 +0200

    refix org menu cursor

--- src/components/org-info/MenuItem.js
+++ src/components/org-info/MenuItem.js
@@ -3,31 +3,33 @@ import injectSheet from 'grape-web/lib/jss'
 import Icon from 'grape-web/lib/svg-icons/Icon'
 import MuiMenuItem from 'material-ui/Menu/MenuItem'
 import {grayDarker} from 'grape-theme/dist/base-colors'
-import {small, normal} from 'grape-theme/dist/fonts'
+import fonts from 'grape-theme/dist/fonts'
 import {ellipsis} from 'grape-web/lib/jss-utils/mixins'
 
 const styles = {
-  root: {
-    display: 'block',
-    textDecoration: 'none',
+  item: {
     '&:hover *': {
       isolate: false,
       textDecoration: 'none',
       cursor: 'pointer'
     }
   },
+  anchorItem: {
+    composes: '$item',
+    display: 'block',
+    textDecoration: 'none'
+  },
   icon: {
-    extend: normal,
+    extend: fonts.normal,
     marginRight: 10,
     fill: grayDarker,
-    cursor: 'inherit',
     flex: {
       grow: 0,
       basis: '10%'
     }
   },
   text: {
-    extend: [small, ellipsis],
+    extend: [fonts.small, ellipsis],
     width: '100%',
     flex: 1
   }
@@ -35,7 +37,7 @@ const styles = {
 
 const MenuItem = ({classes, icon, children, onClick, href, target}) => {
   const item = (
-    <MuiMenuItem onClick={onClick} dense>
+    <MuiMenuItem className={classes.item} onClick={onClick} dense>
       <Icon name={icon} className={classes.icon} />
       <span className={classes.text}>{children}</span>
     </MuiMenuItem>
@@ -46,7 +48,7 @@ const MenuItem = ({classes, icon, children, onClick, href, target}) => {
       <a
         href={href}
         target={target}
-        className={classes.root}
+        className={classes.anchorItem}
       >
         {item}
       </a>
