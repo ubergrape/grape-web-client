dea375a13ceae65aa8b10af5afe825fa859952cc
Oleg Slobodskoi <oleg008@gmail.com>
2017-10-06 21:22:03 +0200

    warn when staticUrl is called before staticPath was defined

--- src/utils/static-url/index.js
+++ src/utils/static-url/index.js
@@ -1,6 +1,9 @@
+import warning from 'warning'
+
 import conf from '../../conf'
 
 export default function staticUrl(url) {
   const path = __STATIC_PATH__ || conf.server.staticPath
+  warning(path, 'Static path is not defined %s.', url)
   return (path || '') + url
 }
