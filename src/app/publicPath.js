6045f8b3be4c86649e79fb360a35fafd35279fd6
Oleh Aloshkin <olegaleshkin@gmail.com>
2018-01-10 11:56:44 +0200

    Removed unnecessary cut of file name from conf.server.staticPath, added 'app' to end of url

--- src/app/publicPath.js
+++ src/app/publicPath.js
@@ -1,5 +1,4 @@
 import conf from '../conf'
 
-const staticPath = conf.server.staticPath
 // eslint-disable-next-line camelcase, no-undef
-__webpack_public_path__ = staticPath.substring(0, staticPath.lastIndexOf('/'))
+__webpack_public_path__ = `${conf.server.staticPath}app`
