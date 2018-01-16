fd6c076254bdb9cb07c635cbdf70e283f60392c6
Oleh Aloshkin <olegaleshkin@gmail.com>
2018-01-16 11:40:36 +0200

    Moved html5-notification import to polyfill file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import '../node_modules/html5-notification/dist/Notification.min.js'
 import './polyfills'
 import api from './api'
 
--- src/polyfills.js
+++ src/polyfills.js
@@ -1,2 +1,3 @@
 import 'custom-event-polyfill'
 import 'innersvg-polyfill'
+import '../node_modules/html5-notification/dist/Notification.min.js'
