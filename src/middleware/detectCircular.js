65ee41b478187bea3aeea242cd4114ef21a24f55
Oleg Slobodskoi <oleg008@gmail.com>
2018-01-22 15:03:54 +0100

    add circular actions detection middleware

---
+++ src/middleware/detectCircular.js
@@ -0,0 +1,9 @@
+import isCircular from 'is-circular'
+
+export default () => next => (action) => {
+  if (isCircular(action)) {
+    // eslint-disable-next-line no-console
+    console.warn('Circular action detected', action)
+  }
+  return next(action)
+}
