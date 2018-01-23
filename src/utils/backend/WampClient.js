87a8e2ffd5e5fa39d6e015b0ff9e6d53466f0c8d
Oleg Slobodskoi <oleg008@gmail.com>
2018-01-23 18:19:35 +0100

    log size of message recived over socket and log the time spent on each rpc call

--- src/utils/backend/WampClient.js
+++ src/utils/backend/WampClient.js
@@ -3,10 +3,12 @@ import Wamp from 'wamp1'
 import Backoff from 'backo'
 import debug from 'debug'
 import WebSocket from 'websocket-wrapper'
+import prettyBytes from 'pretty-bytes'
 
 const log = debug('ws')
 const prefix = 'http://domain/'
 const pingInterval = 10000
+const {time, timeEnd} = console
 
 export default class WampClient {
   constructor(options = {}) {
@@ -21,7 +23,7 @@ export default class WampClient {
     if (this.wamp) return this.out
     log('connect')
     this.open()
-    this.intervalId = setInterval(::this.ping, this.pingInterval)
+    this.intervalId = setInterval(this.ping, this.pingInterval)
     return this.out
   }
 
@@ -43,15 +45,16 @@ export default class WampClient {
 
   open() {
     this.socket = new WebSocket(this.url)
-    this.socket.on('error', ::this.onSocketError)
-    this.socket.on('close', ::this.onSocketClose)
+    this.socket.on('error', this.onSocketError)
+    this.socket.on('close', this.onSocketClose)
+    this.socket.on('message', this.onSocketMessage)
     this.wamp = new Wamp(
       this.socket,
       {omitSubscribe: true},
-      ::this.onOpen
+      this.onOpen
     )
-    this.wamp.on('error', ::this.onError)
-    this.wamp.on('event', ::this.onEvent)
+    this.wamp.on('error', this.onError)
+    this.wamp.on('event', this.onEvent)
   }
 
   close() {
@@ -77,7 +80,7 @@ export default class WampClient {
    * Ping is needed for server. Otherwise it doesn't know when to cleanupn the
    * session.
    */
-  ping() {
+  ping = () => {
     if (!this.connected || this.reopening) return
     if (this.waitingForPong) {
       this.waitingForPong = false
@@ -99,12 +102,25 @@ export default class WampClient {
    * Why do we need this again?
    */
   call(...args) {
-    const argsClone = [...args]
+    let callback = args.pop()
+
+    if (log.enabled) {
+      const originCallback = callback
+      const marker = `Call time ${args[0]}`
+      time(marker)
+      callback = (...callbackArgs) => {
+        timeEnd(marker)
+        originCallback(...callbackArgs)
+      }
+    }
+
+    const argsClone = [...args, callback]
     argsClone[0] = prefix + args[0]
+
     this.wamp.call(...argsClone)
   }
 
-  onOpen({sessionId}) {
+  onOpen = ({sessionId}) => {
     this.backoff.reset()
     this.onConnected()
     if (sessionId !== this.id) {
@@ -114,14 +130,14 @@ export default class WampClient {
     }
   }
 
-  onConnected() {
+  onConnected = () => {
     if (this.connected) return
     this.connected = true
     log('connected')
     this.out.emit('connected')
   }
 
-  onDisconnected() {
+  onDisconnected = () => {
     if (!this.connected) return
     this.id = null
     this.connected = false
@@ -133,28 +149,35 @@ export default class WampClient {
    * We get a url as event name. For compatibility with long polling clinet
    * we convert it to event name.
    */
-  onEvent(name, data) {
+  onEvent = (name, data) => {
     const event = name.split('/').pop().replace('#', '.')
     log('received event "%s"', event, data)
     this.out.emit('data', {...data, event})
   }
 
-  onError(err) {
+  onError = (err) => {
+    log(err)
     this.out.emit('error', err)
     this.close()
     this.reopen()
   }
 
-  onSocketError(event) {
+  onSocketError = (event) => {
     log('socket error', event)
     const err = new Error('Socket error.')
     err.event = event
     this.onError(err)
   }
 
-  onSocketClose(event) {
+  onSocketClose = (event) => {
     log('socket close', event)
     this.close()
     this.reopen()
+  }
+
+  onSocketMessage = (message) => {
+    if (log.enabled) {
+      log('socket message size', prettyBytes(message.length))
+    }
   }
 }
