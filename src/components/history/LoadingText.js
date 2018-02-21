e45bfa3a32cb0086df3bb4a99dcf940db802e97d
Oleh Aloshkin <olegaleshkin@gmail.com>
2018-02-21 15:51:29 +0200

    make LoadingText a functional component

--- src/components/history/LoadingText.js
+++ src/components/history/LoadingText.js
@@ -1,42 +1,40 @@
-import React, {PureComponent} from 'react'
+import React from 'react'
 import injectSheet from 'grape-web/lib/jss'
 import {spacer} from 'grape-theme/dist/sizes'
 import fonts from 'grape-theme/dist/fonts'
 
 import {Loading, Communication} from '../i18n'
 
-@injectSheet(({palette}) => ({
+const styles = ({palette}) => ({
   loading: {
     display: 'flex',
     flexDirection: 'column',
     padding: [spacer.xxl * 4, spacer.xl * 2, 0]
   },
   title: {
+    extend: fonts.big,
     textAlign: 'center',
     color: palette.grey[800],
-    extend: fonts.big,
     fontWeight: 600
   },
   text: {
+    extend: fonts.normal,
     textAlign: 'center',
     color: palette.grey[800],
     paddingTop: spacer.l,
-    extend: fonts.normal,
     lineHeight: '150%'
   }
-}))
-export default class LoadingText extends PureComponent {
-  render() {
-    const {classes} = this.props
-    return (
-      <div className={classes.loading}>
-        <span className={classes.title}>
-          <Loading />
-        </span>
-        <span className={classes.text}>
-          <Communication />
-        </span>
-      </div>
-    )
-  }
-}
+})
+
+const LoadingText = ({classes}) => (
+  <div className={classes.loading}>
+    <span className={classes.title}>
+      <Loading />
+    </span>
+    <span className={classes.text}>
+      <Communication />
+    </span>
+  </div>
+)
+
+export default injectSheet(styles)(LoadingText)
