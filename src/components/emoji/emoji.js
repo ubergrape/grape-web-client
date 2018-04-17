470d5f1be094bc83e6fb0c659e4c2dc89fddbe0b
Nik Graf <nik@nikgraf.com>
2018-04-17 15:42:17 +0200

    upgrade to latest forked emoji library

--- src/components/emoji/emoji.js
+++ src/components/emoji/emoji.js
@@ -1,35 +1,34 @@
-import EmojiConvertor from 'emoji-js'
+import EmojiConvertor from 'grape-js-emoji2'
 import {emojiSheet} from '../../constants/images'
 
 const emoji = new EmojiConvertor()
 
-// emoji.img_sets.apple.sheet = emojiSheet
+emoji.img_sets.apple.sheet = emojiSheet
+emoji.img_set = 'apple'
 emoji.use_sheet = true
-
-console.log('weeee')
 
 // https://github.com/ubergrape/chatgrape/issues/839
 // https://bugzilla.mozilla.org/show_bug.cgi?id=923007
 // if (navigator.userAgent.includes('Firefox') && navigator.platform === 'MacIntel') {
 //   emoji.allow_native = false
 // }
+emoji.allow_native = false
 
-// emoji.init_env()
+emoji.init_env()
 emoji.init_colons()
 
 export default emoji
 
 export function getEmojiSliceStyle(id) {
   const img = emoji.find_image(id)
-  const sheet_size = emoji.sheet_size * (img.sheet_size + 2) // size of image in pixels
-  console.log(sheet_size)
-  const sheet_x = 100 * (((img.px * (img.sheet_size + 2)) + 1) / (sheet_size - img.sheet_size))
-  const sheet_y = 100 * (((img.py * (img.sheet_size + 2)) + 1) / (sheet_size - img.sheet_size))
-  const sheet_sz = 100 * (sheet_size / img.sheet_size)
+  const sheetSize = emoji.sheet_size * (img.sheet_size + 2) // size of sprite sheet image in pixels
+  const sheetX = 100 * (((img.px * (img.sheet_size + 2)) + 1) / (sheetSize - img.sheet_size))
+  const sheetY = 100 * (((img.py * (img.sheet_size + 2)) + 1) / (sheetSize - img.sheet_size))
+  const sheetZoom = 100 * (sheetSize / img.sheet_size)
 
   return {
-    backgroundPosition: `${sheet_x}% ${sheet_y}%`,
-    backgroundSize: `${sheet_sz}% ${sheet_sz}%`,
+    backgroundPosition: `${sheetX}% ${sheetY}%`,
+    backgroundSize: `${sheetZoom}% ${sheetZoom}%`,
     backgroundImage: `url(${emojiSheet})`
   }
 }
