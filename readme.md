b94d2d753df063fded168579f3e4cd8076b16661
Glenn Reyes <glenn@glennreyes.com>
2018-06-14 17:51:35 +0200

    Run prettier

--- readme.md
+++ readme.md
@@ -20,5 +20,4 @@ npm run build
 - filter objects - quick filter selection objects displayed in the "queries" section
 - sidebar can contain info or detail view
 
-
 (c) UberGrape GmbH
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,40 @@
-var webpack = require('webpack')
-var path = require('path')
+const webpack = require('webpack')
+const path = require('path')
 
 module.exports = {
   entry: ['webpack/hot/dev-server', './src/index.js'],
   output: {
     path: './examples/build',
     publicPath: '/build/',
-    filename: 'grape-browser.js'
+    filename: 'grape-browser.js',
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
       __DEV__: process.env.NODE_ENV === 'development',
-      __TEST__: process.env.NODE_ENV === 'testing'
+      __TEST__: process.env.NODE_ENV === 'testing',
     }),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin(),
   ],
   module: {
     rules: [
       {
         loader: 'babel-loader',
         test: /\.js$/,
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         loader: 'json-loader',
-        test: /\.json$/
+        test: /\.json$/,
       },
       {
         test: /.svg$/,
-        loaders: [
-          'raw-loader'
-        ]
-      }
-    ]
+        loaders: ['raw-loader'],
+      },
+    ],
   },
   resolve: {
-    modules: ['node_modules', path.resolve(__dirname, 'node_modules')]
-  }
+    modules: ['node_modules', path.resolve(__dirname, 'node_modules')],
+  },
 }
