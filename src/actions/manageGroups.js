b62b9fba5785a9bff3f6c81af17cc5ec9a14391e
Oleh Aloshkin <olegaleshkin@gmail.com>
2018-06-18 21:56:51 +0300

    Unused type removed and prettier changes

--- src/actions/manageGroups.js
+++ src/actions/manageGroups.js
@@ -1,14 +1,14 @@
 import * as types from '../constants/actionTypes'
 import * as api from '../utils/backend/api'
-import {orgSelector, manageGroupsSelector} from '../selectors'
-import {error} from './'
+import { orgSelector, manageGroupsSelector } from '../selectors'
+import { error } from './'
 
-export const showManageGroups = () => (dispatch) => {
-  dispatch({type: types.SHOW_MANAGE_GROUPS})
+export const showManageGroups = () => dispatch => {
+  dispatch({ type: types.SHOW_MANAGE_GROUPS })
 }
 
-export const hideManageGroups = () => (dispatch) => {
-  dispatch({type: types.HIDE_MANAGE_GROUPS})
+export const hideManageGroups = () => dispatch => {
+  dispatch({ type: types.HIDE_MANAGE_GROUPS })
 }
 
 export const loadManageGroupsChannels = () => (dispatch, getState) => {
@@ -20,22 +20,26 @@ export const loadManageGroupsChannels = () => (dispatch, getState) => {
   const { page, activeFilter } = manageGroupsSelector(getState())
 
   return api
-    .getRooms(org.id, {membership: activeFilter === 'joined', page, pageSize: 50})
+    .getRooms(org.id, {
+      membership: activeFilter === 'joined',
+      page,
+      pageSize: 50,
+    })
     .then(({ results }) => {
       if (results) {
         dispatch({
           type: types.HANDLE_MANAGE_GROUPS_CHANNELS,
-          payload: results
+          payload: results,
         })
       }
     })
     .catch(err => dispatch(error(err)))
 }
 
-export const selectManageGroupsFilter = filter => (dispatch) => {
+export const selectManageGroupsFilter = filter => dispatch => {
   dispatch({
     type: types.SELECT_MANAGE_GROUPS_FILTER,
-    payload: filter
+    payload: filter,
   })
 
   dispatch(loadManageGroupsChannels(filter))
