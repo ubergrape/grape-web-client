213bae704e21a8d97e225a6f540a0a24fe8c7f9e
Nik Graf <nik@nikgraf.com>
2018-07-04 18:04:50 +0200

    refactor to extract special case and explain it in a comment

--- src/actions/pubSubHandlers.js
+++ src/actions/pubSubHandlers.js
@@ -70,10 +70,17 @@ export const handleNewMessage = message => (dispatch, getState) => {
   const state = getState()
   const channels = channelSelector(state)
   const user = userSelector(state)
+  // This is a special case for activity messages. These are special messages and the only
+  // one having the property type attached to it. It is showed in the
+  // "Development activities" channel and therefor it's not necessary to invoke addNewUser
+  // which would result in the undesired API call open_pm.
+  if (message.type) {
+    dispatch(addNewMessage(message))
+    return
+  }
   if (
     message.author.id === user.id ||
-    findIndex(channels, { id: message.author.id }) !== -1 ||
-    message.type
+    findIndex(channels, { id: message.author.id }) !== -1
   ) {
     dispatch(addNewMessage(message))
     return
