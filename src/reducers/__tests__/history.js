01ac271c186287d2be59a372fc080b7a3b7f99b0
Nik Graf <nik@nikgraf.com>
2018-07-20 11:24:22 +0200

    add more history tests

--- src/reducers/__tests__/history.js
+++ src/reducers/__tests__/history.js
@@ -1,5 +1,8 @@
 import history from '../history'
-import { UNSET_HISTORY_SCROLL_TO } from '../../constants/actionTypes'
+import {
+  ADD_NEW_MESSAGE,
+  UNSET_HISTORY_SCROLL_TO,
+} from '../../constants/actionTypes'
 
 describe('history reducer', () => {
   it('should handle UNSET_HISTORY_SCROLL_TO', () => {
@@ -8,6 +11,49 @@ describe('history reducer', () => {
         { scrollToAlignment: 'end', scrollTo: 'abc' },
         { type: UNSET_HISTORY_SCROLL_TO },
       ),
-    ).toEqual({ scrollToAlignment: null, scrollTo: null })
+    ).toMatchSnapshot()
+  })
+
+  describe('ADD_NEW_MESSAGE', () => {
+    it('should not update the state on a different channel', () => {
+      expect(
+        history(
+          { channel: { id: 'aaa' } },
+          { type: ADD_NEW_MESSAGE, payload: { channelId: 'bbb' } },
+        ),
+      ).toMatchSnapshot()
+    })
+
+    it('should set scrollTo if the author is the current user', () => {
+      expect(
+        history(
+          { user: { id: 'userId1' }, channel: { id: 'channelId1' } },
+          {
+            type: ADD_NEW_MESSAGE,
+            payload: {
+              id: 'msgId1',
+              author: { id: 'userId1' },
+              channel: { id: 'channelId1' },
+            },
+          },
+        ),
+      ).toMatchSnapshot()
+    })
+
+    it('should not set scrollTo if the author is a different user', () => {
+      expect(
+        history(
+          { user: { id: 'userId1' }, channel: { id: 'channelId1' } },
+          {
+            type: ADD_NEW_MESSAGE,
+            payload: {
+              id: 'msgId1',
+              author: { id: 'userId2' },
+              channel: { id: 'channelId1' },
+            },
+          },
+        ),
+      ).toMatchSnapshot()
+    })
   })
 })
