47f0a1c82c2fef369374fca10ffcf02c5969a7ec
Oleh Aloshkin <olegaleshkin@gmail.com>
2018-07-31 13:02:21 +0200

    Increase max bundle size

--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,98 @@
-var path = require('path')
-var webpack = require('webpack')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
-var CopyFilesPlugin = require('copy-webpack-plugin')
-var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-var UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-var DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+const CopyFilesPlugin = require('copy-webpack-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin')
 
-var NODE_ENV = process.env.NODE_ENV
-var STATIC_PATH = process.env.STATIC_PATH
-var APP = process.env.APP
-var isDevServer = process.argv[1].indexOf('webpack-dev-server') !== -1
-var ANALIZE = process.env.ANALIZE
+const { NODE_ENV, STATIC_PATH, APP, ANALIZE } = process.env
+const isDevServer = process.argv[1].indexOf('webpack-dev-server') !== -1
 
-var plugins = [
-  new CopyFilesPlugin([{
-    from: './src/images',
-    to: './images'
-  }]),
-  new CopyFilesPlugin([{
-    from: './src/fonts',
-    to: './fonts'
-  }]),
-  new CopyFilesPlugin([{
-    from: './src/sounds',
-    to: './sounds'
-  }]),
+const plugins = [
+  new CopyFilesPlugin([
+    {
+      from: './src/images',
+      to: './images',
+    },
+  ]),
+  new CopyFilesPlugin([
+    {
+      from: './src/fonts',
+      to: './fonts',
+    },
+  ]),
+  new CopyFilesPlugin([
+    {
+      from: './src/sounds',
+      to: './sounds',
+    },
+  ]),
   new webpack.DefinePlugin({
     __DEV__: !NODE_ENV || NODE_ENV === 'development',
     __TEST__: NODE_ENV === 'test',
     __STATIC_PATH__: JSON.stringify(STATIC_PATH),
-    'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
+    'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
   }),
-  new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en|de/),
-  new DuplicatePackageCheckerPlugin()
+  new webpack.ContextReplacementPlugin(/moment[\\]locale$/, /en|de/),
+  new DuplicatePackageCheckerPlugin(),
 ]
 
-module.exports = exports = {
+const exports = {
   entry: () => {
-    var app = ['babel-polyfill', './src/index.js']
-    var embedded = ['babel-polyfill', './src/embedded.js']
-    if (APP === 'full') return {app}
-    if (APP === 'embedded') return {embedded}
-    return {app, embedded}
+    const app = ['babel-polyfill', './src/index.js']
+    const embedded = ['babel-polyfill', './src/embedded.js']
+    if (APP === 'full') return { app }
+    if (APP === 'embedded') return { embedded }
+    return { app, embedded }
   },
   output: {
     path: path.resolve(__dirname, 'dist/app'),
     filename: '[name].js',
-    library: 'grapeClient'
+    library: 'grapeClient',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [
-          'src',
-          'node_modules/pretty-bytes'
-        ].map(dir =>  path.resolve(__dirname, dir))
+        include: ['src', 'node_modules/pretty-bytes'].map(dir =>
+          path.resolve(__dirname, dir),
+        ),
       },
       {
         test: /.svg$/,
-        loader: 'raw-loader'
+        loader: 'raw-loader',
       },
       {
         test: /\.(png|jpg|gif)$/,
-        loader: 'url-loader?limit=8192'
+        loader: 'url-loader?limit=8192',
       },
       {
         test: /\.html$/,
-        loader: 'html-loader'
-      }
-    ]
+        loader: 'html-loader',
+      },
+    ],
   },
-  plugins: plugins,
+  plugins,
   resolve: {
     alias: {
-      'emitter': 'component-emitter'
+      emitter: 'component-emitter',
     },
     // Workaround for simlinked dependencies.
     // This will help to find dependency in the parent package if missing in a
     // symlinked one.
     // http://webpack.github.io/docs/troubleshooting.html
     // https://github.com/webpack/webpack/issues/784
-    modules: [path.resolve(__dirname, 'node_modules'), 'web_modules', 'node_modules']
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+      'web_modules',
+      'node_modules',
+    ],
   },
-  devtool: NODE_ENV === 'production' ? 'source-map' : 'cheap-source-map'
+  devtool: NODE_ENV === 'production' ? 'source-map' : 'cheap-source-map',
 }
+
+module.exports = exports
 
 if (isDevServer) {
   exports.output.publicPath = '/static/app/'
@@ -97,14 +104,14 @@ if (NODE_ENV === 'production') {
     // https://github.com/webpack/webpack/issues/283
     new UglifyJsPlugin({
       compress: {
-        warnings: false
-      }
-    })
+        warnings: false,
+      },
+    }),
   )
   exports.performance = {
-    hints: "error",
-    maxEntrypointSize: 2650 * 1024,
-    maxAssetSize: 2650 * 1024
+    hints: 'error',
+    maxEntrypointSize: 2850 * 1024,
+    maxAssetSize: 2650 * 1024,
   }
 }
 
