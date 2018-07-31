cfcdc6b1067128cfbf46b2f715b5211dcf461d45
Nik Graf <nik@nikgraf.com>
2018-07-31 16:11:41 +0200

    make sure LinkAttachments are rendered as blocks

--- src/components/message-parts/attachments/LinkAttachment.js
+++ src/components/message-parts/attachments/LinkAttachment.js
@@ -4,10 +4,14 @@ import React from 'react'
 import LinkWithIcon from '../LinkWithIcon'
 
 export default function LinkAttachment({ category, children, ...rest }) {
+  // LinkWithIcon is used in other placed. That's why we use a wrapper div
+  // for the LinkAttachment. It ensures this has display: block
   return (
-    <LinkWithIcon {...rest} icon={category} target="_blank">
-      {children}
-    </LinkWithIcon>
+    <div>
+      <LinkWithIcon {...rest} icon={category} target="_blank">
+        {children}
+      </LinkWithIcon>
+    </div>
   )
 }
 
