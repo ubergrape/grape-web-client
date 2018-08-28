db020ccf9428116d8d389bc96fdb810a0973d90d
Oleh Aloshkin <olegaleshkin@gmail.com>
2018-08-28 13:53:52 +0200

    Migrated to snapshot testing for channels reducers, added one more test

--- src/reducers/__tests__/channels.js
+++ src/reducers/__tests__/channels.js
@@ -1,37 +1,57 @@
 import channels from '../channels'
-import * as types from '../../constants/actionTypes'
+import {
+  SET_CHANNELS,
+  UPDATE_CHANNEL_UNREAD_COUNTER,
+} from '../../constants/actionTypes'
 
-import { c1, c2, c3, c4 } from './data/channels'
+import { c1, c2, c3 } from './data/channels'
 
 describe('channels reducer', () => {
   it('should handle SET_CHANNELS', () => {
     expect(
       channels([], {
-        type: types.SET_CHANNELS,
+        type: SET_CHANNELS,
         payload: [c1, c2],
       }),
-    ).toEqual([c1, c2])
+    ).toMatchSnapshot()
   })
 
   it('should handle correct merge channels on SET_CHANNELS', () => {
     expect(
       channels([c1, c2], {
-        type: types.SET_CHANNELS,
+        type: SET_CHANNELS,
         payload: [c1, c3],
       }),
-    ).toEqual([c2, c1, c3])
+    ).toMatchSnapshot()
   })
 
   it('should handle correct update unread field on UPDATE_CHANNEL_UNREAD_COUNTER', () => {
     expect(
-      channels([c4], {
-        type: types.UPDATE_CHANNEL_UNREAD_COUNTER,
+      channels([{ unread: 1, latestMessageTime: 1508772967000 }], {
+        type: UPDATE_CHANNEL_UNREAD_COUNTER,
         payload: {
           id: 3339,
           unread: 0,
           time: 'Mon Oct 23 2017 17:36:08 GMT+0200',
         },
       }),
-    ).toEqual([c2])
+    ).toMatchSnapshot()
+  })
+
+  it('should handle correct updating channel with current field on UPDATE_CHANNEL_UNREAD_COUNTER', () => {
+    expect(
+      channels(
+        [{ unread: 1, current: true, latestMessageTime: 1508772967000 }],
+        {
+          type: UPDATE_CHANNEL_UNREAD_COUNTER,
+          payload: {
+            id: 3339,
+            unread: 1,
+            current: true,
+            time: 'Mon Oct 23 2017 17:36:08 GMT+0200',
+          },
+        },
+      ),
+    ).toMatchSnapshot()
   })
 })
--- src/reducers/__tests__/data/channels.js
+++ src/reducers/__tests__/data/channels.js
@@ -48,25 +48,3 @@ export const c3 = {
     'https://staging.chatgrape.com/imgsrc/m/cebabcff380f42ea8fd2395d86dbc4a0/200x200/',
   joined: true,
 }
-
-export const c4 = {
-  id: 3339,
-  type: 'room',
-  creator: 2674,
-  created: 1498568936,
-  unread: 1,
-  mentioned: 0,
-  firstMessageTime: 1498568939879,
-  latestMessageTime: 1508772968000,
-  users: [2404],
-  invites: [],
-  name: 'team testing',
-  description: '',
-  slug: 'team-testing',
-  isPublic: true,
-  abbr: 'TT',
-  color: '#EA4C3A',
-  isManaged: false,
-  icon: 'dinner',
-  joined: false,
-}
