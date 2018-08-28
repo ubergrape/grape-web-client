5aec374748c5bfdfa33631052d82d90b5ebbac28
Oleh Aloshkin <olegaleshkin@gmail.com>
2018-08-28 14:48:40 +0200

    On yarn theme:import download all files from design-system repo

--- theo-import.js
+++ theo-import.js
@@ -1,34 +1,36 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-const theo = require('theo')
+const theo = require('theo') // eslint-disable-line import/no-extraneous-dependencies
 const fs = require('fs')
 const camelCase = require('lodash/camelCase')
 const mapKeys = require('lodash/mapKeys')
+const download = require('download-git-repo') // eslint-disable-line import/no-extraneous-dependencies
 
-theo.registerTransform('web', ['color/hex'])
-
-theo
-  .convert({
-    transform: {
-      type: 'web',
-      file: './node_modules/design-system/design-tokens/grape-skin.yml',
-    },
-    format: {
-      type: 'json',
-    },
-  })
-  .then(json => {
-    const theme = {}
-    mapKeys(JSON.parse(json), (v, k) => {
-      theme[camelCase(k)] = v
+download('ubergrape/design-system', 'design-system', err => {
+  if (err) throw err
+  theo.registerTransform('web', ['color/hex'])
+  theo
+    .convert({
+      transform: {
+        type: 'web',
+        file: './design-system/design-tokens/grape-skin.yml',
+      },
+      format: {
+        type: 'json',
+      },
     })
-    fs.writeFile(
-      './src/constants/theme.json',
-      JSON.stringify(theme, null, ' '),
-      'utf8',
-      () => {},
-    )
-  })
-  .catch(error => {
-    // eslint-disable-next-line no-console
-    console.log(`Something went wrong: ${error}`)
-  })
+    .then(json => {
+      const theme = {}
+      mapKeys(JSON.parse(json), (v, k) => {
+        theme[camelCase(k)] = v
+      })
+      fs.writeFile(
+        './src/constants/theme.json',
+        JSON.stringify(theme, null, ' '),
+        'utf8',
+        () => {},
+      )
+    })
+    .catch(error => {
+      // eslint-disable-next-line no-console
+      console.log(`Error: ${error}`)
+    })
+})
