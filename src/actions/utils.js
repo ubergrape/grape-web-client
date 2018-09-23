d89598eb33dceae3dd8523f25f576bb7d0ac49e5
Oleh Aloshkin <olegaleshkin@gmail.com>
2018-09-23 23:28:53 +0200

    Fix for favorited channels

--- src/actions/utils.js
+++ src/actions/utils.js
@@ -29,7 +29,7 @@ export function pinToFavorite(channel) {
   const { pin } = channel
   const newChannel = {
     ...channel,
-    favorited: pin ? { order: pin } : null,
+    favorited: pin || pin === 0 ? { order: pin } : null,
   }
   delete newChannel.pin
   return newChannel
