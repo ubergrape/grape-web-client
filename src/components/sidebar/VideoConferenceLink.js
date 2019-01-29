0a4754f7720c6c699c51692d152e1a5d38032707
Oleh Aloshkin <olegaleshkin@gmail.com>
2019-01-29 22:21:27 +0100

    Removed unnecessary check for Electron for video chat button

--- src/components/sidebar/VideoConferenceLink.js
+++ src/components/sidebar/VideoConferenceLink.js
@@ -3,8 +3,6 @@ import { FormattedMessage } from 'react-intl'
 import injectSheet from 'grape-web/lib/jss'
 import sizes from 'grape-theme/dist/sizes'
 import { small } from 'grape-theme/dist/fonts'
-import { isElectron } from 'grape-web/lib/x-platform/electron'
-import { Link as RouterLink } from 'grape-web/lib/router'
 import linkButton from '../button/link'
 import buttonIcon from '../button/icon'
 
@@ -35,18 +33,6 @@ const styles = ({ palette }) => ({
 // In a Browser it should be opened as a new tab.
 function VideoConferenceLink(props) {
   const { channel, classes } = props
-
-  if (isElectron) {
-    return (
-      <RouterLink to={channel.videoconferenceUrl} className={classes.root}>
-        <FormattedMessage
-          id="joinVideoConference"
-          defaultMessage="Join video conference"
-          description="Sidebar: link to join a video conference"
-        />
-      </RouterLink>
-    )
-  }
 
   return (
     <a
