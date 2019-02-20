5f6a70d4e865c03ca7c0d28622760a10f390eb79
Oleh Aloshkin <olegaleshkin@gmail.com>
2019-02-20 18:19:10 +0100

    Override dialog zIndex value for mui theme

--- src/mui-theme/index.js
+++ src/mui-theme/index.js
@@ -1,6 +1,7 @@
 import createMuiTheme from 'material-ui/styles/createMuiTheme'
 import merge from 'lodash/merge'
 import palette from './palette'
+import zIndex from './zIndex'
 import typography, { constants as typographyConstants } from './typography'
 import * as overrides from './overrides'
 import './setupDefaults'
@@ -14,6 +15,7 @@ export const create = theme =>
         typography,
         palette,
         overrides,
+        zIndex,
       },
       theme,
     ),
