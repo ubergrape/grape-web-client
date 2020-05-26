30d01b944825bc34bb5e330c8f836d1e7f3f7b45
Oleh Aloshkin <olegaleshkin@gmail.com>
2020-05-26 20:02:24 +0200

    Translation import

---
+++ src/i18n/pl.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+/**
+ * This is auto-generated file by 'i18n/scripts/import.js'.
+ * Details at https://github.com/ubergrape/chatgrape/wiki/Web-client-i18n
+ * DON'T EDIT!
+ */
+export default {}
