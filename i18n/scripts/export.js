99954e40b2f94cde5244b24c2c77d65df0d69b86
Oleh Aloshkin <olegaleshkin@gmail.com>
2020-06-25 20:00:03 +0200

    Renamed translations scripts names and synchronized translations import and export scripts with other repos

--- i18n/scripts/export.js
+++ i18n/scripts/export.js
@@ -1,5 +1,6 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { sync as globSync } from 'glob'
 import * as fs from 'fs'
-import {sync as globSync} from 'glob'
 
 const messagesPattern = './i18n/src/**/*.json'
 const filePath = './i18n/translations/en.json'
@@ -9,23 +10,24 @@ const lastImport = JSON.parse(fs.readFileSync(filePath, 'utf8'))
 // React components via the React Intl Babel plugin. An error will be thrown if
 // there are messages in different components that use the same `id`. The result
 // is a flat collection of `id: message` pairs for the app's default locale.
-const defaultMessages = globSync(messagesPattern)
-    .map((filename) => fs.readFileSync(filename, 'utf8'))
-    .map((file) => JSON.parse(file))
-    .reduce((collection, descriptors) => {
-      descriptors.forEach(({id, defaultMessage, description}) => {
-        if (collection.hasOwnProperty(id) &&
-          collection[id] !== defaultMessage) {
-          throw new Error(`Duplicate message id: ${id}`)
-        }
 
-        // use edited value from last import or create new from source
-        collection[id] = lastImport[id] || defaultMessage
-        if (description) collection[`_${id}.comment`] = description
-      })
+globSync(messagesPattern)
+  .map(filename => fs.readFileSync(filename, 'utf8'))
+  .map(file => JSON.parse(file))
+  .reduce((collection, descriptors) => {
+    const newCollection = collection
+    descriptors.forEach(({ id, defaultMessage, description }) => {
+      if (
+        Object.prototype.hasOwnProperty.call(newCollection, id) &&
+        newCollection[id] !== defaultMessage
+      ) {
+        throw new Error(`Duplicate message id: ${id}`)
+      }
 
-      return collection
-    }, {})
+      // use edited value from last import or create new from source
+      newCollection[id] = lastImport[id] || defaultMessage
+      if (description) newCollection[`_${id}.comment`] = description
+    })
 
-
-fs.writeFileSync(filePath, JSON.stringify(defaultMessages, null, 2))
+    return newCollection
+  }, {})
--- i18n/scripts/import.js
+++ i18n/scripts/import.js
@@ -1,22 +1,28 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { sync } from 'glob'
 import * as fs from 'fs'
-import {sync} from 'glob'
 
 const messagesPattern = './i18n/translations/*.json'
 const outputDir = './src/i18n/'
 const commentRegExp = /\.comment$/
 
-sync(messagesPattern).map((file) => {
-  const filename = file.split('/').pop().split('.').slice(0, -1).join('.')
+sync(messagesPattern).forEach(file => {
+  const filename = file
+    .split('/')
+    .pop()
+    .split('.')
+    .slice(0, -1)
+    .join('.')
   const parsed = JSON.parse(fs.readFileSync(file, 'utf8'))
   const messages = Object.keys(parsed).reduce((result, key) => {
+    const newResult = result
     if (commentRegExp.test(key)) return result
     const message = parsed[key]
-    result[key] = message
+    newResult[key] = message
     return result
   }, {})
 
-  const output =
-`/* eslint-disable */
+  const output = `/* eslint-disable */
 /**
  * This is auto-generated file by 'i18n/scripts/import.js'.
  * Details at https://github.com/ubergrape/chatgrape/wiki/Web-client-i18n
