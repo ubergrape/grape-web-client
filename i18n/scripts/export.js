369330dbecbf7e36f40f312eb715299e16df4c04
Oleh Aloshkin <olegaleshkin@gmail.com>
2020-06-27 20:56:10 +0200

    Sync import/export translations scripts with other repos and import translations

--- i18n/scripts/export.js
+++ i18n/scripts/export.js
@@ -1,5 +1,6 @@
-import * as fs from 'fs'
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { sync as globSync } from 'glob'
+import * as fs from 'fs'
 
 const messagesPattern = './i18n/src/**/*.json'
 const filePath = './i18n/translations/en.json'
@@ -9,21 +10,24 @@ const lastImport = JSON.parse(fs.readFileSync(filePath, 'utf8'))
 // React components via the React Intl Babel plugin. An error will be thrown if
 // there are messages in different components that use the same `id`. The result
 // is a flat collection of `id: message` pairs for the app's default locale.
-const defaultMessages = globSync(messagesPattern)
+
+globSync(messagesPattern)
   .map(filename => fs.readFileSync(filename, 'utf8'))
   .map(file => JSON.parse(file))
   .reduce((collection, descriptors) => {
+    const newCollection = collection
     descriptors.forEach(({ id, defaultMessage, description }) => {
-      if (collection.hasOwnProperty(id) && collection[id] !== defaultMessage) {
+      if (
+        Object.prototype.hasOwnProperty.call(newCollection, id) &&
+        newCollection[id] !== defaultMessage
+      ) {
         throw new Error(`Duplicate message id: ${id}`)
       }
 
       // use edited value from last import or create new from source
-      collection[id] = lastImport[id] || defaultMessage
-      if (description) collection[`_${id}.comment`] = description
+      newCollection[id] = lastImport[id] || defaultMessage
+      if (description) newCollection[`_${id}.comment`] = description
     })
 
-    return collection
+    return newCollection
   }, {})
-
-fs.writeFileSync(filePath, JSON.stringify(defaultMessages, null, 2))
