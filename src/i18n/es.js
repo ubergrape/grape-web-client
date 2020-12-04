5aab6c160fe7fcf6a95b51eced11aa23d8a60d41
Oleh Aloshkin <olegaleshkin@gmail.com>
2020-12-04 17:39:07 +0100

    Added Spanish, Ukrainian, Romanian, Turkish and Russian translations

---
+++ src/i18n/es.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+/**
+ * This is auto-generated file by 'i18n/scripts/import.js'.
+ * Details at https://github.com/ubergrape/chatgrape/wiki/Web-client-i18n
+ * DON'T EDIT!
+ */
+export default {
+  "noEmojiFound": "No se encontró ningún emoji.",
+  "enterKeywordPlaceholder": "Introducir una palabra clave..",
+  "enterMessagePlaceholder": "Escribir un mensaje..",
+  "groupPostingLimitedPlaceholder": "El envío de mensajes a este grupo ha sido limitado.",
+  "willNotifyGroup": "- notificar a todos los miembros de este grupo",
+  "wontNotifyGroup": "- no notifica a los miembros del grupo",
+  "notInGroup": "no en el grupo",
+  "insertAction": "Insertar en el mensaje",
+  "openAction": "Abrir",
+  "noDetailForItem": "No hay información detallada para este elemento",
+  "searchInfo": "Busque en Wikipedia, YouTube, Giphy y {tools} haciendo clic en {plusIcon} o pulsando la tecla + del teclado",
+  "yourBusinessTools": "sus herramientas de trabajo",
+  "resultsAmountHint": "{amount} resultados",
+  "noResults": "No hay resultados.",
+  "amountResults": "{resultsAmount} {resultsAmount, plural, uno {Result} otro {Results}}",
+  "serviesHeadline": "seleccionar el servicio",
+  "noServicesFound": "No se han encontrado servicios."
+}
