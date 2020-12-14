f9e3460e19c4fffb3421909cb67c4f5d068de1a3
Oleh Aloshkin <olegaleshkin@gmail.com>
2020-12-14 11:35:13 +0100

    Import translations, correct export file name for ukrainian

--- src/i18n/ro.js
+++ src/i18n/ro.js
@@ -4,4 +4,22 @@
  * Details at https://github.com/ubergrape/chatgrape/wiki/Web-client-i18n
  * DON'T EDIT!
  */
-export default {}
+export default {
+  "noEmojiFound": "Nu s-a găsit niciun emoji.",
+  "enterKeywordPlaceholder": "Introduceți un cuvânt cheie ...",
+  "enterMessagePlaceholder": "Introduceți un mesaj ...",
+  "groupPostingLimitedPlaceholder": "Postările în acest grup au fost limitate.",
+  "willNotifyGroup": "- anunțați pe toți din acest grup",
+  "wontNotifyGroup": "- nu notifică membrii grupului",
+  "notInGroup": "nu în grup",
+  "insertAction": "Introduceți în mesaj",
+  "openAction": "Deschis",
+  "noDetailForItem": "Nu există informații detaliate pentru acest articol",
+  "searchInfo": "Căutați pe Wikipedia, YouTube, Giphy și {tools} făcând clic pe {plusIcon} sau apăsând tasta plus de pe tastatură",
+  "yourBusinessTools": "instrumentele dvs. de afaceri",
+  "resultsAmountHint": "{amount} rezultate",
+  "noResults": "Fara rezultate.",
+  "amountResults": "{resultsAmount} {resultsAmount, plural, one {Result} other {Results}}",
+  "serviesHeadline": "selectați serviciul",
+  "noServicesFound": "Nu s-au găsit servicii."
+}
--- src/i18n/tr.js
+++ src/i18n/tr.js
@@ -4,4 +4,22 @@
  * Details at https://github.com/ubergrape/chatgrape/wiki/Web-client-i18n
  * DON'T EDIT!
  */
-export default {}
+export default {
+  "noEmojiFound": "Emoji bulunamadı.",
+  "enterKeywordPlaceholder": "Bir anahtar kelime girin…",
+  "enterMessagePlaceholder": "Bir mesaj girin…",
+  "groupPostingLimitedPlaceholder": "Bu gruba gönderme sınırlandırılmıştır.",
+  "willNotifyGroup": "- bu gruptaki herkese bildir",
+  "wontNotifyGroup": "- grup üyelerini bilgilendirmez",
+  "notInGroup": "grupta değil",
+  "insertAction": "Mesaja ekle",
+  "openAction": "Açık",
+  "noDetailForItem": "Bu öğe için detay bilgisi yok",
+  "searchInfo": "{plusIcon} 'u tıklayarak veya klavyenizdeki artı tuşuna basarak Wikipedia, YouTube, Giphy ve {tools}' da arayın",
+  "yourBusinessTools": "iş araçlarınız",
+  "resultsAmountHint": "{amount} sonuçları",
+  "noResults": "Sonuç yok.",
+  "amountResults": "{resultsAmount} {resultsAmount, plural, one {Result} other {Results}}",
+  "serviesHeadline": "hizmet seçin",
+  "noServicesFound": "Hizmet bulunamadı."
+}
