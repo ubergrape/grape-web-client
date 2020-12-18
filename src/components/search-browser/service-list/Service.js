706588f1ee6d0dbf0e571648e0f8e1d8df3f1fa0
Oleh Aloshkin <olegaleshkin@gmail.com>
2020-12-18 20:49:58 +0100

    Break down complicated text segments with plural forms into easier ones

--- src/components/search-browser/service-list/Service.js
+++ src/components/search-browser/service-list/Service.js
@@ -1,56 +1,64 @@
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-import noop from 'lodash/noop'
+import React from 'react'
 import injectSheet from 'grape-web/lib/jss'
 import { FormattedMessage } from 'react-intl'
 
 import style from './serviceStyle'
 import ServiceIcon from '../service-icon/ServiceIcon'
 
-class Service extends Component {
-  static propTypes = {
-    sheet: PropTypes.object.isRequired,
-    focused: PropTypes.bool,
-    item: PropTypes.object,
-    resultsAmount: PropTypes.number,
-    onSelect: PropTypes.func,
-    onFocus: PropTypes.func,
-  }
-
-  static defaultProps = {
-    onSelect: noop,
-    onFocus: noop,
-  }
-
-  render() {
-    const { classes } = this.props.sheet
-    const { item, focused, resultsAmount } = this.props
-
-    return (
-      <div
-        className={`${classes.service} ${
-          focused ? classes.serviceFocused : ''
-        }`}
-        onClick={this.props.onSelect.bind(null, item)}
-        onMouseOver={this.props.onFocus.bind(null, item)}
-      >
-        <div className={classes.iconContainer}>
-          <ServiceIcon service={item.id} />
-        </div>
-        <div className={classes.name}>{item.label}</div>
-        {resultsAmount && (
-          <div className={classes.hint}>
-            <FormattedMessage
-              id="amountResults"
-              defaultMessage="{resultsAmount} {resultsAmount, plural, one {Result} other {Results}}"
-              values={{ resultsAmount }}
-            />
-          </div>
-        )}
-        <div className={classes.return}>&crarr;</div>
+const Service = ({
+  item,
+  focused,
+  resultsAmount,
+  onSelect,
+  onFocus,
+  sheet: { classes },
+}) => (
+  <button
+    type="button"
+    className={`${classes.service} ${focused ? classes.serviceFocused : ''}`}
+    onClick={onSelect.bind(null, item)}
+    onMouseOver={onFocus.bind(null, item)}
+    onFocus={onFocus.bind(null, item)}
+  >
+    <div className={classes.iconContainer}>
+      <ServiceIcon service={item.id} />
+    </div>
+    <div className={classes.name}>{item.label}</div>
+    {resultsAmount && (
+      <div className={classes.hint}>
+        <FormattedMessage
+          id="amountResults"
+          defaultMessage="{resultsAmount} {resultsAmount, plural, one {{resultsAmountResult}} other {{resultsAmountResults}}}"
+          values={{
+            resultsAmount,
+            resultsAmountResult: (
+              <FormattedMessage
+                id="resultsAmountResult"
+                defaultMessage="Result"
+              />
+            ),
+            resultsAmountResults: (
+              <FormattedMessage
+                id="resultsAmountResults"
+                defaultMessage="Results"
+              />
+            ),
+          }}
+        />
       </div>
-    )
-  }
+    )}
+    <div className={classes.return}>&crarr;</div>
+  </button>
+)
+
+Service.propTypes = {
+  sheet: PropTypes.object.isRequired,
+  focused: PropTypes.bool.isRequired,
+  item: PropTypes.object.isRequired,
+  resultsAmount: PropTypes.number.isRequired,
+  onSelect: PropTypes.func.isRequired,
+  onFocus: PropTypes.func.isRequired,
 }
 
 export default injectSheet(style)(Service)
